1c3c984ad1d87f276c64cdec174d761d536f19ea
mayandar <mayandar@9cf8542c-9828-0410-8776-d8f572d62bc1>
2009-02-11 03:19:22 +0000

    Set new DB Design; minor changes in Maps class
    git-svn-id: svn+ssh://devel.inticol.com/home/svn/di8/trunk@549 9cf8542c-9828-0410-8776-d8f572d62bc1

--- files/database/core.sql
+++ files/database/core.sql
@@ -1,77 +1,77 @@
-/*
-  DesInventar - http://www.desinventar.org
-  (c) 1998-2009 Corporacion OSSO
-  CORE.DB
+/* CORE.DB - DesInventar8.2
+2009-02-09
 */
+
 DROP TABLE IF EXISTS Region;
-CREATE TABLE Region (
-	RegionId VARCHAR(50),
-	RegionLabel VARCHAR(200) NOT NULL,
-	LangIsoCode VARCHAR(3),
-	CountryIso VARCHAR(3),
-	RegionOrder INTEGER,
-	RegionStatus INTEGER,
-	RegionLastUpdate DATETIME,
-	IsCRegion BOOLEAN,
-	IsVRegion BOOLEAN,
-	PRIMARY KEY(RegionId)
+CREATE TABLE 'Region' ( 
+RegionId VARCHAR(50), 
+RegionLabel VARCHAR(200) NOT NULL, 
+LangIsoCode VARCHAR(3), 
+CountryIso VARCHAR(3), 
+RegionOrder INTEGER, 
+RegionStatus INTEGER, 
+RegionLastUpdate DATETIME, 
+IsCRegion BOOLEAN, 
+IsVRegion BOOLEAN, 
+PRIMARY KEY('RegionId')
 );
 
 DROP TABLE IF EXISTS CVRegionItem;
-CREATE TABLE CVRegionItem ( 
-	RegionId VARCHAR(50),
-	RegionItem VARCHAR(50),
-	RegionQuery VARCHAR(50),
-	PRIMARY KEY(RegionId,RegionItem)
+CREATE TABLE 'CVRegionItem' ( 
+RegionId VARCHAR(50), 
+RegionItem VARCHAR(50), 
+RegionQuery VARCHAR(50), 
+PRIMARY KEY('RegionId','RegionItem')
 );
 
 DROP TABLE IF EXISTS RegionAuth;
-CREATE TABLE RegionAuth (
-	UserName VARCHAR(20) NOT NULL,
-	RegionId VARCHAR(50),
-	AuthKey VARCHAR(50),
-	AuthValue INTEGER,
-	AuthAuxValue VARCHAR(1024),
-	PRIMARY KEY(UserName,RegionId,AuthKey)
+CREATE TABLE 'RegionAuth' ( 
+Username VARCHAR(20) NOT NULL, 
+RegionId VARCHAR(50), 
+AuthKey VARCHAR(50), 
+AuthValue INTEGER, 
+AuthAuxValue VARCHAR(1024), 
+PRIMARY KEY('Username','RegionId','AuthKey')
 );
 
 DROP TABLE IF EXISTS Queries;
-CREATE TABLE Queries (
-	QueryId VARCHAR(50),
-	RegionId VARCHAR(50),
-	QueryStatus VARCHAR(10),
-	UserName VARCHAR(20),
-	QueryDate DATETIME,
-	QueryName VARCHAR(100),
-	QueryContent TEXT,
-	PRIMARY KEY(QueryId)
+CREATE TABLE 'Queries' ( 
+QueryId VARCHAR(50), 
+RegionId VARCHAR(50), 
+QueryStatus VARCHAR(10), 
+Username VARCHAR(20), 
+QueryDate DATETIME, 
+QueryName VARCHAR(100), 
+QueryContent TEXT, 
+PRIMARY KEY('QueryId')
 );
 
 DROP TABLE IF EXISTS User;
-CREATE TABLE User ( 
-	UserName VARCHAR(20) NOT NULL,
-	UserEMail VARCHAR(100) NOT NULL,
-	UserPasswd VARCHAR(100) NOT NULL,
-	UserFullName VARCHAR(100) NOT NULL,
-	Organization VARCHAR(100),
-	CountryIso VARCHAR(3),
-	UserCity VARCHAR(50),
-	UserCreationDate  DATETIME,
-	UserNotes TEXT,
-	UserActive BOOLEAN,
-	PRIMARY KEY(UserName)
+CREATE TABLE 'User' ( 
+Username VARCHAR(20) NOT NULL, 
+UserEMail VARCHAR(100) NOT NULL, 
+UserPasswd VARCHAR(100) NOT NULL, 
+UserFullName VARCHAR(100) NOT NULL, 
+Organization VARCHAR(100), 
+CountryIso VARCHAR(3), 
+UserCity VARCHAR(50), 
+UserCreationDate  DATETIME, 
+UserNotes TEXT, 
+UserActive BOOLEAN, 
+PRIMARY KEY('Username')
 );
 
 DROP TABLE IF EXISTS UserOption;
-CREATE TABLE UserOption ( 
-	UserName VARCHAR(20) NOT NULL,
-	OptionKey VARCHAR(50),
-	OptionValue VARCHAR(1024),
-	OptionAuxValue VARCHAR(1024),
-	PRIMARY KEY(UserName,OptionKey)
+CREATE TABLE 'UserOption' ( 
+Username VARCHAR(20) NOT NULL, 
+OptionKey VARCHAR(50), 
+OptionValue VARCHAR(1024), 
+OptionAuxValue VARCHAR(1024), 
+PRIMARY KEY('Username','OptionKey')
 );
 
+
 /* Set initial values */
-INSERT INTO User VALUES ('root','root@localhost','root','Portal Administrator','','','','2008-01-01','',1);
-INSERT INTO RegionAuth VALUES ('root','','REGION',5,'');
-INSERT INTO RegionAuth VALUES ('root','','USER',5,'');
+INSERT INTO User VALUES ('root', 'root@localhost', 'root', 'Portal Administrator', '', '', '', '2008-01-01', '', true);
+INSERT INTO RegionAuth VALUES ('root', '', 'REGION', 5, '');
+INSERT INTO RegionAuth VALUES ('root', '', 'USER', 5, '');
--- files/database/region.sql
+++ files/database/region.sql
@@ -1,162 +1,171 @@
-/*
-  DesInventar - http://www.desinventar.org
-  (c) 1998-2009 Corporacion OSSO
-  REGION.DB
+/* <REGION>.DB - DesInventar8.2
+2009-02-09
 */
 
 DROP TABLE IF EXISTS Info;
-CREATE TABLE Info (
-	InfoKey VARCHAR(50), 
-	InfoValue VARCHAR(1024), 
-	InfoAuxValue VARCHAR(1024),
-	PRIMARY KEY(InfoKey)
+CREATE TABLE 'Info' ( 
+InfoKey VARCHAR(50), 
+InfoValue VARCHAR(1024), 
+InfoAuxValue VARCHAR(1024), 
+PRIMARY KEY('InfoKey')
 );
 
 DROP TABLE IF EXISTS Event;
-CREATE TABLE Event (
-	EventId VARCHAR(50) NOT NULL, 
-	EventLangCode VARCHAR(3) NOT NULL, 
-	EvenlName VARCHAR(50), 
-	EventDesc TEXT, 
-	EventActive BOOLEAN NOT NULL DEFAULT TRUE, 
-	EventPredefined BOOLEAN NOT NULL DEFAULT False, 
-	EventRGBColor VARCHAR(10), 
-	EventKeyWords TEXT, 
-	EventCreationDate DATETIME, 
-	EventLastUpdate DATETIME,
-	PRIMARY KEY(EventId,EventLangCode)
+CREATE TABLE 'Event' ( 
+EventId VARCHAR(50) NOT NULL, 
+EventLangCode VARCHAR(3) NOT NULL, 
+EvenlName VARCHAR(50), 
+EventDesc TEXT, 
+EventActive BOOLEAN NOT NULL DEFAULT TRUE, 
+EventPredefined BOOLEAN NOT NULL DEFAULT False, 
+EventRGBColor VARCHAR(10), 
+EventKeyWords TEXT, 
+EventCreationDate DATETIME, 
+EventLastUpdate DATETIME, 
+PRIMARY KEY('EventId','EventLangCode')
 );
 
+
 DROP TABLE IF EXISTS Cause;
-CREATE TABLE Cause (
-	CauseId VARCHAR(50) NOT NULL, 
-	CauseLangCode VARCHAR(3) NOT NULL, 
-	CauseName VARCHAR(50), 
-	CauseDesc TEXT, 
-	CauseActive BOOLEAN NOT NULL DEFAULT TRUE, 
-	CausePredefined BOOLEAN NOT NULL DEFAULT False, 
-	CauseRGBColor VARCHAR(10), 
-	CauseKeyWords TEXT, 
-	CauseCreationDate DATETIME, 
-	CauseLastUpdate DATETIME,
-	PRIMARY KEY(CauseId,CauseLangCode)
+CREATE TABLE 'Cause' ( 
+CauseId VARCHAR(50) NOT NULL, 
+CauseLangCode VARCHAR(3) NOT NULL, 
+CauseName VARCHAR(50), 
+CauseDesc TEXT, 
+CauseActive BOOLEAN NOT NULL DEFAULT TRUE, 
+CausePredefined BOOLEAN NOT NULL DEFAULT False, 
+CauseRGBColor VARCHAR(10), 
+CauseKeyWords TEXT, 
+CauseCreationDate DATETIME, 
+CauseLastUpdate DATETIME, 
+PRIMARY KEY('CauseId','CauseLangCode')
 );
 
 
 DROP TABLE IF EXISTS GeoLevel;
-CREATE TABLE GeoLevel (
-	GeoLevelId INTEGER NOT NULL, 
-	GeoLevelLangCode VARCHAR(3) NOT NULL, 
-	GeoLevelName VARCHAR(50) NOT NULL DEFAULT '---', 
-	GeoLevelDesc TEXT NULL, 
-	GeoLevelActive BOOLEAN NOT NULL DEFAULT False, 
-	GeoLevelLayerFile VARCHAR(50) NULL, 
-	GeoLevelLayerName VARCHAR(50) NULL, 
-	GeoLevelLayerCode VARCHAR(50) NULL,
-	PRIMARY KEY(GeoLevelId,GeoLevelLangCode)
+CREATE TABLE 'GeoLevel' ( 
+GeoLevelId INTEGER NOT NULL, 
+GeoLevelLangCode VARCHAR(3) NOT NULL, 
+GeoLevelName VARCHAR(50) NOT NULL DEFAULT '---', 
+GeoLevelDesc TEXT NULL, 
+GeoLevelActive BOOLEAN NOT NULL DEFAULT False, 
+GeoLevelLayerFile VARCHAR(50) NULL, 
+GeoLevelLayerName VARCHAR(50) NULL, 
+GeoLevelLayerCode VARCHAR(50) NULL, 
+PRIMARY KEY('GeoLevelId','GeoLevelLangCode')
 );
 
 
 DROP TABLE IF EXISTS Geography;
-CREATE TABLE Geography (
-	GeographyId VARCHAR(100) NOT NULL, 
-	GeographyLangCode VARCHAR(3) NOT NULL, 
-	GeographyCode VARCHAR(100) NOT NULL DEFAULT '---', 
-	GeographyName VARCHAR(200) NOT NULL DEFAULT '---', 
-	GeographyLevel INTEGER DEFAULT -1, 
-	GeographyActive BOOLEAN NOT NULL DEFAULT TRUE,
-	PRIMARY KEY(GeographyId,GeographyLangCode)
-);
-
-DROP TABLE IF EXISTS EEField;
-CREATE TABLE EEField (
-	EEFieldId VARCHAR(30) NOT NULL, 
-	EEGroupId VARCHAR(30), 
-	EEFieldLabel VARCHAR(30), 
-	EEFieldDesc TEXT, 
-	EEFieldType VARCHAR(20), 
-	EEFieldSize INTEGER, 
-	EEFieldOrder INTEGER, 
-	EEFieldStatus INTEGER,
-	PRIMARY KEY(EEFieldId)
-);
-
-DROP TABLE IF EXISTS EEData;
-CREATE TABLE EEData (
-	DisasterId VARCHAR(50) NOT NULL, 
-	PRIMARY KEY(DisasterId)
+CREATE TABLE 'Geography' ( 
+GeographyId VARCHAR(100) NOT NULL, 
+GeographyLangCode VARCHAR(3) NOT NULL, 
+GeographyCode VARCHAR(100) NOT NULL DEFAULT '---', 
+GeographyName VARCHAR(200) NOT NULL DEFAULT '---', 
+GeographyLevel INTEGER DEFAULT -1, 
+GeographyActive BOOLEAN NOT NULL DEFAULT TRUE, 
+PRIMARY KEY('GeographyId','GeographyLangCode')
 );
 
 DROP TABLE IF EXISTS Disaster;
-CREATE TABLE Disaster (
-	DisasterId VARCHAR(50) NOT NULL, 
-	DisasterSerial VARCHAR(50) NOT NULL, 
-	DisasterBeginTime VARCHAR(30) NOT NULL, 
-	DisasterGeographyId VARCHAR(100) NOT NULL, 
-	DisasterSiteNotes TEXT, 
-	DisasterLatitude DOUBLE, 
-	DisasterLongitude DOUBLE, 
-	DisasterSource VARCHAR(200), 
-	RecordStatus VARCHAR(20), 
-	RecordAuthor VARCHAR(100), 
-	RecordCreation DATETIME, 
-	RecordLastUpdate DATETIME, 
-	EventId VARCHAR(50), 
-	EventNotes TEXT, 
-	EventDuration INTEGER, 
-	EventMagnitude VARCHAR(80), 
-	CauseId VARCHAR(50), 
-	CauseNotes TEXT, 
-	EffectPeopleDead INTEGER, 
-	EffectPeopleMissing INTEGER, 
-	EffectPeopleInjured INTEGER, 
-	EffectPeopleHarmed INTEGER, 
-	EffectPeopleAffected INTEGER, 
-	EffectPeopleEvacuated INTEGER, 
-	EffectPeopleRelocated INTEGER, 
-	EffectHousesDestroyed INTEGER, 
-	EffectHousesAffected INTEGER, 
-	EffectPeopleDeadQ INTEGER, 
-	EffectPeopleMissingQ INTEGER, 
-	EffectPeopleInjuredQ INTEGER, 
-	EffectPeopleHarmedQ INTEGER, 
-	EffectPeopleAffectedQ INTEGER, 
-	EffectPeopleEvacuatedQ INTEGER, 
-	EffectPeopleRelocatedQ INTEGER, 
-	EffectHousesDestroyedQ INTEGER, 
-	EffectHousesAffectedQ INTEGER, 
-	EffectLossesValueLocal NUMERIC(30,2), 
-	EffectLossesValueUSD NUMERIC(30,2), 
-	EffectRoads NUMERIC(30,2), 
-	EffectFarmingAndForest NUMERIC(30,2), 
-	EffectLiveStock NUMERIC(10), 
-	EffectEducationCenters NUMERIC(10), 
-	EffectMedicalCenters NUMERIC(10), 
-	EffectOtherLosses TEXT, 
-	EffectNotes TEXT, 
-	SectorTransportQ INTEGER, 
-	SectorCommunicationsQ INTEGER, 
-	SectorReliefQ INTEGER,
-	SectorAgriculturalQ INTEGER, 
-	SectorWaterSupplyQ INTEGER, 
-	SectorSewerageQ INTEGER, 
-	SectorEducationQ INTEGER, 
-	SectorPowerQ INTEGER, 
-	SectorIndustryQ INTEGER, 
-	SectorHealthQ INTEGER, 
-	SectorOtherQ INTEGER,
-	PRIMARY KEY(DisasterId)
+CREATE TABLE 'Disaster' ( 
+DisasterId VARCHAR(50) NOT NULL, 
+DisasterSerial VARCHAR(50) NOT NULL, 
+DisasterBeginTime VARCHAR(30) NOT NULL, 
+DisasterGeographyId VARCHAR(100) NOT NULL, 
+DisasterSiteNotes TEXT, 
+DisasterLatitude DOUBLE, 
+DisasterLongitude DOUBLE, 
+DisasterSource VARCHAR(200), 
+RecordStatus VARCHAR(20), 
+RecordAuthor VARCHAR(100), 
+RecordCreation DATETIME, 
+RecordLastUpdate DATETIME, 
+EventId VARCHAR(50), 
+EventNotes TEXT, 
+EventDuration INTEGER, 
+EventMagnitude VARCHAR(80), 
+CauseId VARCHAR(50), 
+CauseNotes TEXT, 
+EffectPeopleDead INTEGER, 
+EffectPeopleMissing INTEGER, 
+EffectPeopleInjured INTEGER, 
+EffectPeopleHarmed INTEGER, 
+EffectPeopleAffected INTEGER, 
+EffectPeopleEvacuated INTEGER, 
+EffectPeopleRelocated INTEGER, 
+EffectHousesDestroyed INTEGER, 
+EffectHousesAffected INTEGER, 
+EffectPeopleDeadQ INTEGER, 
+EffectPeopleMissingQ INTEGER, 
+EffectPeopleInjuredQ INTEGER, 
+EffectPeopleHarmedQ INTEGER, 
+EffectPeopleAffectedQ INTEGER, 
+EffectPeopleEvacuatedQ INTEGER, 
+EffectPeopleRelocatedQ INTEGER, 
+EffectHousesDestroyedQ INTEGER, 
+EffectHousesAffectedQ INTEGER, 
+EffectLossesValueLocal NUMERIC(30,2), 
+EffectLossesValueUSD NUMERIC(30,2), 
+EffectRoads NUMERIC(30,2), 
+EffectFarmingAndForest NUMERIC(30,2), 
+EffectLiveStock NUMERIC(10), 
+EffectEducationCenters NUMERIC(10), 
+EffectMedicalCenters NUMERIC(10), 
+EffectOtherLosses TEXT, 
+EffectNotes TEXT, 
+SectorTransportQ INTEGER, 
+SectorCommunicationsQ INTEGER, 
+SectorReliefQ INTEGER, 
+SectorAgriculturalQ INTEGER, 
+SectorWaterSupplyQ INTEGER, 
+SectorSewerageQ INTEGER, 
+SectorEducationQ INTEGER, 
+SectorPowerQ INTEGER, 
+SectorIndustryQ INTEGER, 
+SectorHealthQ INTEGER, 
+SectorOtherQ INTEGER, 
+PRIMARY KEY('DisasterId')
 );
 
+
 DROP TABLE IF EXISTS EEGroup;
-CREATE TABLE EEGroup ( 
-	EEGroupId VARCHAR(30), 
-	EEGroupLabel VARCHAR(50), 
-	EEGroupDesc TEXT, 
-	EEGroupStatus INTEGER,
-	PRIMARY KEY(EEGroupId)
+CREATE TABLE 'EEGroup' ( 
+EEGroupId VARCHAR(30), 
+EEGroupLabel VARCHAR(50), 
+EEGroupDesc TEXT, 
+EEGroupStatus INTEGER, 
+PRIMARY KEY('EEGroupId')
 );
-/*
+
+DROP TABLE IF EXISTS EEField;
+CREATE TABLE 'EEField' ( 
+EEFieldId VARCHAR(30) NOT NULL, 
+EEGroupId VARCHAR(30), 
+EEFieldLabel VARCHAR(30), 
+EEFieldDesc TEXT, 
+EEFieldType VARCHAR(20), 
+EEFieldSize INTEGER, 
+EEFieldOrder INTEGER, 
+EEFieldStatus INTEGER, 
+PRIMARY KEY('EEFieldId')
+);
+
+
+DROP TABLE IF EXISTS EEData;
+CREATE TABLE 'EEData' ( 
+DisasterId VARCHAR(50) NOT NULL, 
+PRIMARY KEY('DisasterId')
+);
+
+/* Set initial values */
+INSERT INTO Info VALUES (“DBVersion”, “”. “”);
+INSERT INTO Info VALUES (“RegCountry”, “”. “”);
+INSERT INTO Info VALUES (“I18NFirstLang”, “”. “”);
+INSERT INTO Info VALUES (“I18NSecondLang”, “”. “”);
+INSERT INTO Info VALUES (“I18NThirdLang”, “”. “”);
+INSERT INTO Info VALUES (“SyncMasterURL”, “”. “”);
+INSERT INTO Info VALUES (“SyncLastupdate”, “”. “”);
 INSERT INTO Info VALUES (“PeriodBeginDate”, “”. “”);
 INSERT INTO Info VALUES (“PeriodEndDate”, “”. “”);
 INSERT INTO Info VALUES (“PeriodOutOfRange”, “”. “”);
@@ -177,6 +186,11 @@ INSERT INTO Info VALUES (“GeoLimitMaxY”, “”. “”);
 INSERT INTO Info VALUES (“CartoLayerFile”, “”. “”);
 INSERT INTO Info VALUES (“CartoLayerName”, “”. “”);
 INSERT INTO Info VALUES (“CartoLayerCode”, “”. “”);
-INSERT INTO Info VALUES (“I18NOtherLang”, “”. “”);
 
-*/
+INSERT INTO Info VALUES (“SyncLastupdate_Event”, “”. “”);
+INSERT INTO Info VALUES (“SyncLastupdate_Cause”, “”. “”);
+INSERT INTO Info VALUES (“SyncLastupdate_GeoLevel”, “”. “”);
+INSERT INTO Info VALUES (“SyncLastupdate_Geography”, “”. “”);
+INSERT INTO Info VALUES (“SyncLastupdate_Disaster”, “”. “”);
+INSERT INTO Info VALUES (“SyncLastupdate_EEField”, “”. “”);
+INSERT INTO Info VALUES (“SyncLastupdate_EEData”, “”. “”);
