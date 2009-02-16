07d380cadb6de55e3c7e8345e4cf906defdbb011
Jhon H. Caicedo <jhcaiced@inticol.com>
2009-02-16 12:47:21 +0000

    Updated dbdesign.ods to add SyncTable and SyncRecord fields in region.db
    git-svn-id: svn+ssh://devel.inticol.com/home/svn/di8/trunk@551 9cf8542c-9828-0410-8776-d8f572d62bc1

--- files/database/region.sql
+++ files/database/region.sql
@@ -1,10 +1,12 @@
-/* <REGION>.DB - DesInventar8.2
+/* REGION.DB - DesInventar8.2
 2009-02-16
 */
 
 DROP TABLE IF EXISTS Info;
 CREATE TABLE 'Info' ( 
 InfoKey VARCHAR(50), 
+SyncTable DATETIME, 
+SyncRecord DATETIME, 
 InfoValue VARCHAR(1024), 
 InfoAuxValue VARCHAR(1024), 
 PRIMARY KEY('InfoKey')
@@ -14,6 +16,8 @@ DROP TABLE IF EXISTS Event;
 CREATE TABLE 'Event' ( 
 EventId VARCHAR(50) NOT NULL, 
 EventLangCode VARCHAR(3) NOT NULL, 
+SyncTable DATETIME, 
+SyncRecord DATETIME, 
 EvenlName VARCHAR(50), 
 EventDesc TEXT, 
 EventActive BOOLEAN NOT NULL DEFAULT TRUE, 
@@ -25,11 +29,12 @@ EventLastUpdate DATETIME,
 PRIMARY KEY('EventId','EventLangCode')
 );
 
-
 DROP TABLE IF EXISTS Cause;
 CREATE TABLE 'Cause' ( 
 CauseId VARCHAR(50) NOT NULL, 
 CauseLangCode VARCHAR(3) NOT NULL, 
+SyncTable DATETIME, 
+SyncRecord DATETIME, 
 CauseName VARCHAR(50), 
 CauseDesc TEXT, 
 CauseActive BOOLEAN NOT NULL DEFAULT TRUE, 
@@ -41,11 +46,12 @@ CauseLastUpdate DATETIME,
 PRIMARY KEY('CauseId','CauseLangCode')
 );
 
-
 DROP TABLE IF EXISTS GeoLevel;
 CREATE TABLE 'GeoLevel' ( 
 GeoLevelId INTEGER NOT NULL, 
 GeoLevelLangCode VARCHAR(3) NOT NULL, 
+SyncTable DATETIME, 
+SyncRecord DATETIME, 
 GeoLevelName VARCHAR(50) NOT NULL DEFAULT '---', 
 GeoLevelDesc TEXT NULL, 
 GeoLevelActive BOOLEAN NOT NULL DEFAULT False, 
@@ -55,11 +61,12 @@ GeoLevelLayerCode VARCHAR(50) NULL,
 PRIMARY KEY('GeoLevelId','GeoLevelLangCode')
 );
 
-
 DROP TABLE IF EXISTS Geography;
 CREATE TABLE 'Geography' ( 
 GeographyId VARCHAR(100) NOT NULL, 
 GeographyLangCode VARCHAR(3) NOT NULL, 
+SyncTable DATETIME, 
+SyncRecord DATETIME, 
 GeographyCode VARCHAR(100) NOT NULL DEFAULT '---', 
 GeographyName VARCHAR(200) NOT NULL DEFAULT '---', 
 GeographyLevel INTEGER DEFAULT -1, 
@@ -70,6 +77,8 @@ PRIMARY KEY('GeographyId','GeographyLangCode')
 DROP TABLE IF EXISTS Disaster;
 CREATE TABLE 'Disaster' ( 
 DisasterId VARCHAR(50) NOT NULL, 
+SyncTable DATETIME, 
+SyncRecord DATETIME, 
 DisasterSerial VARCHAR(50) NOT NULL, 
 DisasterBeginTime VARCHAR(30) NOT NULL, 
 DisasterGeographyId VARCHAR(100) NOT NULL, 
@@ -128,10 +137,11 @@ SectorOtherQ INTEGER,
 PRIMARY KEY('DisasterId')
 );
 
-
 DROP TABLE IF EXISTS EEGroup;
 CREATE TABLE 'EEGroup' ( 
 EEGroupId VARCHAR(30), 
+SyncTable DATETIME, 
+SyncRecord DATETIME, 
 EEGroupLabel VARCHAR(50), 
 EEGroupDesc TEXT, 
 EEGroupStatus INTEGER, 
@@ -141,6 +151,8 @@ PRIMARY KEY('EEGroupId')
 DROP TABLE IF EXISTS EEField;
 CREATE TABLE 'EEField' ( 
 EEFieldId VARCHAR(30) NOT NULL, 
+SyncTable DATETIME, 
+SyncRecord DATETIME, 
 EEGroupId VARCHAR(30), 
 EEFieldLabel VARCHAR(30), 
 EEFieldDesc TEXT, 
@@ -151,46 +163,47 @@ EEFieldStatus INTEGER,
 PRIMARY KEY('EEFieldId')
 );
 
-
 DROP TABLE IF EXISTS EEData;
 CREATE TABLE 'EEData' ( 
 DisasterId VARCHAR(50) NOT NULL, 
+SyncTable DATETIME, 
+SyncRecord DATETIME, 
 PRIMARY KEY('DisasterId')
 );
 
 /* Set initial values */
-INSERT INTO Info VALUES ('DBVersion','','');
-INSERT INTO Info VALUES ('RegCountry','','');
-INSERT INTO Info VALUES ('I18NFirstLang', '', '');
-INSERT INTO Info VALUES ('I18NSecondLang', '','');
-INSERT INTO Info VALUES ('I18NThirdLang', '', '');
-INSERT INTO Info VALUES ('SyncMasterURL', '', '');
-INSERT INTO Info VALUES ('SyncLastupdate', '', '');
-INSERT INTO Info VALUES ('PeriodBeginDate', '', '');
-INSERT INTO Info VALUES ('PeriodEndDate', '', '');
-INSERT INTO Info VALUES ('PeriodOutOfRange', '', '');
-INSERT INTO Info VALUES ('InfoAdminURL', '', '');
-INSERT INTO Info VALUES ('InfoCredits', '', '');
-INSERT INTO Info VALUES ('InfoGeneral', '', '');
-INSERT INTO Info VALUES ('InfoSources', '', '');
-INSERT INTO Info VALUES ('InfoSynopsis', '', '');
-INSERT INTO Info VALUES ('InfoObservation', '', '');
-INSERT INTO Info VALUES ('InfoGeography', '', '');
-INSERT INTO Info VALUES ('InfoCartography','','');
-INSERT INTO Info VALUES ('InfoImageLogo', '', '');
-INSERT INTO Info VALUES ('InfoImageIcon', '', '');
-INSERT INTO Info VALUES ('GeoLimitMinX', '', '');
-INSERT INTO Info VALUES ('GeoLimitMinY', '', '');
-INSERT INTO Info VALUES ('GeoLimitMaxX', '', '');
-INSERT INTO Info VALUES ('GeoLimitMaxY', '', '');
-INSERT INTO Info VALUES ('CartoLayerFile', '', '');
-INSERT INTO Info VALUES ('CartoLayerName', '', '');
-INSERT INTO Info VALUES ('CartoLayerCode', '', '');
+INSERT INTO Info VALUES ('DBVersion','','','','');
+INSERT INTO Info VALUES ('RegCountry','','','','');
+INSERT INTO Info VALUES ('I18NFirstLang','','','','');
+INSERT INTO Info VALUES ('I18NSecondLang','','','','');
+INSERT INTO Info VALUES ('I18NThirdLang','','','','');
+INSERT INTO Info VALUES ('SyncMasterURL','','','','');
+INSERT INTO Info VALUES ('SyncLastupdate','','','','');
+INSERT INTO Info VALUES ('PeriodBeginDate','','','','');
+INSERT INTO Info VALUES ('PeriodEndDate','','','','');
+INSERT INTO Info VALUES ('PeriodOutOfRange','','','','');
+INSERT INTO Info VALUES ('InfoAdminURL','','','','');
+INSERT INTO Info VALUES ('InfoCredits','','','','');
+INSERT INTO Info VALUES ('InfoGeneral','','','','');
+INSERT INTO Info VALUES ('InfoSources','','','','');
+INSERT INTO Info VALUES ('InfoSynopsis','','','','');
+INSERT INTO Info VALUES ('InfoObservation','','','','');
+INSERT INTO Info VALUES ('InfoGeography','','','','');
+INSERT INTO Info VALUES ('InfoCartography','','','','');
+INSERT INTO Info VALUES ('InfoImageLogo','','','','');
+INSERT INTO Info VALUES ('InfoImageIcon','','','','');
+INSERT INTO Info VALUES ('GeoLimitMinX','','','','');
+INSERT INTO Info VALUES ('GeoLimitMinY','','','','');
+INSERT INTO Info VALUES ('GeoLimitMaxX','','','','');
+INSERT INTO Info VALUES ('GeoLimitMaxY','','','','');
+INSERT INTO Info VALUES ('CartoLayerFile','','','','');
+INSERT INTO Info VALUES ('CartoLayerName','','','','');
+INSERT INTO Info VALUES ('CartoLayerCode','','','','');
 
-INSERT INTO Info VALUES ('Sync_Event', '', '');
-INSERT INTO Info VALUES ('Sync_Cause', '', '');
-INSERT INTO Info VALUES ('Sync_GeoLevel', '', '');
-INSERT INTO Info VALUES ('Sync_Geography', '', '');
-INSERT INTO Info VALUES ('Sync_Disaster', '', '');
-INSERT INTO Info VALUES ('Sync_EEField', '', '');
-INSERT INTO Info VALUES ('Sync_EEData', '', '');
+INSERT INTO Info VALUES ('Sync_Event','','','','');
+INSERT INTO Info VALUES ('Sync_Cause','','','','');
+INSERT INTO Info VALUES ('Sync_GeoLevel','','','','');
+INSERT INTO Info VALUES ('Sync_Geography','','','','');
+INSERT INTO Info VALUES ('Sync_Disaster','','','','');
+INSERT INTO Info VALUES ('Sync_EEField','','','','');
+INSERT INTO Info VALUES ('Sync_EEData','','','','');
