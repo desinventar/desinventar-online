7dd95a9d4f3802febb38c045e035dc82360cbbc7
jhcaiced <jhcaiced@9cf8542c-9828-0410-8776-d8f572d62bc1>
2009-03-01 14:32:59 +0000

    Added constants.php to handle different constant values
    git-svn-id: svn+ssh://devel.inticol.com/home/svn/di8/trunk@578 9cf8542c-9828-0410-8776-d8f572d62bc1

---
+++ web/include/constants.php
@@ -0,0 +1,12 @@
+<script language="php">
+/*
+ **********************************************
+ DesInventar - http://www.desinventar.org  
+ (c) 1999-2009 Corporacion OSSO
+ **********************************************
+*/
+
+define('CONST_REGIONACTIVE', 1);
+define('CONST_REGIONPUBLIC', 2);
+
+</script>
