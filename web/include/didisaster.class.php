2c11a8d604f1d8a0e35112467bb3fa131c68689c
jhcaiced <jhcaiced@9cf8542c-9828-0410-8776-d8f572d62bc1>
2009-07-04 13:58:10 +0000

    Fixed didisaster.class.php for use with SQLite
    git-svn-id: svn+ssh://devel.inticol.com/home/svn/di8/trunk@662 9cf8542c-9828-0410-8776-d8f572d62bc1

--- web/include/didisaster.class.php
+++ web/include/didisaster.class.php
@@ -71,7 +71,7 @@ class DIDisaster extends DIObject {
 		                      "SectorPower/INTEGER," .
 		                      "SectorIndustry/INTEGER," .
 		                      "SectorHealth/INTEGER," .
-		                      "SectorOther/INTEGER"
+		                      "SectorOther/INTEGER";
 		parent::__construct($prmSession);
 		$this->set("EventPredefined", 0);
 		$this->set("EventActive", 1);
@@ -79,47 +79,10 @@ class DIDisaster extends DIObject {
 
 		$num_args = func_num_args();
 		if ($num_args >= 2) {
-			$this->set('EventId', func_get_arg(1));
-			if ($num_args >= 3) {
-				$this->set('EventName', func_get_arg(1));
-				$this->set('EventDesc', func_get_arg(2));
-				$this->setIdByName($this->get('EventName'));
-			}
+			$prmDisasterId = func_get_arg(1);
+			$this->set('DisasterId', $prmDisasterId);
 		}
-	}
-	
-	public function setIdByName($prmEventName) {
-		$iReturn = 0;
-		$sQuery = "SELECT * FROM " . $this->getTableName() .
-		  " WHERE EventName='" . $prmEventName . "'";
-		$q = new Query();
-		if ($result = $q->query($sQuery)) {
-			if ($result->num_rows>0) {
-				// Local Event Found
-				while ($row = $result->fetch_object()) {
-					$this->set('EventId', $row->EventId);
-					$this->set('EventPreDefined', $row->EventPreDefined);
-					$this->set('EventCreationDate', $row->EventCreationDate);
-				} // while
-			} else {
-				// Search PreDefined Event
-				$sQuery = "SELECT * FROM DIEvent " . 
-				  " WHERE EventLangCode='" . $this->oSession->sRegionLangCode . "'" .
-				  "   AND (EventLocalName='" . $this->get('EventName') . "'" .
-				  "        OR EventDI6Name='" . $this->get('EventName') . "')";
-				if ($result = $q->query($sQuery)) {
-					while ($row = $result->fetch_object()) {
-						$this->set('EventId', $row->EventId);
-						$this->set('EventName', $row->EventName);
-						$this->set('EventDesc', $row->EventDesc);
-						$this->set('EventPreDefined', 1);
-						$this->set('EventCreationDate', $row->EventCreationDate);
-					}
-				}
-			}
-		}
-		return $iReturn;
-	} // function
+	} //__construct
 	
 	public function getDeleteQuery() {
 		$sQuery = "UPDATE " . $this->getTableName() . " SET RecordStatus='DELETED'" .
