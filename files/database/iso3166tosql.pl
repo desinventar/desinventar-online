46ec1b5acd95123733c7f6313f1c2b62bf92c025
jhcaiced <jhcaiced@9cf8542c-9828-0410-8776-d8f572d62bc1>
2009-07-07 00:33:33 +0000

    Added iso3166.txt and iso3166tosql.pl used to initialize the Country list in base.db
    git-svn-id: svn+ssh://devel.inticol.com/home/svn/di8/trunk@722 9cf8542c-9828-0410-8776-d8f572d62bc1

---
+++ files/database/iso3166tosql.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/perl
+# Process iso3166.txt file to load Country List into base.db
+# iso3166.txt can be downloaded from:
+# http://www.davros.org/misc/iso3166.txt
+# 2009-07-06 (jhcaiced) Jhon H. Caicedo <jhcaiced@desinventar.org>
+
+# This script's output is in UTF-8
+binmode(STDOUT, ':utf8');
+
+my $File = 'iso3166.txt';
+my $bProcess = 1;
+
+print "DELETE FROM Country;\n";
+open(F, '<', $File) || die "Can't open ISO Country List\n";
+while(<F>) {
+	chomp $_;
+	$line = $_;
+	if ($line =~ m/withdrawn/) {
+		$bProcess = 0;
+	}
+	if ($bProcess) {
+		if ($line =~ m/^[A-Z][A-Z] /) {
+			($CountryIsoCode,$rest) = ($line =~ m/^[A-Z][A-Z] ([A-Z][A-Z][A-Z]) [0-9][0-9][0-9] (.*)/);
+			($CountryName,$rest) = split(',',$rest);
+			$CountryIsoName = $rest . ' ' . $CountryName;
+			$CountryIsoName =~ s/,//g;
+			$CountryIsoName =~ s/^ +//g;
+			printf("INSERT INTO Country (CountryIso,CountryIsoName,CountryName) VALUES ('%s',\"%s\",\"%s\");\n", $CountryIsoCode, $CountryIsoName, $CountryName);
+		}
+	}
+}
+close(F);
