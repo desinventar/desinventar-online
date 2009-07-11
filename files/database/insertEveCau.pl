5e2e13eb1aec8a6ff3314fa0c5ded5ba1ac21e9d
Jhon H. Caicedo <jhcaiced@inticol.com>
2009-07-11 12:42:54 +0000

    Updated creation of Virtual Region
    git-svn-id: svn+ssh://devel.inticol.com/home/svn/di8/trunk@755 9cf8542c-9828-0410-8776-d8f572d62bc1

--- files/database/insertEveCau.pl
+++ files/database/insertEveCau.pl
@@ -32,7 +32,7 @@ $sth->execute();
 while (my $row = $sth->fetchrow_hashref) {
 	$sNow = strftime("%Y-%m-%d %H:%M:%S", gmtime); # ISO8601
 	$Query = sprintf("INSERT INTO %s VALUES " . 
-		"('%s','%s','%s','%s','%s',%d,%d,'%s','%s','%s','%s');",
+		"('%s','%s','%s','%s',\"%s\",%d,%d,'%s','%s','%s','%s');",
 		$sTableName,
 		$row->{$sTableName . 'Id'},
 		$sLangIsoCode,
