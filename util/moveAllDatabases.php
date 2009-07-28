9d553843c9e433fdfce5de82b2ee2a5bca300dee
jhcaiced <jhcaiced@9cf8542c-9828-0410-8776-d8f572d62bc1>
2009-07-28 13:10:54 +0000

    Completed moveAllDatabases.php used to convert between 8.1.9 and 8.2.0 databases
    git-svn-id: svn+ssh://devel.inticol.com/home/svn/di8/trunk@797 9cf8542c-9828-0410-8776-d8f572d62bc1

--- util/moveAllDatabases.php
+++ util/moveAllDatabases.php
@@ -11,18 +11,19 @@
 $_SERVER["DI8_WEB"] = '/home/jhcaiced/devel/di8/web';
 require_once('../web/include/loader.php');
 require_once('../web/include/diregion.class.php');
-$pdo = new PDO(
+require_once('../web/include/diregionauth.class.php');
+$dbh = new PDO(
    'mysql:host=localhost;dbname=di8db',
    '','',
    array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")
    );
 $RegionList = array();
-foreach($pdo->query("SELECT * FROM Region") as $row) {
+foreach($dbh->query("SELECT * FROM Region") as $row) {
 	$RegionList[] = $row['RegionUUID'];
 }
 $RegionList = array('COLOMBIA');
 foreach ($RegionList as $RegionUUID) {
-	foreach($pdo->query("SELECT * FROM Region WHERE RegionUUID='" . $RegionUUID . "'") as $row) {
+	foreach($dbh->query("SELECT * FROM Region WHERE RegionUUID='" . $RegionUUID . "'") as $row) {
 		$r = new DIRegion($us);
 		$r->set('CountryIso'     , $row['CountryIsoCode']);
 		$r->set('RegionLabel'    , $row['RegionLabel']);
@@ -46,13 +47,42 @@ foreach ($RegionList as $RegionUUID) {
 		$r->set('GeoLimitMinY'     , $row['GeoLimitMinY']);
 		$r->set('GeoLimitMaxY'     , $row['GeoLimitMaxY']);
 		$r->set('InfoCredits'      , $row['RegionCredits']);
-		print $r->get('RegionId') . "\n";
-		//$r->createRegionDB();
-		//$r->insert();
-		$data_dir = '/var/lib/desinventar';
-		$cmd = "./mysql2sqlite.pl -r " . $row['RegionUUID'] . " | sqlite3 " . $data_dir . "/" . $r->get('RegionId') . "/desinventar.db";
-		print "$cmd\n";
+	} //foreach
+	$RegionId = 'COL-1248786161-colombia_inventario_historico_de_desastres';
+	print $r->get('RegionId') . "\n";
+	$iReturn = $r->createRegionDB();
+	if ($iReturn > 0) {
+		$r->q->core->query("DELETE FROM Region WHERE RegionId='" . $RegionId . "'");
+		$r->insert();
 	}
-}
-$pdo = null;
+	
+	$data_dir = '/var/lib/desinventar';
+	$cmd = "./mysql2sqlite.pl -r " . $RegionUUID . " | sqlite3 " . $data_dir . "/" . $r->get('RegionId') . "/desinventar.db";
+	//system($cmd, $iReturn);
+	
+	if ($iReturn >= 0) {	
+		// Copy Cartography
+		$Query = 'SELECT * FROM ' . $RegionUUID . '_GeoLevel';
+		foreach($dbh->query($Query) as $geolevel) {
+			$CartoFile = $geolevel['GeoLevelLayerFile'];
+			foreach(array('dbf','shp','shx','prj') as $ext) {
+				$file0 = $CartoFile . '.' . $ext;
+				$file1 = $data_dir . '/carto/' . $RegionUUID . '/' . $file0;
+				$file2 = $data_dir . '/' . $r->get('RegionId') . '/' . $file0;
+				if (file_exists($file1)) {
+					copy($file1, $file2);
+				}
+			} //foreach
+		} //foreach
+	}
+	
+	if ($iReturn >= 0) {
+		$r->q->core->query("DELETE FROM RegionAuth WHERE RegionId='" . $RegionId . "'");
+		foreach($dbh->query("SELECT * FROM RegionAuth WHERE RegionUUID='" . $RegionUUID . "'") as $row) {
+			$a = new DIRegionAuth($us, $RegionId, $row['UserName'], $row['AuthKey'], $row['AuthValue'], $row['AuthAuxValue']);
+			$a->insert();
+		} //foreach
+	}
+} //foreach
+$dbh = null;
 </script>
---
+++ web/include/diregionauth.class.php
@@ -0,0 +1,43 @@
+<script language="php">
+/*
+ DesInventar - http://www.desinventar.org
+ (c) 1999-2009 Corporacion OSSO
+*/
+
+class DIRegionAuth extends DIObject {
+	public function __construct($prmSession) {
+		$this->sTableName   = "RegionAuth";
+		$this->sPermPrefix  = "ADMIN";
+		$this->sFieldKeyDef = "UserName/STRING," . 
+		                      "RegionId/STRING," .
+		                      "AuthKey/STRING";
+		$this->sFieldDef    = "AuthValue/STRING," .
+		                      "AuthAuxValue/STRING";
+
+		parent::__construct($prmSession);
+		$num_args = func_num_args();
+		$this->setConnection("core");
+		if ($num_args >= 2) {
+			$prmRegionId = func_get_arg(1);
+			if ($prmRegionId != '') {
+				$this->set('RegionId', $prmRegionId);
+			}
+			if ($num_args >= 3) {
+				$prmUserName = func_get_arg(2);
+				$this->set('UserName', $prmUserName);
+			}
+			if ($num_args >= 6) {
+				$prmAuthKey = func_get_arg(3);
+				$this->set('AuthKey', $prmAuthKey);
+				$prmAuthValue= func_get_arg(4);
+				$this->set('AuthValue', $prmAuthValue);
+				$prmAuthAuxValue = func_get_arg(5);
+				$this->set('AuthAuxValue', $prmAuthAuxValue);
+			}
+			$this->load();
+		}
+	} // __construct
+	
+} //class
+
+</script>
