ce79cd5d16e3581a7ac20a71057c0452ec965aad
Jhon H. Caicedo <jhcaiced@inticol.com>
2009-08-01 11:50:13 +0000

    Updated buildVirtualRegion.php script
    git-svn-id: svn+ssh://devel.inticol.com/home/svn/di8/trunk@821 9cf8542c-9828-0410-8776-d8f572d62bc1

--- util/buildVirtualRegion.php
+++ util/buildVirtualRegion.php
@@ -14,30 +14,46 @@
 	require_once(BASE . '/include/digeolevel.class.php');
 	require_once(BASE . '/include/digeocarto.class.php');
 	
-	$RegionItems = array('ARG-1248983179-argentina_inventario_historico_de_desastres',
-	                     'BOL-1248983224-bolivia_inventario_historico_de_desastres',
-	                     'COL-1248983239-colombia_inventario_historico_de_desastres',
-	                     'ECU-1248983677-ecuador_inventario_historico_de_desastres',
-	                     'VEN-1248984232-inventario_de_desastres_de_venezuela');
-	$RegionItems = array('BOL-1248983224-bolivia_inventario_historico_de_desastres');
+	$RegionId = '';
+	$bOption = 1;
+	if ($bOption) {
+		// Gran Chaco
+		$RegionItems = array('ARG-1248983690-argentina_gran_chaco',
+		                     'BOL-1248983699-bolivia_gran_chaco',
+		                     'PAR-1248983701-paraguay_gran_chaco');
+		//$RegionItems = array('BOL-1248983699-bolivia_gran_chaco');
+		$RegionId = 'DESINV-1249126759-subregion_gran_chaco';
+		$RegionLabel = 'Subregion Gran Chaco';
+	} else {
+		// CAN - SubRegion Andina
+		$RegionItems = array('ARG-1248983179-argentina_inventario_historico_de_desastres',
+		    	             'BOL-1248983224-bolivia_inventario_historico_de_desastres',
+	 	      	             'COL-1248983239-colombia_inventario_historico_de_desastres',
+	   	    	             'ECU-1248983677-ecuador_inventario_historico_de_desastres',
+	   	    	             'VEN-1248984232-inventario_de_desastres_de_venezuela');
+		//$RegionItems = array('BOL-1248983224-bolivia_inventario_historico_de_desastres');
+		$RegionId = 'DESINV-1249040429-can_subregion_andina';
+		$RegionLabel = 'CAN Subregion Andina';
+	}
 	// loader.php creates a UserSession when loaded...
 	$r = ERR_NO_ERROR;
 	$r = $us->login('diadmin','di8');
 	if ($r > 0) {
 		$o = new DIRegion($us);
-		$o->set('RegionLabel', 'CAN Subregion Andina');
-		$RegionId = $o->buildRegionId();
-		$RegionId = 'DESINV-1249040429-can_subregion_andina';
+		$o->set('RegionLabel', $RegionLabel);
+		if ($RegionId == '') {
+			$RegionId = $o->buildRegionId();
+		}
 		$o->set('RegionId'    , $RegionId);
 		$o->load();
 		$o->set('RegionStatus', CONST_REGIONACTIVE | CONST_REGIONPUBLIC);
 		$o->set('IsCRegion'   , TRUE);
+		$o->update();
 		$iReturn = $o->createRegionDB();
 		$us->open($RegionId);
 		foreach($RegionItems as $RegionItemId) {
-			fb($RegionItemId);
+			print $RegionItemId . "\n";
 			$o->addRegionItem($RegionItemId);
-			//fb($o->getRegionItemGeographyId($RegionItemId));
 		}
 		$o->updateMapArea();
 		$us->close();
