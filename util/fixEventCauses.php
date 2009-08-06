5ce97189f411431d7961e836a1d200e6f1e2572f
Jhon H. Caicedo <jhcaiced@inticol.com>
2009-08-06 10:32:21 +0000

    Added fixEventCauses.php script used to rebuild the default Event/Cause list in each database
    git-svn-id: svn+ssh://devel.inticol.com/home/svn/di8/trunk@836 9cf8542c-9828-0410-8776-d8f572d62bc1

---
+++ util/fixEventCauses.php
@@ -0,0 +1,31 @@
+#!/usr/bin/php
+<script language="php">
+/*
+  DesInventar - http://www.desinventar.org
+  (c) 1999-2009 Corporacion OSSO
+  
+  2009-08-06 Jhon H. Caicedo <jhcaiced@desinventar.org>
+ 
+  Fix Cause table, re-import predefined Causes
+  in each database.
+*/
+
+$_SERVER["DI8_WEB"] = '../web';
+require_once($_SERVER["DI8_WEB"] . '/include/loader.php');
+require_once(BASE . '/include/diregion.class.php');
+$q = new Query();
+$RegionList = array();
+foreach($q->core->query("SELECT * FROM Region") as $row) {
+	$RegionList[] = $row['RegionId'];
+}
+//DEBUG
+//$RegionList = array('BOL-1248983224-bolivia_inventario_historico_de_desastres');
+foreach ($RegionList as $RegionId) {
+	print $RegionId . "\n";
+	$q->setDBConnection($RegionId);
+	$r = new DIRegion($us, $RegionId);
+	$r->copyEvents($r->get('LangIsoCode'));
+	$r->copyCauses($r->get('LangIsoCode'));
+}
+$q = null;
+</script>
