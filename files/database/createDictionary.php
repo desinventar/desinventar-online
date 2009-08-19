49823f4cf1d9429d782e6cfe3a6d279df86aa51c
jhcaiced <jhcaiced@9cf8542c-9828-0410-8776-d8f572d62bc1>
2009-08-19 12:32:15 +0000

    Updated generation of Dictionary data, removed doc-db.sql and added script createDictionary to process the csv file saved from OOo
    git-svn-id: svn+ssh://devel.inticol.com/home/svn/di8/trunk@870 9cf8542c-9828-0410-8776-d8f572d62bc1

---
+++ files/database/createDictionary.php
@@ -0,0 +1,36 @@
+#!/usr/bin/php
+<script language="php">
+	$h = fopen('./doc-db.csv','r');
+	$i = 0;
+	print "DELETE FROM Dictionary;" . "\n";
+	print "DELETE FROM LabelGroup;" . "\n";
+	
+	while(!feof($h)) {
+	//while ($i < 3) {
+		$a = fgetcsv($h);
+		if (count($a) > 1) {
+			if ($i == 0) {
+				// Ignore First Line (Headers...)
+			} else {
+				$now = gmdate('c');
+				$query = sprintf('INSERT INTO LabelGroup VALUES ("%s","%s","%s","%s","%s","%s","%s");', 
+					$i,$a[0],$a[1],$a[2], $now,$now,$now);
+				print $query . "\n";
+				$langlist = array('spa','eng','fre','por');
+				$Index = 3; // First column with language information
+				foreach($langlist as $Lang) {
+					$query = sprintf('INSERT INTO Dictionary VALUES ("%s","%s","%s","%s","%s","%s","%s","%s","%s");',$i,$Lang,$a[$Index],escapeQuotes($a[$Index+4]),escapeQuotes($a[$Index+8]),escapeQuotes($a[$Index+12]),$now,$now,$now);
+					print $query . "\n";
+					$Index++;
+				} //foreach
+			} //if
+		} //if
+		$i++;
+	}
+	fclose($h);
+	
+	function escapeQuotes($prmValue) {
+		$prmValue = str_replace('"','',$prmValue);
+		return $prmValue;
+	}
+</script>
