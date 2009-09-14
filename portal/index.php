96272a552ff67745bf24cf9bb56d4592e5a961c4
mayandar <mayandar@9cf8542c-9828-0410-8776-d8f572d62bc1>
2009-09-14 16:34:49 +0000

    move desinventar directory to web_s root
    git-svn-id: svn+ssh://devel.inticol.com/home/svn/di8/trunk@965 9cf8542c-9828-0410-8776-d8f572d62bc1

---
+++ portal/index.php
@@ -0,0 +1,54 @@
+<script language="php">
+/*
+ DesInventar8 - http://www.desinventar.org
+ (c) 1999-2009 Corporacion OSSO
+*/
+
+// Load required Functions
+require_once('include/loader.php');
+
+$t->config_dir = 'include';
+
+$d = new Query();
+
+$t->assign ("DIver", "8.2.0.49");
+$t->assign ("DImode", MODE);
+// 2009-01-20 (jhcaiced) At this point, loader.php should have
+// created or loaded the UserSession in the $us variable
+$us->awake();
+$t->assign("stat", "on");
+
+// PAGES: Show Information for selected Page from top menu
+if (isset($_GET['p'])) {
+	if ($_GET['p'] == 'init') {
+		if (file_exists('default/index.php')) {
+			include("default/index.php");
+			exit();
+		} else {
+			$reglst = array();
+			$result = $d->core->query("SELECT RegionId, RegionLabel FROM Region WHERE RegionStatus=3 ORDER BY RegionLabel, RegionOrder");
+			while ($row = $result->fetch(PDO::FETCH_OBJ))
+				$reglst[$row->RegionId] = $row->RegionLabel;
+			$t->assign ("ctl_init", true);
+			$t->assign ("reglst", $reglst);
+		}
+	} else {
+		$t->assign ("ctl_pages", true);
+		$t->assign ("menu", $d->queryLabelsFromGroup('MainPage', $lg));
+		$t->assign ("page", $_GET['p']);
+	}
+} else {
+	// Default portal: init session and get country list
+	$t->assign ("menu", $d->queryLabelsFromGroup('MainPage', $lg));
+	// load languages available list
+	$t->assign ("lglst", $d->loadLanguages(1));
+	// load available countries with databases
+	$ctlst = array();
+	$result = $d->core->query("SELECT CountryIso FROM Region WHERE RegionStatus=3 GROUP BY CountryIso");
+	while ($row = $result->fetch(PDO::FETCH_OBJ))
+		$ctlst[] = $row->CountryIso;
+	$t->assign ("ctlst", $ctlst);
+}
+$t->display ("index.tpl");
+
+</script>
