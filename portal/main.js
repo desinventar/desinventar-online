d8a3b199e4f3bc3eaa97a45f02ed1d5ebde5cbf4
jhcaiced <jhcaiced@9cf8542c-9828-0410-8776-d8f572d62bc1>
2009-09-16 12:03:55 +0000

    Merge some changes to index.php used to build the basic database list for DesInventar
    git-svn-id: svn+ssh://devel.inticol.com/home/svn/di8/trunk@978 9cf8542c-9828-0410-8776-d8f572d62bc1

--- portal/main.js
+++ portal/main.js
@@ -2,4 +2,13 @@ $(function() {
 	//$("#desinventar_content").html('This is the content');
 	$("#desinventar_content").load('http://192.168.0.13/di8-devel/');
 	//$("#desinventar_content").load('http://192.168.0.13/di8-devel/?r=BOL-1250695036-bolivia_gran_chaco');
+	$("#searchdbquery").change(function() {
+		var Value = $(this).val();
+		$("#searchdbresult").load(
+			'/di8-devel/',
+			{ cmd: 'searchdb',
+			  searchdbquery: Value }
+		);
+	})
+	.change();
 });
