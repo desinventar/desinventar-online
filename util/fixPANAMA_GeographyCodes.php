05f42dea762e76ead4e649460f519a5f0dfbf82e
jhcaiced <jhcaiced@9cf8542c-9828-0410-8776-d8f572d62bc1>
2009-10-02 11:56:40 +0000

    Added DIRole class used to set/get Role for users in each database
    git-svn-id: svn+ssh://devel.inticol.com/home/svn/di8/trunk@1072 9cf8542c-9828-0410-8776-d8f572d62bc1

--- util/fixPANAMA_GeographyCodes.php
+++ util/fixPANAMA_GeographyCodes.php
@@ -22,34 +22,37 @@
 			}
 			$NewCode = $NewParent . $NewCode;
 			$codes[$OldCode] = $NewCode;
-			//printf('%d,"%s","%s","%s"' . "\n", $Level, $NewCode, $Name, $NewParent);
+			printf('%d,"%s","%s","%s"' . "\n", $Level, $NewCode, $Name, $NewParent);
 		}
 	}
 	fclose($fh);
+	
+	$bRun = 0;
+	if ($bRun) {
+		// Fix Geography Code in Disaster file
+		$fh = fopen($DisasterFile, 'r');
+		$line = fgets($fh); print $line;
+		$line = fgets($fh); print $line;
+		while (! feof($fh) ) {
+			$line = fgets($fh);
+			$i = strpos($line, ',', 0);
+			$j = strpos($line, ',', $i+1);
+			$j = strpos($line, ',', $j+1); $i=$j+2;
+			$j = strpos($line, ',', $j+1);
+			$OldCode = substr($line, $i, $j - $i - 1);
+			$l1 = substr($line, 0, $i);
+			$l2 = substr($line, $j-1);
 
-	// Fix Geography Code in Disaster file
-	$fh = fopen($DisasterFile, 'r');
-	$line = fgets($fh); print $line;
-	$line = fgets($fh); print $line;
-	while (! feof($fh) ) {
-		$line = fgets($fh);
-		$i = strpos($line, ',', 0);
-		$j = strpos($line, ',', $i+1);
-		$j = strpos($line, ',', $j+1); $i=$j+2;
-		$j = strpos($line, ',', $j+1);
-		$OldCode = substr($line, $i, $j - $i - 1);
-		$l1 = substr($line, 0, $i);
-		$l2 = substr($line, $j-1);
-
-		$NewCode = '';
-		if (isset($codes[$OldCode])) {
-			$NewCode = $codes[$OldCode];
-		} else {
-			print "ERROR : " . $OldCode . "\n";
-		}
-		print $l1 . $NewCode . $l2;
-	} //while
-	fclose($fh);
+			$NewCode = '';
+			if (isset($codes[$OldCode])) {
+				$NewCode = $codes[$OldCode];
+			} else {
+				print "ERROR : " . $OldCode . "\n";
+			}
+			print $l1 . $NewCode . $l2;
+		} //while
+		fclose($fh);
+	}
 		
 	function padCode($value, $len) {
 		while (strlen($value) < $len) {
