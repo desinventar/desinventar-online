cc6d7ee48eda6bae0a7fd87c6fc973c3a4c16a9e
jhcaiced <jhcaiced@9cf8542c-9828-0410-8776-d8f572d62bc1>
2009-12-16 16:33:44 +0000

    Updated jQuery actions in useradmin.js
    git-svn-id: svn+ssh://devel.inticol.com/home/svn/di8/trunk@1324 9cf8542c-9828-0410-8776-d8f572d62bc1

--- web/js/useradmin.js
+++ web/js/useradmin.js
@@ -1,14 +1,34 @@
 
 function onReadyUserAdmin() {
-	jQuery("#userListTable tr:odd").addClass("normal");
-	jQuery("#userListTable tr:even").addClass("under");
-	jQuery("#userListTable tr").mouseover(function() {
+	// Create table stripes
+	jQuery("#tblUserList tr:odd").addClass("normal");
+	jQuery("#tblUserList tr:even").addClass("under");
+	// Change background color of row under mouse
+	jQuery("#tblUserList tr").mouseover(function() {
 		jQuery(this).addClass('highlight');
 	});
-	jQuery("#userListTable tr").mouseout(function() {
+	jQuery("#tblUserList tr").mouseout(function() {
 		jQuery(this).removeClass('highlight');
 	});
-	jQuery("#userListTable tr").click(function() {
-		alert('click on row');
+	// When selecting a row, start editing data...
+	jQuery("#tblUserList tr").click(function() {
+		jQuery("#divUserEdit").show();
+	});
+	// Add new User...
+	jQuery("#btnUserAdd").click(function() {
+		jQuery("#divUserEdit").show();
+		//onclick="setUserPA('','','','','','','1'); $('cmd').value='insert'; $('UserPasswd').disabled=true;"
+	});
+	// Finish edit, validate form and send data...
+	jQuery("#btnUserEditSubmit").click(function() {
+		alert('submit');
+	});
+	jQuery("#btnUserEditCancel").click(function() {
+		jQuery("#divUserEdit").hide();
+		//onClick="$('userpaaddsect').style.display='none'; uploadMsg('');"
 	});
 };
+
+function clearUserEditForm() {
+};
+
