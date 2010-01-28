8339f985122cf52cce139d854417e58139c71083
Jhon H. Caicedo <jhcaiced@gmail.com>
2010-01-28 10:16:09 -0500

    Added web/test to the ignored files

--- web/test/test.php
+++ web/test/test.php
@@ -1,5 +1,6 @@
 <?
 	require_once('../include/loader.php');
-	$pass = generatePassword();
-	print_r($pass) . "<br />\n";
+	$pass = generatePasswd();
+	print($pass) . "<br />\n";
+	print md5('di8welcome') . "<br />\n";
 ?>
