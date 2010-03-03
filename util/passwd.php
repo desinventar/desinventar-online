cc224023d47f7d19f39a82a78354f05b7d954a37
Jhon H. Caicedo <jhcaiced@gmail.com>
2010-03-03 08:42:29 -0500

    Added small script to show the default passwd

---
+++ util/passwd.php
@@ -0,0 +1,14 @@
+#!/usr/bin/php -d session.save_path='/tmp'
+<script language="php">
+	require_once('../web/include/loader.php');
+	require_once(BASE . '/include/dievent.class.php');
+	require_once(BASE . '/include/dicause.class.php');
+	require_once(BASE . '/include/digeography.class.php');
+	require_once(BASE . '/include/didisaster.class.php');
+	require_once(BASE . '/include/dieedata.class.php');
+	require_once(BASE . '/include/diimport.class.php');
+	
+	//$pass = generatePasswd();
+	//print($pass) . "<br />\n";
+	print 'di8welcome => ' . md5('di8welcome') . "\n";
+</script>
