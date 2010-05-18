44bc1d6ef2fc27ce56dfbc5be9b7b9edd0ec1eb7
Jhon H. Caicedo <jhcaiced@gmail.com>
2010-05-18 06:02:20 -0500

    Added Honduras script

---
+++ util/import_HND.php
@@ -0,0 +1,25 @@
+#!/usr/bin/php -d session.save_path='/tmp'
+<script language="php">
+	require_once('../web/include/loader.php');
+	require_once(BASE . '/include/dievent.class.php');
+	require_once(BASE . '/include/dicause.class.php');
+	require_once(BASE . '/include/digeography.class.php');
+	require_once(BASE . '/include/didisaster.class.php');
+	require_once(BASE . '/include/dieedata.class.php');
+	require_once(BASE . '/include/diimport.class.php');
+	require_once(BASE . '/include/diregion.class.php');
+	
+	$RegionId = 'HND-1273785384-honduras_inventario_de_desastres_';
+	$us->login('diadmin','di8');
+	$us->open($RegionId);
+	//$r = new DIRegion($us, $RegionId);
+	//$r->copyEvents('spa');
+	//$r->copyCauses('spa');
+	//$i = new DIImport($us);
+	//$a = $i->importFromCSV('/tmp/ho_event.csv', DI_EVENT, true, 0);
+	//$a = $i->importFromCSV('/tmp/ho_cause.csv', DI_CAUSE, true, 0);
+	//$a = $i->importFromCSV('/tmp/ho_geography.csv', DI_GEOGRAPHY, true, 0);
+	//$a = $i->importFromCSV('/tmp/ho_disaster.csv', DI_DISASTER, true, 0);
+	$us->close();
+	$us->logout();
+</script>
