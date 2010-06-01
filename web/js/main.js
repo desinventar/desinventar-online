fd823e7585de206a7b78e981b019de2492c50660
Jhon H. Caicedo <jhcaiced@inticol.com>
2010-06-01 11:00:30 -0500

    Multiple Fixes to ThematicMap methods

--- web/js/main.js
+++ web/js/main.js
@@ -1,17 +1,22 @@
 function onReadyMain() {
 	jQuery('#DC').submit(function() {
 		var myURL = jQuery(this).attr('action');
-		jQuery.post(myURL,
-			jQuery(this).serialize(),
-			function(data) {
-				jQuery('#dcr').html('');
-				jQuery('#dcr').html(data);
-				onReadyData();
-				onReadyGraphic();
-				onReadyThematicMap();
-				onReadyStatistic();
-			}
-		);
-		return false;
+		var myCmd = jQuery('#prmCommand').val();
+		if (myCmd == 'cmdMapSave') {
+			return true;
+		} else {
+			jQuery.post(myURL,
+				jQuery(this).serialize(),
+				function(data) {
+					jQuery('#dcr').html('');
+					jQuery('#dcr').html(data);
+					onReadyData();
+					onReadyGraphic();
+					onReadyThematicMap();
+					onReadyStatistic();
+				}
+			);
+			return false;
+		}
 	});
 }
