8bfeb66a31d7e70b41639606cfed3714842a5d53
Jhon H. Caicedo <jhcaiced@gmail.com>
2010-06-10 09:33:10 -0500

    Create block_southamerica.tpl to handle the display of the portal map

--- portal/gar.php
+++ portal/gar.php
@@ -4,9 +4,6 @@
  (c) 1998-2010 Corporación OSSO
 */
 	require_once('../web/include/loader.php');
-
-	// 2009-09-23 (jhcaiced) Choose which ISDR logo to use...
-	$t->assign('imgCANMap', 'subreg_can_' . $lg . '.png');
 	$t->display ("index.tpl");
 	//phpInfo();
 </script>
