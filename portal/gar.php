0441cead6af6f5134dcb74779f3fc0c7c52b02fa
Jhon H. Caicedo <jhcaiced@gmail.com>
2010-06-10 08:26:35 -0500

    Introduce gar-isdr alias and update portal to work with that alias

---
+++ portal/gar.php
@@ -0,0 +1,15 @@
+<script language="php">
+/*
+ DesInventar8 - http://www.desinventar.org
+ (c) 1998-2010 Corporación OSSO
+*/
+	require_once('../web/include/loader.php');
+
+	// 2009-09-23 (jhcaiced) Choose which ISDR logo to use...
+	$isdrlogo = 'logo-isdr.gif';
+	if ($lg == 'spa') { $isdrlogo = 'logo-eird.gif'; }
+	$t->assign("isdrlogo", $isdrlogo);
+	$t->assign('imgCANMap', 'subreg_can_' . $lg . '.png');
+	$t->display ("index.tpl");
+	//phpInfo();
+</script>
