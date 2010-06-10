3c1b5f4fdfa3c14ee588b6ca0c40a33e95d5edc1
Jhon H. Caicedo <jhcaiced@gmail.com>
2010-06-09 19:21:28 -0500

    Updated JSVERSION to force reload of new version

--- web/include/loader.php
+++ web/include/loader.php
@@ -10,7 +10,7 @@ if (! isset($_SERVER["DI8_WEB"])) {
 
 // This is the version of the software
 define('VERSION', '8.2.0.80');
-define('JSVERSION', '2010-06-04.04');
+define('JSVERSION', '2010-06-09.01');
 
 // 2009-07-22 (jhcaiced) Adapted Configuration and Startup for 
 // using with PHP Command Line 
