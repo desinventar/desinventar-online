8d80ba939ecfd489033432b23142e6a4a541b45f
Jhon H. Caicedo <jhcaiced@gmail.com>
2010-06-10 08:26:27 -0500

    Introduce gar-isdr alias and update portal to work with that alias

--- web/include/loader.php
+++ web/include/loader.php
@@ -4,8 +4,8 @@
  (c) 1998-2010 Corporacion OSSO
 */
 // 2009-09-16 (jhcaiced) Autoconfigure software directory
-if (! isset($_SERVER["DI8_WEB"])) {
-	$_SERVER["DI8_WEB"] = dirname(dirname(__FILE__));
+if (! isset($_SERVER['DI8_WEB'])) {
+	$_SERVER['DI8_WEB'] = dirname(dirname(__FILE__));
 }
 
 // This is the version of the software
@@ -14,80 +14,80 @@ define('JSVERSION', '2010-06-09.01');
 
 // 2009-07-22 (jhcaiced) Adapted Configuration and Startup for 
 // using with PHP Command Line 
-if (isset($_SERVER["HTTP_HOST"])) {
+if (isset($_SERVER['HTTP_HOST'])) {
 	// Online Modes (HTTP)
-	if (isset($_SERVER["WINDIR"])) {
+	if (isset($_SERVER['WINDIR'])) {
 		// Running on a Windows Server
-		define('MODE', "online");
+		define('MODE', 'online');
 		define('ARCH', 'WINDOWS');
-		define('MAPSERV', "mapserv.exe");
+		define('MAPSERV', 'mapserv.exe');
 		// 2009-05-01 (jhcaiced) Read Registry to obtain MS4W 
 		//                       installation path	
-		$shell = new COM("WScript.Shell") or die("Requires Windows Scripting Host");
-		$Install_Dir = $shell->RegRead("HKEY_LOCAL_MACHINE\\Software\\OSSO\\DesInventar8\Install_Dir");		
-		define("SMARTYDIR", $Install_Dir . "/ms4w/apps/Smarty/libs");
-		define("JPGRAPHDIR", $Install_Dir . "/ms4w/apps/jpgraph/src");
-		define("TEMP", $Install_Dir . "/tmp");
+		$shell = new COM('WScript.Shell') or die('Requires Windows Scripting Host');
+		$Install_Dir = $shell->RegRead('HKEY_LOCAL_MACHINE\\Software\\OSSO\\DesInventar8\Install_Dir');		
+		define('SMARTYDIR', $Install_Dir . '/ms4w/apps/Smarty/libs');
+		define('JPGRAPHDIR', $Install_Dir . '/ms4w/apps/jpgraph/src');
+		define('TEMP', $Install_Dir . '/tmp');
 		// MS4W doesn't load the gd extension by default, so we do here now...
 		if (!extension_loaded( 'gd' )) {
 			//dl( 'php_gd2.'.PHP_SHLIB_SUFFIX);
 		}
-		if (! isset($_SERVER["DI8_WEB"])) {
+		if (! isset($_SERVER['DI8_WEB'])) {
 			$_SERVER['DI8_WEB'] = $Install_Dir . '/ms4w/Apache/htdocs';
 		}
 		$_SERVER['DI8_WWWDIR'] = $Install_Dir . '/www';
 		$_SERVER['DI8_DATADIR'] = $Install_Dir . '/data';
 		$_SERVER['DI8_CACHEDIR'] = $Install_Dir . '/tmp';
-		define("FONTSET" , $Install_Dir . '/data/main/fontswin.txt');	
+		define('FONTSET' , $Install_Dir . '/data/main/fontswin.txt');	
 	} else {
 		// Running on a Linux Server
-		define('MODE', "online");
-		define('ARCH', "LINUX");
-		define('MAPSERV', "mapserv");
-		define("SMARTYDIR", "/usr/share/php/Smarty");
-		define("TEMP", "/tmp");
-		define("JPGRAPHDIR", "/usr/share/php/jpgraph");
-		define("FONTSET" , "/usr/share/fonts/liberation/fonts.txt");
-		if (! isset($_SERVER["DI8_WEB"])) {
-			$_SERVER["DI8_WEB"]      = "/usr/share/desinventar-8.2/web";
+		define('MODE', 'online');
+		define('ARCH', 'LINUX');
+		define('MAPSERV', 'mapserv');
+		define('SMARTYDIR', '/usr/share/php/Smarty');
+		define('TEMP', '/tmp');
+		define('JPGRAPHDIR', '/usr/share/php/jpgraph');
+		define('FONTSET' , '/usr/share/fonts/liberation/fonts.txt');
+		if (! isset($_SERVER['DI8_WEB'])) {
+			$_SERVER['DI8_WEB']      = '/usr/share/desinventar-8.2/web';
 		}
-		$_SERVER["DI8_WWWDIR"]   = "/var/www/desinventar-8.2";
-		$_SERVER["DI8_DATADIR"]  = "/var/lib/desinventar-8.2";
-		$_SERVER["DI8_CACHEDIR"] = "/var/cache/Smarty/di8";
+		$_SERVER['DI8_WWWDIR']   = '/var/www/desinventar-8.2';
+		$_SERVER['DI8_DATADIR']  = '/var/lib/desinventar-8.2';
+		$_SERVER['DI8_CACHEDIR'] = '/var/cache/Smarty/di8';
 	}
 } else {
 	// Running a Command Line Script
-	define('MODE', "command");
-	$_SERVER["DI8_WWWDIR"]   = "/var/www/desinventar-8.2";
-	$_SERVER["DI8_DATADIR"]  = "/var/lib/desinventar-8.2";
-	$_SERVER["DI8_CACHEDIR"] = "/var/cache/Smarty/di8";
+	define('MODE', 'command');
+	$_SERVER['DI8_WWWDIR']   = '/var/www/desinventar-8.2';
+	$_SERVER['DI8_DATADIR']  = '/var/lib/desinventar-8.2';
+	$_SERVER['DI8_CACHEDIR'] = '/var/cache/Smarty/di8';
 	define('TEMP', '/tmp');
 }
-define("BASE"    , $_SERVER["DI8_WEB"]);
-define("WWWDIR"  , $_SERVER["DI8_WWWDIR"]);
-define("WWWDATA" , "/desinventar-8.2-data");
-define("WWWURL"  , "/");
-define("DATADIR" , $_SERVER["DI8_DATADIR"]);
-define("CACHEDIR", $_SERVER["DI8_CACHEDIR"]);
-define("VAR_DIR" , DATADIR);
-define("TMP_DIR" , TEMP);
-define("SMTY_DIR", CACHEDIR); // Smarty temp dir
-define("TMPM_DIR", CACHEDIR); // Mapserver temp dir
-require_once(BASE . "/include/fb.php");
-require_once(BASE . "/include/usersession.class.php");
-require_once(BASE . "/include/diobject.class.php");
+define('BASE'    , $_SERVER['DI8_WEB']);
+define('WWWDIR'  , $_SERVER['DI8_WWWDIR']);
+define('WWWDATA' , '/desinventar-8.2-data');
+define('WWWURL'  , '/');
+define('DATADIR' , $_SERVER['DI8_DATADIR']);
+define('CACHEDIR', $_SERVER['DI8_CACHEDIR']);
+define('VAR_DIR' , DATADIR);
+define('TMP_DIR' , TEMP);
+define('SMTY_DIR', CACHEDIR); // Smarty temp dir
+define('TMPM_DIR', CACHEDIR); // Mapserver temp dir
+require_once(BASE . '/include/fb.php');
+require_once(BASE . '/include/usersession.class.php');
+require_once(BASE . '/include/diobject.class.php');
 require_once(BASE . '/include/diuser.class.php');
-require_once(BASE . "/include/query.class.php");
-require_once(BASE . "/include/constants.php");
-require_once(BASE . "/include/common.php");
-require_once(BASE . "/include/xml2array.php");
+require_once(BASE . '/include/query.class.php');
+require_once(BASE . '/include/constants.php');
+require_once(BASE . '/include/common.php');
+require_once(BASE . '/include/xml2array.php');
 
 /* SETTINGS */
 $time_start = microtime_float();
 $SessionId = uuid();
-if (MODE != "command") {
+if (MODE != 'command') {
 	// Session Management
-	session_name("DI8SESSID");
+	session_name('DI8SESSID');
 	session_start();
 	$SessionId = session_id();
 }
@@ -97,13 +97,16 @@ if (MODE != "command") {
 $us = new UserSession($SessionId);
 $us->awake();
 
-if (MODE != "command") {
+if (MODE != 'command') {
 	error_reporting(E_ALL && ~E_NOTICE);
 	header('Content-Type: text/html; charset=UTF-8');
-	define("DEFAULT_CHARSET", 'UTF-8');
+	define('DEFAULT_CHARSET', 'UTF-8');
 
 	$confdir = dirname($_SERVER['SCRIPT_FILENAME']) . '/conf';
 	$templatedir = dirname($_SERVER['SCRIPT_FILENAME']) . '/templates';
+	fb($confdir);
+	fb($templatedir);
+	
 	/* Smarty configuration */
 	require_once(SMARTYDIR . '/Smarty.class.php');
 	/* SMARTY template */
@@ -132,8 +135,8 @@ if (MODE != "command") {
 	if ($lg == 'pt') { $lg = 'por'; }
 
 	$_SESSION['lang'] = $lg;
-	$t->assign ("lg", $lg);
-	$t->assign("lang"  , $lg);
+	$t->assign ('lg', $lg);
+	$t->assign('lang'  , $lg);
 
 	// 2010-05-25 (jhcaiced) Handle the versioning of js files, used to force refresh of
 	// these files when doing changes.
@@ -142,13 +145,21 @@ if (MODE != "command") {
 
 	// Configure DI8 (web) application location
 	$desinventarURL = WWWURL;
-	if (isset($_SERVER["REDIRECT_DI8_URL"])) {
-		$_SERVER["DI8_URL"] = $_SERVER["REDIRECT_DI8_URL"];
+	if (isset($_SERVER['REDIRECT_DI8_URL'])) {
+		$_SERVER['DI8_URL'] = $_SERVER['REDIRECT_DI8_URL'];
 	}
-	$desinventarURL = $_SERVER["DI8_URL"];
+	$desinventarURL = $_SERVER['DI8_URL'];
+
+	// Configure DI8 (portal) application location
+	$desinventarURLPortal = WWWURL;
+	if (isset($_SERVER['REDIRECT_DI8_PORTAL'])) {
+		$_SERVER['DI8_PORTAL'] = $_SERVER['REDIRECT_DI8_PORTAL'];
+	}
+	$desinventarURLPortal = $_SERVER['DI8_PORTAL'];
 
 	// General Information (common to portal/app)
 	$t->assign('desinventarURL'         , $desinventarURL);
+	$t->assign('desinventarURLPortal'   , $desinventarURLPortal);
 	$t->assign('desinventarVersion'     , VERSION);
 	$t->assign('desinventarLang'        , $lg);
 	$t->assign('desinventarUserId'      , $us->UserId);
