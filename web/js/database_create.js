483703ad47cfa80c6bb4a019cf0cd9bf577d78f9
Jhon H. Caicedo <jhcaiced@inticol.com>
2010-06-22 14:59:54 -0500

    Added a Cancel button to the upload process

--- web/js/database_create.js
+++ web/js/database_create.js
@@ -1,11 +1,17 @@
 function onReadyDatabaseCreate() {
+	// Load Images in bb buttons
+	jQuery('.bb').each(function(index, Element) {
+		jQuery(this).css('background','url(' + jQuery(this).attr('img') + ')');
+	});
+	
+	// Create a SWFUpload instance and attach events...
 	jQuery('#divDBImport-Control').swfupload({
 		upload_url: 'index.php',
 		post_params: {cmd : 'fileupload'},
-		file_size_limit : "10240",
+		file_size_limit : "204800",
 		file_types : "*.*",
 		file_types_description : "All Files",
-		file_upload_limit : "1",
+		file_upload_limit : "0",
 		//file_post_name: 'file',
 		flash_url: 'external/swfupload/swfupload.swf',
 		button_image_url : 'external/swfupload/XPButtonUploadText_61x22.png',
@@ -17,14 +23,26 @@ function onReadyDatabaseCreate() {
 	})
 	.bind('fileQueued', function(event, file) {
 		// start the upload since it's queued
+		jQuery('#progressMark').css('width', '0px');
+		jQuery('#btnDBImportCancel').attr('file_id', file.id);
 		jQuery(this).swfupload('startUpload');
+	})
+	.bind('uploadProgress', function(event, file, bytesLoaded) {
+		// Show Progress
+		var percentage = Math.round((bytesLoaded/file.size)*100);
+		jQuery('#progressMark').css('width', percentage + '%');
 	})
 	.bind('uploadSuccess', function(event, file, serverData) {
 		var data = eval('(' + serverData + ')');
-		//alert(data.Status);
 	})
 	.bind('uploadComplete', function(event, file) {
 		// upload has completed, lets try the next one in the queue
 		jQuery(this).swfupload('startUpload');
+	});
+	
+	jQuery('#btnDBImportCancel').click(function() {
+		var swfu = jQuery.swfupload.getInstance('#divDBImport-Control');
+		swfu.cancelUpload(jQuery(this).attr('file_id'));
+		jQuery('#progressMark').css('width', '0px');
 	});
 }
--- web/templates/database_create.tpl
+++ web/templates/database_create.tpl
@@ -3,6 +3,11 @@
 	<h2>Crear Base de Datos{-#msgDBCreate_Title#-}</h2>
 	<div id="divDBImport-Control">
 		<input type="button" id="button">
-		<ol id="log"></ol>
+		<li>
+			<div id="progressBar" style="width:120px;height:6px;background-color:#dddddd">
+				<div id="progressMark" style="width:0px;height:6px;background-color:#0000ff"></div>
+			</div>
+		</li>
+		<input type="button" id="btnDBImportCancel" class="bb" img="images/cancelicon.png" />
 	</div>
 </div>
