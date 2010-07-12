867ca12563c60dc01e208da176095566afb8edaa
Jhon H. Caicedo <jhcaiced@inticol.com>
2010-07-12 18:31:10 -0500

    Updated spec file, requirements are php-xml for reading info.xml

--- web/thematicmap.php
+++ web/thematicmap.php
@@ -211,7 +211,7 @@ if (isset($post['_M+cmd'])) {
 	//$t->assign('mapinfoimg', WWWDATA . '/' . $MapInfoImg);
 	
 	$mapfile = str_replace('\\', '/', $m->filename());
-	$worldmap = str_replace('\\','/', MAPDIR . "/world_adm0.map");
+	$worldmap = str_replace('\\','/', MAPDIR . "/worldmap.map");
 	$legend = "/cgi-bin/". MAPSERV ."?map=" . rawurlencode($mapfile) . "&SERVICE=WMS&VERSION=1.1.1".
 				"&REQUEST=getlegendgraphic&LAYER=". substr($myly, 0, 12) ."&FORMAT=image/png";
 	$t->assign('legend', $legend);	
