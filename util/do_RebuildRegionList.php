a186215ef701e6ca83daa521b90ff1735f8a0d69
Jhon H. Caicedo <jhcaiced@asimov.tuq.inticol.com>
2010-08-02 07:01:12 -0500

    Create function to rebuild core.Region table from the databases on directory

--- util/do_RebuildRegionList.php
+++ util/do_RebuildRegionList.php
@@ -3,12 +3,30 @@
 	require_once('../web/include/loader.php');
 	
 	if ($handle = opendir(DBDIR)) {
-		while ( ($file = readdir($handle)) == true) {
-			$DBDir = DBDIR . '/' . $file;
-			if (is_dir($DBDir)) {
-				echo $file . "\n";
+		$dirlist = array();
+		while ( ($diritem = readdir($handle)) == true) {
+			if ($diritem != '.' && $diritem != '..') {
+				$DBDir = DBDIR . '/' . $diritem;
+				if (is_dir($DBDir)) {
+					if (file_exists($DBDir . '/desinventar.db')) {
+						array_push($dirlist, $diritem);
+					}
+				}
 			}
 		}
 	}
 	closedir($handle);
+		
+	foreach($dirlist as $diritem) {
+		echo $diritem . "\n";
+		/*
+			$results[] = $file;
+			$RegionId = $file;
+
+			// Create a new entry using data from Info table
+			$r = new DIRegion($us, $RegionId);
+			$r->loadInfo();
+			$r->insert();
+		*/
+	}
 </script>
