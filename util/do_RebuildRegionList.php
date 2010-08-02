090241b96d04a7f08c929ad7ec99ebb661fdbfb9
Jhon H. Caicedo <jhcaiced@inticol.com>
2010-08-02 09:07:39 -0500

    Updating queries to use PDOStatements instead of direct query method, this should take care of special chars in data

--- util/do_RebuildRegionList.php
+++ util/do_RebuildRegionList.php
@@ -1,6 +1,7 @@
 #!/usr/bin/php
 <script language="php">
 	require_once('../web/include/loader.php');
+	require_once(BASE . '/include/diregion.class.php');
 	
 	if ($handle = opendir(DBDIR)) {
 		$dirlist = array();
@@ -16,17 +17,21 @@
 		}
 	}
 	closedir($handle);
+	
+	//$dirlist = array('PAR-1250695038-paraguay_gran_chaco');
+	$dirlist = array('GAR-ISDR-2011_COL');
 		
+	// Remove Previous Entries
+	$query = "DELETE FROM Region";
+	$us->q->core->query($query);
+	
+	// Process list to add new Items
 	foreach($dirlist as $diritem) {
 		echo $diritem . "\n";
-		/*
-			$results[] = $file;
-			$RegionId = $file;
-
-			// Create a new entry using data from Info table
-			$r = new DIRegion($us, $RegionId);
-			$r->loadInfo();
-			$r->insert();
-		*/
+		$RegionId = $diritem;
+		// Create a new entry using data from Info table
+		$r = new DIRegion($us, $RegionId);
+		$r->loadInfo();
+		$r->insert();
 	}
 </script>
