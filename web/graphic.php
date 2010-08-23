7d83751541461aa97699489391e00de171c3bcc6
Jhon H. Caicedo <jhcaiced@inticol.com>
2010-08-23 13:43:07 -0500

    Added timestamp to graph filename to avoid caching in browser

--- web/graphic.php
+++ web/graphic.php
@@ -30,15 +30,15 @@ $t->assign('RegionLabel', $RegionLabel);
 fixPost($post);
 // load levels to display in totalizations
 foreach ($us->q->loadGeoLevels('', -1, false) as $k=>$i) {
-	$st["GraphGeographyId_". $k] = array($i[0], $i[1]);
+	$st['GraphGeographyId_'. $k] = array($i[0], $i[1]);
 } //foreach
 
 $dic = array_merge(array(), $st);
 $dic = array_merge($dic, $us->q->queryLabelsFromGroup('Graph', $lg));
 $dic = array_merge($dic, $us->q->queryLabelsFromGroup('Effect', $lg));
 $dic = array_merge($dic, $us->q->queryLabelsFromGroup('Sector', $lg));
-$dic = array_merge($dic, $us->q->getEEFieldList("True"));
-//$t->assign ("dic", $dic);
+$dic = array_merge($dic, $us->q->getEEFieldList('True'));
+//$t->assign ('dic', $dic);
 $prmGraph = $post['prmGraph'];
 $GraphCommand = $prmGraph['Command'];
 if ($GraphCommand != '') {
@@ -47,7 +47,7 @@ if ($GraphCommand != '') {
 	$sqc = $us->q->genSQLSelectCount($qd);	
 	$c   = $us->q->getresult($sqc);
 	$NumRecords = $c['counter'];
-	$t->assign ("NumRecords", $NumRecords);
+	$t->assign ('NumRecords', $NumRecords);
 
 	$prmGraph['SubType'] = (int)$prmGraph['SubType'];
 	if ($prmGraph['SubType'] == GRAPH_HISTOGRAM_TEMPORAL) {
@@ -72,19 +72,19 @@ if ($GraphCommand != '') {
 	$post['prmGraph'] = $prmGraph;
 	// Process Configuration options to Graphic
 	$ele = array();
-	foreach (explode("|", $prmGraph['VarList']) as $itm) {
-		if ($itm == "D.DisasterBeginTime") {
+	foreach (explode('|', $prmGraph['VarList']) as $itm) {
+		if ($itm == 'D.DisasterBeginTime') {
 			// Histogram
 			if (isset($prmGraph['Stat']) && strlen($prmGraph['Stat'])>0) {
-				$ele[] = $prmGraph['Stat'] ."|". $itm;
+				$ele[] = $prmGraph['Stat'] .'|'. $itm;
 			} else {
-				$ele[] = $prmGraph['Period'] ."|". $itm;
+				$ele[] = $prmGraph['Period'] .'|'. $itm;
 			}
-		} elseif (substr($itm, 2, 11) == "GeographyId") {
-			$gl = explode("_", $itm);
-			$ele[] = $gl[1] ."|". $gl[0];// "0|$itm"; 
+		} elseif (substr($itm, 2, 11) == 'GeographyId') {
+			$gl = explode('_', $itm);
+			$ele[] = $gl[1] .'|'. $gl[0];// '0|$itm'; 
 		} else {
-			$ele[] = "|". $itm;
+			$ele[] = '|'. $itm;
 		}
 	} // foreach
 
@@ -140,7 +140,7 @@ if ($GraphCommand != '') {
 	$dislist = $us->q->getassoc($sql);
 	if (!empty($dislist)) {
 		// Process results data
-		$dl = $us->q->prepareList($dislist, "GRAPH");
+		$dl = $us->q->prepareList($dislist, 'GRAPH');
 		$gl = array();
 		// Translate Labels to Selected Language
 		foreach ($dl as $k=>$i) {
@@ -160,14 +160,14 @@ if ($GraphCommand != '') {
 		}
 		// Construct Graphic Object and Show Page
 		$g = new Graphic($post, $gl);
-		$sImageURL  = WWWDATA . "/graphs/di8graphic_". session_id() . ".png";
-		$sImageFile = WWWDIR . "/graphs/di8graphic_". session_id() . ".png";
+		$sImageURL  = WWWDATA . '/graphs/di8graphic_'. session_id() . '_' . time() . '.png';
+		$sImageFile = WWWDIR  . '/graphs/di8graphic_'. session_id() . '_' . time() . '.png';
 		// Wrote graphic to file
 		$g->Stroke($sImageFile);
-		if ($GraphCommand == "export") {
+		if ($GraphCommand == 'export') {
 			// Export Graph as a Image
-			header("Content-type: Image/png");
-			header("Content-Disposition: attachment; filename=DI8_". str_replace(" ", "", $RegionLabel) ."_Graphic.png");
+			header('Content-type: Image/png');
+			header('Content-Disposition: attachment; filename=DI8_'. str_replace(' ', '', $RegionLabel) .'_Graphic.png');
 			header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
 			header('Last-Modified: '. gmdate('D, d M Y H:i:s') .' GMT');
 			header('Cache-Control: no-store, no-cache, must-revalidate');
@@ -177,13 +177,13 @@ if ($GraphCommand != '') {
 			exit();
 		} else {
 			// Display Graph in Browser
-			$t->assign ("qdet", $us->q->getQueryDetails($dic, $post));
-			$t->assign ("image", "$sImageURL?". rand(1,3000));
-			$t->assign ("ctl_showres", true);
+			$t->assign('qdet', $us->q->getQueryDetails($dic, $post));
+			$t->assign('image', $sImageURL . '?'. rand(1,3000));
+			$t->assign('ctl_showres', true);
 		} //if
 	} // if
 } //if
 
-$t->display("graphic.tpl");
+$t->display('graphic.tpl');
 
 </script>
