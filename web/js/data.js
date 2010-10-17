3ea04a556358b9f38f98fbd79eb28e9127f588bf
Jhon H. Caicedo <jhcaiced@inticol.com>
2010-10-17 08:14:36 -0500

    Fixed navigation in View Data to avoid multiple clicks in navigation

--- web/js/data.js
+++ web/js/data.js
@@ -40,40 +40,44 @@ function onReadyData() {
 }
 
 function doDataDisplayPage(page) {
-	var mypag = page;
-	var now = parseInt(jQuery('#DataCurPage').val());
-	if (page == 'prev') {
-		mypag = now - 1;
-	} else if (page == 'next') {
-		mypag = now + 1;
-	}
-	var NumberOfPages = jQuery('#prmDataPageCount').val();
-	if ((mypag < 1) || (mypag > NumberOfPages)) {
-		// Out of Range Page
-	} else {
-		jQuery('#DataCurPage').val(mypag);
-		var RegionId = jQuery('#desinventarRegionId').val();
-		var RecordsPerPage = jQuery('#prmDataPageSize').val();
-		var QueryDef = jQuery('#prmDataQueryDef').val();
-		var FieldList = jQuery('#prmDataFieldList').val();
-		
-		jQuery('#tblDataRows').html('<img src="loading.gif">');
-		jQuery.post('data.php',
-			{'r' : RegionId,
-			 'page': mypag,
-			 'RecordsPerPage' : RecordsPerPage,
-			 'sql'            : QueryDef,
-			 'fld'            : FieldList
-			},
-			function(data) {
-				jQuery('#tblDataRows').html(data);
-				// Reload the jQuery functions on the new DOM elements...
-				onReadyData();
-				jQuery('#prmDataPageNumber').val(mypag);
-				// Set Number of Records in Current Displayed Page
-				jQuery('#prmDataPageRecords').val(jQuery('#tblDataRows tr').size());
-			}
-		);
+	if (parseInt(jQuery('#prmDataPageUpdate').val()) < 1) {
+		jQuery('#prmDataPageUpdate').val(1);
+		var mypag = page;
+		var now = parseInt(jQuery('#DataCurPage').val());
+		if (page == 'prev') {
+			mypag = now - 1;
+		} else if (page == 'next') {
+			mypag = now + 1;
+		}
+		var NumberOfPages = jQuery('#prmDataPageCount').val();
+		if ((mypag < 1) || (mypag > NumberOfPages)) {
+			// Out of Range Page, do nothing
+		} else {
+			jQuery('#DataCurPage').val(mypag);
+			var RegionId = jQuery('#desinventarRegionId').val();
+			var RecordsPerPage = jQuery('#prmDataPageSize').val();
+			var QueryDef = jQuery('#prmDataQueryDef').val();
+			var FieldList = jQuery('#prmDataFieldList').val();
+			
+			jQuery('#tblDataRows').html('<img src="loading.gif">');
+			jQuery.post('data.php',
+				{'r' : RegionId,
+				 'page': mypag,
+				 'RecordsPerPage' : RecordsPerPage,
+				 'sql'            : QueryDef,
+				 'fld'            : FieldList
+				},
+				function(data) {
+					jQuery('#tblDataRows').html(data);
+					// Reload the jQuery functions on the new DOM elements...
+					onReadyData();
+					jQuery('#prmDataPageNumber').val(mypag);
+					// Set Number of Records in Current Displayed Page
+					jQuery('#prmDataPageRecords').val(jQuery('#tblDataRows tr').size());
+					jQuery('#prmDataPageUpdate').val(0);
+				}
+			);
+		}
 	}
 } //function
 
