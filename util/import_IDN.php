b962e5205a276a0b33eae027d6abd38af87c5449
Jhon H. Caicedo <jhcaiced@asimov.tuq.inticol.com>
2010-10-19 06:00:41 -0500

    Fixed geography import of IDN

--- util/import_IDN.php
+++ util/import_IDN.php
@@ -20,13 +20,13 @@
 	//$e->load();
 	//print_r($e->oField);
 	$r = new DIRegionRecord($us, $RegionId);
-	$r->copyEvents('eng');
-	$r->copyCauses('eng');
+	//$r->copyEvents('eng');
+	//$r->copyCauses('eng');
 	$i = new DIImport($us);
 	//$a = $i->importFromCSV('/tmp/IDN_event.csv', DI_EVENT, true, 0);
 	//$a = $i->importFromCSV('/tmp/IDN_cause.csv', DI_CAUSE, true, 0);
 	//$a = $i->importFromCSV('/tmp/IDN_geolevel.csv', DI_GEOLEVEL, true, 0);
-	//$a = $i->importFromCSV('/tmp/IDN_geography.csv', DI_GEOGRAPHY, true, 0);
+	$a = $i->importFromCSV('/tmp/IDN_geography.csv', DI_GEOGRAPHY, true, 0);
 	//$a = $i->importFromCSV('/tmp/IDN_disaster.csv', DI_DISASTER, true, 0);
 	$us->close();
 	$us->logout();
