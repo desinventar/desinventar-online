e39ba06fe930480e1fe42600bc42b92a0837e6dc
Jhon H. Caicedo <jhcaiced@inticol.com>
2010-10-24 07:27:58 -0500

    Updated external version number to YYYY.JJJ format

--- web/include/loader.php
+++ web/include/loader.php
@@ -9,8 +9,9 @@ if (! isset($_SERVER['DI8_WEB'])) {
 }
 
 // This is the version of the software
-define('VERSION', '8.2.2.01');
-define('JSVERSION', '2010-10-24.01');
+define('VERSION'   , '2010.297');
+define('INTVERSION', '8.2.2.01');
+define('JSVERSION' , '2010-10-24.01');
 
 // 2009-07-22 (jhcaiced) Adapted Configuration and Startup for 
 // using with PHP Command Line 
