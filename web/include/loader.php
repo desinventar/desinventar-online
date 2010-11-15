67b6234c5e577b23e7d878d141894deeda864985
Jhon H. Caicedo <jhcaiced@inticol.com>
2010-11-15 06:32:26 -0500

    Updated translation of country names in portal

--- web/include/loader.php
+++ web/include/loader.php
@@ -10,10 +10,10 @@ if (! isset($_SERVER['DI8_WEB'])) {
 
 // This is the version of the software
 define('MAJORVERSION', '2010');
-define('MINORVERSION', '317');
+define('MINORVERSION', '319');
 define('VERSION'     , MAJORVERSION . '.' . MINORVERSION);
 define('INTVERSION'  , '8.2.2.01');
-define('JSVERSION'   , '2010-11-09.01');
+define('JSVERSION'   , '2010-11-15.01');
 
 // 2009-07-22 (jhcaiced) Adapted Configuration and Startup for 
 // using with PHP Command Line 
