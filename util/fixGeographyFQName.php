d2f9fba56c8e32f03fbe4f61c011be573847c678
Jhon H. Caicedo <jhcaiced@inticol.com>
2010-11-17 06:29:42 -0500

    Fixed some country names in portal

--- util/fixGeographyFQName.php
+++ util/fixGeographyFQName.php
@@ -16,7 +16,7 @@ $RegionList = array();
 foreach($q->core->query("SELECT * FROM Region ORDER BY RegionId") as $row) {
 	$RegionList[] = $row['RegionId'];
 }
-$RegionList = array('IND-1250695040-india_orissa_historic_inventory_of_disasters');
+$RegionList = array('BLZ-DISASTER');
 foreach ($RegionList as $RegionId) {
 	print $RegionId . "\n";
 	$us->open($RegionId);
