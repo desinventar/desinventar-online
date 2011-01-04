d0e4861f4564ef0f2d0c3b6cf6cc75f3dd7306a5
Jhon H. Caicedo <jhcaiced@inticol.com>
2011-01-04 06:44:54 -0500

    Updated version from 2010.324 to 2011.001

--- web/include/loader.php
+++ web/include/loader.php
@@ -9,8 +9,8 @@ if (! isset($_SERVER['DI8_WEB'])) {
 }
 
 // This is the version of the software
-define('MAJORVERSION', '2010');
-define('MINORVERSION', '324');
+define('MAJORVERSION', '2011');
+define('MINORVERSION', '001');
 define('VERSION'     , MAJORVERSION . '.' . MINORVERSION);
 define('INTVERSION'  , '8.2.2.05');
 define('JSVERSION'   , '2010-11-20.01');
