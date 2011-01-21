da12625f87218128e12e01e557cf84b37433ffa6
Jhon H. Caicedo <jhcaiced@inticol.com>
2011-01-21 07:15:16 -0500

    Added code to reposition the MousePosition control in maps

--- web/include/loader.php
+++ web/include/loader.php
@@ -10,10 +10,10 @@ if (! isset($_SERVER['DI8_WEB'])) {
 
 // This is the version of the software
 define('MAJORVERSION', '2011');
-define('MINORVERSION', '020');
+define('MINORVERSION', '021');
 define('VERSION'     , MAJORVERSION . '.' . MINORVERSION);
 define('INTVERSION'  , '8.2.2.05');
-define('JSVERSION'   , '2011-01-20.01');
+define('JSVERSION'   , '2011-01-21.01');
 
 // 2009-07-22 (jhcaiced) Adapted Configuration and Startup for 
 // using with PHP Command Line 
