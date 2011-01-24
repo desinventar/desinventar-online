30c53395b9d443496b48c6d847a5c6572aa7f388
Jhon H. Caicedo <jhcaiced@inticol.com>
2011-01-24 18:03:54 -0500

    Added getTime() calls when edit Events/Causes to avoid cache

--- web/js/database_config.js
+++ web/js/database_config.js
@@ -9,21 +9,26 @@ function onReadyDatabaseConfig() {
 		//action="javascript:var s=$('frmCauseEdit').serialize(); sendData('{-$reg-}', 'causes.php', s, $('aCausePreDefined').value);"
 		var a = new Array('aCauseName','aCauseDesc');
 		var validForm = checkForm('frmCauseEdit', a, '');
-		if (validForm == true) {
+		if (validForm == true)
+		{
 			jQuery(this).find('#RegionId').val(jQuery('#desinventarRegionId').val());
 			var params = jQuery(this).serialize();
 			jQuery.post(
 				'causes.php',
 				params,
-				function(data) {
+				function(data)
+				{
 					var reg = jQuery('#desinventarRegionId').val();
 					var opt = jQuery('#aCausePreDefined').val();
-					if (opt == "1") {
-						updateList('lst_caupred', 'causes.php', 'r='+ reg +'&cmd=list&predef=1');
-					} else {
-						updateList('lst_cauuser', 'causes.php', 'r='+ reg +'&cmd=list&predef=0');
+					if (opt == "1")
+					{
+						updateList('lst_caupred', 'causes.php', 'r='+ reg +'&cmd=list&predef=1&t=' + new Date().getTime());
 					}
-					updateList('qcaulst', 'index.php', 'r='+ reg +'&cmd=caulst');
+					else
+					{
+						updateList('lst_cauuser', 'causes.php', 'r='+ reg +'&cmd=list&predef=0&t=' + new Date().getTime());
+					}
+					updateList('qcaulst', 'index.php', 'r='+ reg +'&cmd=caulst&t=' + new Date().getTime());
 					jQuery('#causeaddsect').hide();
 				}
 			);
@@ -34,21 +39,26 @@ function onReadyDatabaseConfig() {
 	jQuery('#frmEventEdit').unbind('submit').submit(function() {
 		var a = new Array('EventName','aEventDesc');
 		var validForm = checkForm('frmEventEdit', a, ''); 
-		if (validForm == true) {
+		if (validForm == true)
+		{
 			jQuery(this).find('#RegionId').val(jQuery('#desinventarRegionId').val());
 			var params = jQuery(this).serialize();
 			jQuery.post(
 				'events.php',
 				params, 
-				function(data) {
+				function(data)
+				{
 					var reg = jQuery('#desinventarRegionId').val();
 					var opt = jQuery('#aEventPreDefined').val();
-					if (opt == "1") {
-						updateList('lst_evepred', 'events.php', 'r='+ reg +'&cmd=list&predef=1');
-					} else {
-						updateList('lst_eveuser', 'events.php', 'r='+ reg +'&cmd=list&predef=0');
+					if (opt == "1")
+					{
+						updateList('lst_evepred', 'events.php', 'r=' + reg + '&cmd=list&predef=1&t=' + new Date().getTime());
 					}
-					updateList('qevelst', 'index.php', 'r='+ reg +'&cmd=evelst');
+					else
+					{
+						updateList('lst_eveuser', 'events.php', 'r=' + reg + '&cmd=list&predef=0&t=' + new Date().getTime());
+					}
+					updateList('qevelst', 'index.php', 'r='+ reg +'&cmd=evelst&t=' + new Date().getTime());
 					jQuery('#eventaddsect').hide();
 				}
 			);
