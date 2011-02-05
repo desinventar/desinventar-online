3c6bba813e0b067cf97f4841ae8973ab76fba02b
Jhon H. Caicedo <jhcaiced@asimov.tuq.inticol.com>
2011-02-05 07:21:56 -0500

    Updated method for validate the BeginYear/EndYear parameters in query

--- web/js/desconsultar.js
+++ web/js/desconsultar.js
@@ -3,18 +3,38 @@
  (c) 1998-2011 Corporacion OSSO
 */
 
+function onReadyQueryDesign()
+{
+	jQuery('#queryBeginYear').blur(function() {
+		validateBeginYear();	
+	});
+	jQuery('#queryEndYear').blur(function() {
+		validateEndYear();
+	});
+}
 
-function validateQueryDefinition() {
+function validateQueryDefinition()
+{
 	var iReturn = 1;
-	var prmQueryMinYear = jQuery("#prmQueryMinYear").val();
-	var prmQueryMaxYear = jQuery("#prmQueryMaxYear").val();
-	var MinYear = jQuery("#queryBeginYear").val();
-	var MaxYear = jQuery("#queryEndYear").val();
-	if ( (MinYear == '') || (MinYear < prmQueryMinYear) ) {
-		jQuery("#queryBeginYear").val(prmQueryMinYear);
-	}
-	if ( (MaxYear == '') || (MaxYear > prmQueryMaxYear) ) {
-		jQuery("#queryEndYear").val(prmQueryMaxYear);
-	}
 	return iReturn;
 };
+
+function validateBeginYear()
+{
+	var prmQueryMinYear = jQuery("#prmQueryMinYear").val();
+	var MinYear = jQuery("#queryBeginYear").val();
+	if (parseInt(MinYear) != MinYear-0 )
+	{
+		jQuery("#queryBeginYear").val(prmQueryMinYear);
+	}
+}
+
+function validateEndYear()
+{
+	var prmQueryMaxYear = jQuery("#prmQueryMaxYear").val();
+	var MaxYear = jQuery("#queryEndYear").val();
+	if (parseInt(MaxYear) != MaxYear-0 )
+	{
+		jQuery("#queryEndYear").val(prmQueryMaxYear);
+	}
+}
