b8d83e4f68f0741430b6c0d6582cf970452e99d7
Jhon H. Caicedo <jhcaiced@inticol.com>
2011-02-18 12:02:04 -0500

    Remove dieedata.clas.php from requires

--- web/cards.php
+++ web/cards.php
@@ -8,7 +8,6 @@ require_once('include/loader.php');
 require_once('include/query.class.php');
 require_once('include/didisaster.class.php');
 require_once('include/diregion.class.php');
-require_once('include/dieedata.class.php');
 
 // Convert Post Form to DesInventar Disaster Table struct 
 // Insert  (1) create DisasterId. 
