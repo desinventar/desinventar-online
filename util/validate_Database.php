724d15bd6285606424f5ef826e3d445200278b2d
Jhon H. Caicedo <jhcaiced@asimov.tuq.inticol.com>
2011-02-24 08:24:21 -0500

    Updated validate_Database to avoid database locking

--- util/validate_Database.php
+++ util/validate_Database.php
@@ -15,42 +15,53 @@
 	$us->login('diadmin','di8');
 	$us->open($RegionId);
 	$iCount = 0;
-	$sQuery = 'SELECT * FROM Disaster'; // WHERE DisasterSerial IN ("2009-01222","2009-01229")';
+	$sQuery = 'SELECT DisasterId,DisasterSerial,EventId,CauseId,GeographyId FROM Disaster'; // WHERE DisasterSerial IN ("DGR-2010-02048")';
 	$result = $us->q->dreg->query($sQuery);
-	while ($record = $result->fetch(PDO::FETCH_ASSOC)) {
+	$reclist = $result->fetchAll(PDO::FETCH_ASSOC);
+	$result->closeCursor();
+	foreach($reclist as $record)
+	{
 		$msg = array();
+		$us->q->dreg->beginTransaction();
 		// Validate EEData Record
 		$sQuery = 'SELECT COUNT(DisasterId) AS C FROM EEData WHERE DisasterId="' . $record['DisasterId'] . '"';
 		foreach($us->q->dreg->query($sQuery) as $row) {
 			$count = $row['C'];
 		}
+		$us->q->dreg->commit();
 		if ($count == 0) {
 			$msg[] = 'No tiene registro en EEData';
 		}
 		
 		// Validate Event
 		$sQuery = 'SELECT COUNT(EventId) AS C FROM Event WHERE EventId="' . $record['EventId'] . '"';
+		$us->q->dreg->beginTransaction();
 		foreach($us->q->dreg->query($sQuery) as $row) {
 			$count = $row['C'];
 		}
 		if ($count == 0) {
 			$msg[] = 'EventId Error : ' . $record['EventId'];
 		}
+		$us->q->dreg->commit();
 		
 		// Validate Cause		
 		$sQuery = 'SELECT COUNT(CauseId) AS C FROM Cause WHERE CauseId="' . $record['CauseId'] . '"';
+		$us->q->dreg->beginTransaction();
 		foreach($us->q->dreg->query($sQuery) as $row) {
 			$count = $row['C'];
 		}
+		$us->q->dreg->commit();
 		if ($count == 0) {
 			$msg[] = 'CauseId Error : ' . $record['CauseId'];
 		}
 
 		// Validate Geography
 		$sQuery = 'SELECT COUNT(GeographyId) AS C FROM Geography WHERE GeographyId="' . $record['GeographyId'] . '"';
+		$us->q->dreg->beginTransaction();
 		foreach($us->q->dreg->query($sQuery) as $row) {
 			$count = $row['C'];
 		}
+		$us->q->dreg->commit();
 		if ($count == 0) {
 			$msg[] = 'GeographyId Error : ' . $record['GeographyId'];
 		}
@@ -58,11 +69,11 @@
 		// Validate DisasterBeginTime
 		if (count($msg) > 0) {
 			foreach($msg as $line) {
-				printf('%-36s %-10s %s' . "\n", $record['DisasterId'], $record['DisasterSerial'], $line);
+				printf('%-36s %-10s %s' . "\n", $DisasterId, $record['DisasterSerial'], $line);
 			}
 		}
 		if ( ($iCount > 0) && ($iCount % 100 == 0) ) {
-			//printf('%-5d' . "\n", $iCount);
+			printf('%-5d' . "\n", $iCount);
 		}
 		$iCount++;
 	} //while
