3dcef94ebef198373d21fc560140688a1d624cd4
Jhon H. Caicedo <jhcaiced@inticol.com>
2011-04-28 08:44:54 -0500

    Added simple perl script to validate if template strings are defined

---
+++ files/database/validateInterfaceStrings.pl
@@ -0,0 +1,56 @@
+#!/usr/bin/perl
+#  DesInventar - http://www.desinventar.org
+#  (c) 1998-2011 Corporacion OSSO
+#  
+#  Jhon H. Caicedo <jhcaiced@desinventar.org>
+#
+#  Process *.tpl and .conf files in order to test if all labels are defined.
+#
+use encoding "utf8";
+use Encode;
+use Data::Dumper;
+use Getopt::Long;
+
+my $lang = 'eng';
+my $bHelp = 0;
+if (!GetOptions('help|h'    => \$bHelp,
+                'lang|l=s'  => \$lang,
+   )) {
+	die "Error : Incorrect parameter list, please use --help\n";
+}                
+$conffile = '../../web/conf/' . $lang . '.conf';
+$i = 0;
+my %confstr = ();
+open(CONF,$conffile);
+while(<CONF>)
+{
+	$line = $_;
+	chomp $_;
+	if ( ($key,$value) = ($line =~ m/(.*)=(.*)/) )
+	{
+		$confstr{$key} = $value;
+		$i++;
+	}
+}
+close(CONF);
+print "$i strings\n";
+$i = 0;
+my $keys = ();
+open(TPL,'cat ../../web/templates/*.tpl ../../portal/templates/*.tpl |') || die "Could'n execute command\n";
+while(<TPL>)
+{
+	$line = $_;
+	chomp $_;
+	while ($line =~ m/{-#(.+?)#-}/g)
+	{
+		$key = $1;
+		if (! exists $confstr{$key})
+		{
+			print $key . "\n";
+			$i++;
+		}
+	}
+}
+close(TPL);
+print "$i lines\n";
+exit(0);
