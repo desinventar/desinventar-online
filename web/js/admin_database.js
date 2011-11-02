be294028f5c219c6a498dd6c60dd8a6f24740b09
Jhon H. Caicedo <jhcaiced@desinventar.org>
2011-11-02 09:34:19 -0500

    Fixing Admin Database screen

--- web/js/admin_database.js
+++ web/js/admin_database.js
@@ -5,10 +5,8 @@
 
 function onReadyAdminDatabase()
 {
-	// Populate CountryList/LanguageList in frmRegionEdit form
-	jQuery('#desinventarCountryList option').clone().appendTo('#frmRegionEdit_CountryIso');
-	jQuery('#desinventarLanguageList option').clone().appendTo('#frmRegionEdit_LangIsoCode');
-	
+	onReadyAdminDatabaseEdit();
+
 	// Highlight row on mouseOver
 	jQuery('#tblDatabaseList tr').live({
 		mouseenter: function() {
@@ -25,44 +23,14 @@ function onReadyAdminDatabase()
 			jQuery('#btnAdminDatabaseNew').hide();
 			jQuery('.clsAdminDatabase').hide();
 			jQuery('#divAdminDatabaseUpdate').show();
-
-			
 		}
 	});
 
 	jQuery('#btnAdminDatabaseEdit').click(function() {
+		jQuery('.clsAdminDatabase').hide();
 		var RegionId = jQuery('#divAdminDatabaseUpdate .RegionId').text();
-		// Load Information about database...
-		jQuery.post(
-			jQuery('#desinventarURL').val(),
-			{cmd      : 'cmdAdminDatabaseGetInfo',
-			 RegionId : RegionId
-			},
-			function(data)
-			{
-				jQuery('.clsAdminDatabase').hide();
-				jQuery('#divAdminDatabaseEdit').show();
-				jQuery('#frmRegionEdit_Cmd').val('cmdRegionUpdate');
-				jQuery('#frmRegionEdit_CountryIso').val(data.Region.CountryIso);
-				jQuery('#frmRegionEdit_RegionId').val(data.Region.RegionId);
-				jQuery('#frmRegionEdit_RegionLabel').val(data.Region.RegionLabel);
-				jQuery('#frmRegionEdit_LangIsoCode').val(data.Region.LangIsoCode);
-				jQuery('#frmRegionEdit_RegionStatus').val(data.Region.RegionStatus);
-				
-				// RegionActive/RegionPublic are set based on RegionStatus value
-				jQuery('#frmRegionEdit_RegionActive').removeAttr('checked');
-				if (parseInt(data.Region.RegionStatus) & 1)
-				{
-					jQuery('#frmRegionEdit_RegionActive').attr('checked','checked');
-				}
-				jQuery('#frmRegionEdit_RegionPublic').removeAttr('checked');
-				if (parseInt(data.Region.RegionStatus) & 2)
-				{
-					jQuery('#frmRegionEdit_RegionPublic').attr('checked','checked');
-				}
-			},
-			'json'
-		);
+		doAdminDatabaseGetInfo(RegionId);
+		jQuery('#divAdminDatabaseEdit').show();
 	});
 
 	jQuery('#btnAdminDatabaseExport').click(function() {
@@ -81,6 +49,7 @@ function onReadyAdminDatabase()
 		setRegionPA('','', '', '', '', true,false);
 		jQuery('#frmRegionEdit_Cmd').val('cmdRegionCreate');
 	}).hide();
+
 	jQuery('.clsAdminDatabaseButton').hide();
 
 	// Select Database from List
@@ -89,68 +58,8 @@ function onReadyAdminDatabase()
 		jQuery('#divAdminDatabaseList').show();
 	});
 
-	jQuery('#frmRegionEdit_CountryIso').change(function() {
-		if (jQuery('#frmRegionEdit_Cmd').val() == 'cmdRegionCreate')
-		{
-			jQuery.post('index.php',
-				{cmd        : 'cmdRegionBuildRegionId',
-				 CountryIso : jQuery(this).val()
-				},
-				function(data) {
-					if (parseInt(data.Status) > 0) {
-						jQuery('#frmRegionEdit_RegionId').val(data.RegionId);
-					}
-				},
-				'json'
-			);
-		}
-	});
-	
-	jQuery('#frmRegionEdit #lblRegionId').dblclick(function() {
-		if (jQuery('#frmRegionEdit_Cmd').val() == 'cmdRegionCreate')
-		{
-			jQuery('#frmRegionEdit_RegionId').removeAttr('disabled').focus();
-		}
-	});
-	
-	jQuery('#frmRegionEdit').submit(function() {
-		// Validate Fields
-		var bContinue = true;
-		/*
-		var a=new Array('CountryIso','RegionLabel','LangIsoCode','RegionUserAdmin');
-		var bContinue = checkForm('frmRegionEdit',a, '{-#errmsgfrm#-}');
-		*/
-		var RegionStatus = jQuery('#frmRegionEdit_RegionStatus');
-		RegionStatus.val(0);
-		if (jQuery('#frmRegionEdit_RegionActive').attr('checked')) {
-			RegionStatus.val(parseInt(RegionStatus.val()) | 1);
-		}
-		if (jQuery('#frmRegionEdit_RegionPublic').attr('checked')) {
-			RegionStatus.val(parseInt(RegionStatus.val()) | 2);
-		}
-		jQuery('#frmRegionEdit_RegionId').removeAttr('disabled');
-		var params = jQuery(this).serializeObject();
-		jQuery('#frmRegionEdit_RegionId').attr('disabled','disabled');
-		if (bContinue) {
-			jQuery('#frmRegionEdit :input').unhighlight();
-			jQuery.post(
-				jQuery('#desinventarURL').val(),
-				params, 
-				function(data) {
-					if (parseInt(data.Status) > 0) {
-						jQuery('#divDatabaseEditResult').html(data.Status + ' ' + data.RegionId);
-						// (jhcaiced) Change this for a method to update only the 
-						// affected row intead of loading all list again
-						updateList('dbl', 'region.php', 'cmd=adminreg');
-					}
-				},
-				'json'
-			);
-		}
-		return false;
-	});
-	jQuery('#frmRegionEdit_Cancel').click(function() {
-		jQuery('#divAdminDatabaseEdit').hide();
+	jQuery('#divAdminDatabaseUpdate').bind('evAdminDatabaseCancel', function() {
+		jQuery('.clsAdminDatabase').hide();
 	});
 } //onReadyAdminDatabase()
 
@@ -163,6 +72,7 @@ function doAdminDatabaseUpdateList()
 		},
 		function(data)
 		{
+			jQuery('#divAdminDatabaseList').show();
 			jQuery.each(data.RegionList, function(index, value) {
 				var clonedRow = jQuery('#tbodyDatabaseList tr:last').clone().show();
 				jQuery('.CountryIso'     , clonedRow).html(value.CountryIso);
