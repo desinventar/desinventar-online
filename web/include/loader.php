94438e91d2a9186c1e62b39946095675298d32db
Jhon H. Caicedo <jhcaiced@inticol.com>
2011-12-14 06:38:32 -0500

    Updated to version 2011.348

--- web/include/loader.php
+++ web/include/loader.php
@@ -12,9 +12,9 @@ if (! isset($_SERVER['DESINVENTAR_WEB']))
 
 // This is the version of the software
 define('MAJORVERSION', '2011');
-define('MINORVERSION', '347');
+define('MINORVERSION', '348');
 define('VERSION'     , MAJORVERSION . '.' . MINORVERSION);
-define('JSVERSION'   , '2011-12-13.01');
+define('JSVERSION'   , '2011-12-14.01');
 
 $appOptions = array();
 $appOptions['UseRemoteMaps'] = 1;
