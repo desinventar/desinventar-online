206c86d40b41ecfba7fd4f9e7eef8b6031dde649
Jhon H. Caicedo <jhcaiced@desinventar.org>
2011-12-28 17:36:49 -0500

    Adding new DatabaseGeolevels screen

--- web/index.php
+++ web/index.php
@@ -142,7 +142,6 @@ switch ($cmd)
 		echo htmlspecialchars(json_encode($answer), ENT_NOQUOTES,'UTF-8');
 	break;
 	case 'cmdDatabaseUsersUpdateOptions':
-		fb($_POST);
 		$iReturn = ERR_NO_ERROR;
 		$answer = array();
 		if ($desinventarUserRoleValue < ROLE_ADMINREGION)
