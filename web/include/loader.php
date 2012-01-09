7223dcd53853b6c2a4a0753c128ca682b1360f80
Jhon H. Caicedo <jhcaiced@desinventar.org>
2012-01-09 06:27:37 -0500

    Started version 2012.009

--- web/include/loader.php
+++ web/include/loader.php
@@ -12,9 +12,9 @@ if (! isset($_SERVER['DESINVENTAR_WEB']))
 
 // This is the version of the software
 define('MAJORVERSION', '2012');
-define('MINORVERSION', '008');
+define('MINORVERSION', '009');
 define('VERSION'     , MAJORVERSION . '.' . MINORVERSION);
-define('JSVERSION'   , '2012-01-08.01');
+define('JSVERSION'   , '2012-01-09.01');
 
 $appOptions = array();
 $appOptions['UseRemoteMaps'] = 1;
