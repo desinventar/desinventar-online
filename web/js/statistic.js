cbb9b176a66e58e1f6aec8199319768c6fab045f
Jhon H. Caicedo <jhcaiced@inticol.com>
2012-01-09 17:09:56 -0500

    Modifying main menu with new options

--- web/js/statistic.js
+++ web/js/statistic.js
@@ -1,8 +1,9 @@
 /*
  DesInventar - http://www.desinventar.org
- (c) 1998-2011 Corporacion OSSO
+ (c) 1998-2012 Corporacion OSSO
 */
-function onReadyStatistic() {
+function onReadyStatistic()
+{
 	jQuery('#btnStatGotoFirstPage').click(function() {
 		doStatDisplayPage(1);
 	});
@@ -30,59 +31,67 @@ function onReadyStatistic() {
 			return blockChars(event, jQuery(this).val(), 'integer:');
 		}
 	});
+} //onReadyStatistic()
 
-	jQuery('body').on('cmdViewStdParams', function() {
-		Ext.getCmp('wndViewStdParams').show();
-	});
-} //function
-
-function doStatDisplayPage(page) {
+function doStatDisplayPage(page)
+{
 	var mypag = page;
 	now = parseInt(jQuery('#StatCurPage').val());
-	if (page == 'prev') {
+	if (page == 'prev')
+	{
 		mypag = now - 1;
-	} else if (page == 'next') {
+	}
+	else if (page == 'next')
+	{
 		mypag = now + 1;
 	}
 	var NumberOfPages = jQuery('#prmStatNumberOfPages').val();
-	if ((mypag < 1) || (mypag > NumberOfPages)) {
+	if ((mypag < 1) || (mypag > NumberOfPages))
+	{
 		return false;
 	}
 	jQuery('#StatCurPage').val(mypag);
 	var RecordsPerPage = jQuery('#prmStatRecordsPerPage').val();
 
 	jQuery('#tblStatRows').html('<img src="' + jQuery('#desinventarURL').val() + '/images/loading.gif" alt="" />');
-	jQuery.post(jQuery('#desinventarURL').val() + '/statistic.php',
-		{r : jQuery('#prmStatRegionId').val(),
-		 page : mypag,
-		 rxp  : RecordsPerPage,
-		 sql  : jQuery('#prmStatQueryDef').val(),
-		 fld  : jQuery('#prmStatFieldList').val(),
-		 geo  : jQuery('#prmStatGeography').val()
+	jQuery.post(
+		jQuery('#desinventarURL').val() + '/statistic.php',
+		{
+			r     : jQuery('#prmStatRegionId').val(),
+			 page : mypag,
+			 rxp  : RecordsPerPage,
+			 sql  : jQuery('#prmStatQueryDef').val(),
+			 fld  : jQuery('#prmStatFieldList').val(),
+			 geo  : jQuery('#prmStatGeography').val()
 		},
-		function(data) {
+		function(data)
+		{
 			jQuery('#tblStatRows').html(data);
 		}
 	);
-} //function
+} //doStatDisplayPage()
 
-function doStatOrderByField(field, dir) {
+function doStatOrderByField(field, dir)
+{
 	jQuery('#tblStatRows').html('<img src="' + jQuery('#desinventarURL').val() + '/images/loading.gif" alt="" />');
-	jQuery.post(jQuery('#desinventarURL').val() + '/statistic.php',
-		{r  : jQuery('#prmStatRegionId').val(),
-		 page : $('StatCurPage').value,
-		 rxp  : jQuery('#prmStatRecordsPerPage').val(),
-		 sql  : jQuery('#prmStatQueryDef').val(),
-		 fld  : jQuery('#prmStatFieldList').val(),
-		 ord  : field,
-		 geo  : jQuery('#prmStatGeography').val(),
-		 dir  : dir
+	jQuery.post(
+		jQuery('#desinventarURL').val() + '/statistic.php',
+		{
+			r    : jQuery('#prmStatRegionId').val(),
+			page : $('StatCurPage').value,
+			rxp  : jQuery('#prmStatRecordsPerPage').val(),
+			sql  : jQuery('#prmStatQueryDef').val(),
+			fld  : jQuery('#prmStatFieldList').val(),
+			ord  : field,
+			geo  : jQuery('#prmStatGeography').val(),
+			dir  : dir
 		},
-		function(data) {
+		function(data)
+		{
 			jQuery('#tblStatRows').html(data);
 		}
 	);
-} //function
+} //doStatOrderByField()
 
 function setTotalize(lnow, lnext)
 {
@@ -111,4 +120,4 @@ function setTotalize(lnow, lnext)
 			}
 		}
 	} //for
-} //function
+} //setTotalize()
