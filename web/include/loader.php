a1eae74a66846e50a4600864cbe704bf9e107f81
Jhon H. Caicedo <jhcaiced@desinventar.org>
2012-01-13 14:20:56 -0500

    Disable AutoLogin feature pending the creation of a configuration option

--- web/include/loader.php
+++ web/include/loader.php
@@ -139,13 +139,6 @@ if (MODE != 'command')
 // 2009-01-15 (jhcaiced) Start by create/recover the session 
 // information, even for anonymous users
 $us = new UserSession($SessionId);
-if ($us->UserId == '')
-{
-	if ($us->doUserAutoLoginCheck() > 0)
-	{
-		$us->doUserAutoLogin();
-	}	
-}
 $us->awake();
 
 if (MODE != 'command')
