d167c296d1d1c1bd4290f8d0158c26a06eb14404
Jhon H. Caicedo <jhcaiced@inticol.com>
2012-02-10 09:18:03 -0500

    Updated version number to 2012.040

--- web/include/loader.php
+++ web/include/loader.php
@@ -12,9 +12,9 @@ if (! isset($_SERVER['DESINVENTAR_WEB']))
 
 // This is the version of the software
 define('MAJORVERSION', '2012');
-define('MINORVERSION', '039');
+define('MINORVERSION', '040');
 define('VERSION'     , MAJORVERSION . '.' . MINORVERSION);
-define('JSVERSION'   , '2012-02-08.01');
+define('JSVERSION'   , '2012-02-09.01');
 
 $appOptions = array();
 $appOptions['UseRemoteMaps'] = 1;
