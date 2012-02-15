02820473476deee3edd1c7d0b949f8b234f361cf
Jhon H. Caicedo <jhcaiced@desinventar.org>
2012-02-15 09:01:54 -0500

    Updated version to devel-2012.045

--- web/include/loader.php
+++ web/include/loader.php
@@ -12,9 +12,9 @@ if (! isset($_SERVER['DESINVENTAR_WEB']))
 
 // This is the version of the software
 define('MAJORVERSION', '2012');
-define('MINORVERSION', '044');
+define('MINORVERSION', '045');
 define('VERSION'     , MAJORVERSION . '.' . MINORVERSION);
-define('JSVERSION'   , '2012-02-13.01');
+define('JSVERSION'   , '2012-02-14.01');
 
 $appOptions = array();
 $appOptions['UseRemoteMaps'] = 1;
