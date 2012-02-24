3d3336f6cfc6f557b7e01df044f60ca1a299c8ee
Jhon H. Caicedo <jhcaiced@desinventar.org>
2012-02-24 11:00:46 -0500

    Added a function to count the ocurrences of a substring in a string

--- web/js/datacards.js
+++ web/js/datacards.js
@@ -1259,3 +1259,24 @@ function setDICard(prmRegionId, arr)
 		disenabutton($('btnDatacardEdit'), false);
 	}
 } //setDICard
+
+function occurrences(string, substring)
+{
+	var n=0;
+	var pos=0;
+	while(true)
+	{
+		pos=string.indexOf(substring,pos);
+		if (pos!=-1)
+		{
+			n++;
+			pos+=substring.length;
+		}
+		else
+		{
+			break;
+		}
+	}
+	return(n);
+}
+                                            
