82026b8ef41b8e2e8f2fadfb49c60efb4d04691e
Jhon H. Caicedo <jhcaiced@inticol.com>
2012-02-29 19:06:55 -0500

    Bug #50:Fixed counting of records in info.xml to exclude the deleted datacards

--- web/include/loader.php
+++ web/include/loader.php
@@ -12,9 +12,9 @@ if (! isset($_SERVER['DESINVENTAR_WEB']))
 
 // This is the version of the software
 define('MAJORVERSION', '2012');
-define('MINORVERSION', '059.03');
+define('MINORVERSION', '059.04');
 define('VERSION'     , MAJORVERSION . '.' . MINORVERSION);
-define('JSVERSION'   , '2012-02-28.03');
+define('JSVERSION'   , '2012-02-28.04');
 
 $appOptions = array();
 $appOptions['UseRemoteMaps'] = 1;
