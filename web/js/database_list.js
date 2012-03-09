14787144d174ef3742be1bc2fa9efe2c78729f6d
Jhon H. Caicedo <jhcaiced@desinventar.org>
2012-03-09 06:46:00 -0500

    DatabaseList : Added method to delete databases

--- web/js/database_list.js
+++ web/js/database_list.js
@@ -4,6 +4,11 @@ function onReadyDatabaseList()
 		jQuery(this).parent().highlight();
 	}).on('mouseout','td.RegionDelete', function() {
 		jQuery(this).parent().unhighlight();
+	}).on('click','td.RegionDelete', function(event) {
+		var RegionId = jQuery(this).parent().find('td.RegionId').text();
+		var RegionLabel = jQuery(this).parent().find('span.RegionLabel').text();
+		alert('Delete Database' + RegionLabel + '(' + RegionId +')');
+		event.preventDefault();
 	});
 } //onReadyDatabaseList
 
