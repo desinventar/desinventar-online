48f22e25a3ac5d26391c537ada12f4eba3b76465
Jhon H. Caicedo <jhcaiced@inticol.com>
2012-03-25 06:15:47 -0500

    Simplified loading of first tempalte

--- web/index.php
+++ web/index.php
@@ -57,7 +57,6 @@ $t->assign('appOptions', $appOptions);
 	fclose($fh);
 	ob_end_clean();
 */
-
 switch ($cmd)
 {
 	case 'test':
@@ -1117,7 +1116,7 @@ switch ($cmd)
 		$answer['Status'] = $iReturn;
 		echo htmlspecialchars(json_encode($answer), ENT_NOQUOTES);
 	break;
-	case 'main':
+	case 'main2':
 		# Direct access returns a list of public regions on this server
 		$t->assign('lg', $lg);
 		$LanguageList = $us->q->loadLanguages(1);
@@ -1273,7 +1272,7 @@ switch ($cmd)
 		echo '<DIQuery />' . base64_encode(serialize($post));
 		exit();
 	break;
-	default:
+	case 'cmdQueryOpen':
 		# Open XML file query
 		if (isset($_FILES['qry']))
 		{
@@ -1300,278 +1299,154 @@ switch ($cmd)
 		{
 			$RegionId = $get['r'];
 		}
-		# 2009-08-07 (jhcaiced) Validate if Database Exists...
-		if (!empty($RegionId) && file_exists($us->q->getDBFile($RegionId)))
+	break;
+	default:
+		# Direct access returns a list of public regions on this server
+		$t->assign('LanguageList', $us->q->loadLanguages(1));
+		$t->assign('CountryList', $us->q->getCountryList());
+
+		# Datacards
+		$t->assign('LabelsDisaster', $us->q->queryLabelsFromGroup('Disaster', $lg));
+		$t->assign('LabelsRecord1', $us->q->queryLabelsFromGroup('Record|1', $lg));
+		$t->assign('LabelsEvent', $us->q->queryLabelsFromGroup('Event', $lg));
+		$t->assign('LabelsCause', $us->q->queryLabelsFromGroup('Cause', $lg));
+
+		# Query Design
+		$t->assign('rc2', $us->q->queryLabelsFromGroup('Record|2', $lg));
+
+		# Query words and phrases in dictionary..
+		$ef1 = $us->q->queryLabelsFromGroup('Effect|People', $lg);
+		$ef2 = $us->q->queryLabelsFromGroup('Effect|Affected', $lg);
+		$ef3 = $us->q->queryLabelsFromGroup('Effect|Economic', $lg);
+		$ef4 = $us->q->queryLabelsFromGroup('Effect|More', $lg);
+		$sec = $us->q->queryLabelsFromGroup('Sector', $lg);
+		# Add some fields to customize lists ??
+		#$ef1['EffectFarmingAndForest'] = $ef2['EffectFarmingAndForest'];
+		#$ef1['EffectLiveStock'] = $ef2['EffectLiveStock'];
+		#$ef1['EffectRoads'] = $ef2['EffectRoads'];
+		#$ef1['EffectEducationCenters'] = $ef2['EffectEducationCenters'];
+		#$ef1['EffectMedicalCenters'] = $ef2['EffectMedicalCenters'];
+
+		$sec['SectorTransport'][3] 		= null; #array('EffectRoads' => $ef2['EffectRoads'][0]);
+		$sec['SectorCommunications'][3] = null;
+		$sec['SectorRelief'][3] 		= null;
+		$sec['SectorAgricultural'][3] 	= null; #array('EffectFarmingAndForest' => $ef2['EffectFarmingAndForest'][0],
+												#'EffectLiveStock' => $ef2['EffectLiveStock'][0]);
+		$sec['SectorWaterSupply'][3] 	= null;
+		$sec['SectorSewerage'][3]		= null;
+		$sec['SectorEducation'][3]		= null; #array('EffectEducationCenters' => $ef2['EffectEducationCenters'][0]);
+		$sec['SectorPower'][3]			= null;
+		$sec['SectorIndustry'][3]		= null;
+		$sec['SectorHealth'][3]			= null; #array('EffectMedicalCenters' => $ef2['EffectMedicalCenters'][0]);
+		$sec['SectorOther'][3]			= null;
+		$dic = array();
+		$dic = array_merge($dic, $us->q->queryLabelsFromGroup('MapOpt', $lg));
+		$dic = array_merge($dic, $us->q->queryLabelsFromGroup('Graph', $lg));
+		$dic = array_merge($dic, $us->q->queryLabelsFromGroup('DB', $lg));
+		$dic = array_merge($dic, $ef1);
+		$dic = array_merge($dic, $ef2);
+		$dic = array_merge($dic, $ef3);
+		$dic = array_merge($dic, $sec);
+		$t->assign('dic', $dic);
+		$t->assign('ef1', $ef1);
+		$t->assign('ef2', $ef2);
+		$t->assign('ef3', $ef3);
+		$t->assign('ef4', $ef4);
+		$t->assign('sec', $sec);
+		# DATA
+		$dc2 = array();
+		$dc2 = array_merge($dc2, $us->q->queryLabelsFromGroup('Disaster', $lg));
+		$dc2 = array_merge($dc2, $us->q->queryLabelsFromGroup('Record|2', $lg));
+		$dc2 = array_merge($dc2, $us->q->queryLabelsFromGroup('Geography', $lg));
+		$dc2 = array_merge($dc2, $us->q->queryLabelsFromGroup('Event', $lg));
+		$dc2 = array_merge($dc2, $us->q->queryLabelsFromGroup('Cause', $lg));
+		$dc2 = array_merge($dc2, $us->q->queryLabelsFromGroup('Effect', $lg));
+		$dc2 = array_merge($dc2, $us->q->queryLabelsFromGroup('Sector', $lg));
+		$t->assign('dc2', $dc2);
+		$fld = 'DisasterSerial,DisasterBeginTime,EventName,GeographyFQName,DisasterSiteNotes,'.
+			'DisasterSource,EffectNotes,EffectPeopleDead,EffectPeopleMissing,EffectPeopleInjured,'.
+			'EffectPeopleHarmed,EffectPeopleAffected,EffectPeopleEvacuated,EffectPeopleRelocated,'.
+			'EffectHousesDestroyed,EffectHousesAffected,EffectFarmingAndForest,EffectRoads,'.
+			'EffectEducationCenters,EffectMedicalCenters,EffectLiveStock,EffectLossesValueLocal,'.
+			'EffectLossesValueUSD,EffectOtherLosses,SectorTransport,SectorCommunications,SectorRelief,'.
+			'SectorAgricultural,SectorWaterSupply,SectorSewerage,SectorEducation,SectorPower,SectorIndustry,'.
+			'SectorHealth,SectorOther,EventDuration,EventMagnitude,CauseName,CauseNotes';
+		$sda = explode(',', $fld);
+		$t->assign('sda', $sda);
+		$sda1 = explode(',', 'GeographyCode,DisasterLatitude,DisasterLongitude,RecordAuthor,RecordCreation,RecordUpdate,EventNotes');
+		$t->assign('sda1', $sda1);	# array_diff_key($dc2, array_flip($sda))
+
+		# MAPS
+		$range[] = array(     10, '1 - 10'          , 'ffff99');
+		$range[] = array(    100, '11 - 100'        , 'ffff00');
+		$range[] = array(   1000, '101 - 1000'      , 'ffcc00');
+		$range[] = array(  10000, '1001 - 10000'    , 'ff6600');
+		$range[] = array( 100000, '10001 - 100000'  , 'cc0000');
+		$range[] = array(1000000, '100001 - 1000000', '660000');
+		$range[] = array(''     , '1000001 ->'      , '000000');
+		$t->assign('range', $range);
+		# STATISTIC
+		$st = array();
+		$std = array();
+		$std = array_merge($std, $us->q->queryLabelsFromGroup('Statistic', $lg));
+		$std = array_merge($std, $st);
+		$t->assign('std', $std);
+
+		# DATACARDS
+		$t->assign('usr', $us->UserId);
+		$dic = $us->q->queryLabelsFromGroup('DB', $lg);
+		switch ($desinventarUserRole)
 		{
-			# Accessing a region with some operation
-			$us->open($RegionId);
-			if (isset($get['lang']) && !empty($get['lang']))
-			{
-				$_SESSION['lang'] = $get['lang'];
-			}
-			# Direct access returns a list of public regions on this server
-			$t->assign('LanguageList', $us->q->loadLanguages(1));
-			$t->assign('CountryList', $us->q->getCountryList());
-			switch ($get['cmd'])
-			{
-				case 'getGeoId':
-					$code = $us->q->getObjectNameById($get['GeoCode'], 'GEOCODE');
-					echo $code;
-				break;
-				default:
-					# Update UserSession with Current Language.
-					$us->update();
-					# Get UserRole
-					$r = new DIRegion($us, $RegionId);
-					$RegionStatus = (int)$r->get('RegionStatus');
-					$RegionPublic = $RegionStatus & 2;
-					$bCanShowDatabase = 0;
-					if ($desinventarUserRoleValue > 0)
-					{
-						$bCanShowDatabase = 1;
-					}
-					else
-					{
-						if ($RegionPublic > 0)
-						{
-							$bCanShowDatabase = 1;
-						}
-					}
-					# Datacards
-					$t->assign('LabelsDisaster', $us->q->queryLabelsFromGroup('Disaster', $lg));
-					$t->assign('LabelsRecord1', $us->q->queryLabelsFromGroup('Record|1', $lg));
-					$t->assign('LabelsEvent', $us->q->queryLabelsFromGroup('Event', $lg));
-					$t->assign('LabelsCause', $us->q->queryLabelsFromGroup('Cause', $lg));
-
-					# Query Design
-					$t->assign('rc2', $us->q->queryLabelsFromGroup('Record|2', $lg));
-					
-					$t->assign('reg', $RegionId);
-					#$t->assign('path', VAR_DIR);
-					
-					$t->assign('role', $desinventarRole);
-					if (strlen($desinventarRole) > 0)
-					{
-						$t->assign('ctl_user', true);
-					}
-					else
-					{
-						$t->assign('ctl_user', false);
-					}
-					# Set selection map
-					$t->assign('RegionLabel', $RegionLabel);
-					$t->assign('ctl_showmap', true);
-					# get range of dates
-					$ydb = $us->getDateRange();
-					$t->assign('yini', substr($ydb[0], 0, 4));
-					$t->assign('yend', substr($ydb[1], 0, 4));
-
-					# Load default list of Geography, Event, Cause
-					$geol = $us->q->loadGeography(0);
-					$glev = $us->q->loadGeoLevels('', -1, false);
-					$evepredl = $us->q->loadEvents('PREDEF', 'active', $lg, $us->RegionLangIsoCode);
-					$eveuserl = $us->q->loadEvents('USER', 'active', $lg, $us->RegionLangIsoCode);
-					$caupredl = $us->q->loadCauses('PREDEF', 'active', $lg, $us->RegionLangIsoCode);
-					$cauuserl = $us->q->loadCauses('USER', 'active', $lg, $us->RegionLangIsoCode);
-
-					# In Saved Queries set true in Geo, Events, Causes selected..
-					if (isset($qd['D_GeographyId']))
-					{
-						$gtree = $us->q->buildGeoTree('', 0, $us->q->getMaxGeoLev(), $qd['D_GeographyId']);
-						$t->assign('gtree', $gtree);
-						$geol = null;
-					}
-
-					if (isset($qd['D_EventId']))
-					{
-						foreach ($qd['D_EventId'] as $ky=>$it)
-						{
-							if (isset($evepredl[$it]))
-							{
-								$evepredl[$it][3] = 1;
-							}
-							if (isset($eveuserl[$it]))
-							{
-								$eveuserl[$it][3] = 1;
-							}
-						}
-					}
-
-					if (isset($qd['D_CauseId']))
-					{
-						foreach ($qd['D_CauseId'] as $ky=>$it)
-						{
-							if (isset($caupredl[$it]))
-							{
-								$caupredl[$it][3] = 1;
-							}
-							if (isset($cauuserl[$it]))
-							{
-								$cauuserl[$it][3] = 1;
-							}
-						}
-					}
-					# List of elements: Geography, GLevels, Events, Causes..
-					$t->assign('geol', $geol);
-					$t->assign('glev', $glev);
-					$t->assign('evepredl', $evepredl);
-					$t->assign('eveuserl', $eveuserl);
-					$t->assign('caupredl', $caupredl);
-					$t->assign('cauuserl', $cauuserl);
-					# Query words and phrases in dictionary..
-					$ef1 = $us->q->queryLabelsFromGroup('Effect|People', $lg);
-					$ef2 = $us->q->queryLabelsFromGroup('Effect|Affected', $lg);
-					$ef3 = $us->q->queryLabelsFromGroup('Effect|Economic', $lg);
-					$ef4 = $us->q->queryLabelsFromGroup('Effect|More', $lg);
-					$sec = $us->q->queryLabelsFromGroup('Sector', $lg);
-					# Add some fields to customize lists ??
-					#$ef1['EffectFarmingAndForest'] = $ef2['EffectFarmingAndForest'];
-					#$ef1['EffectLiveStock'] = $ef2['EffectLiveStock'];
-					#$ef1['EffectRoads'] = $ef2['EffectRoads'];
-					#$ef1['EffectEducationCenters'] = $ef2['EffectEducationCenters'];
-					#$ef1['EffectMedicalCenters'] = $ef2['EffectMedicalCenters'];
-
-					$sec['SectorTransport'][3] 		= null; #array('EffectRoads' => $ef2['EffectRoads'][0]);
-					$sec['SectorCommunications'][3] = null;
-					$sec['SectorRelief'][3] 		= null;
-					$sec['SectorAgricultural'][3] 	= null; #array('EffectFarmingAndForest' => $ef2['EffectFarmingAndForest'][0],
-															#'EffectLiveStock' => $ef2['EffectLiveStock'][0]);
-					$sec['SectorWaterSupply'][3] 	= null;
-					$sec['SectorSewerage'][3]		= null;
-					$sec['SectorEducation'][3]		= null; #array('EffectEducationCenters' => $ef2['EffectEducationCenters'][0]);
-					$sec['SectorPower'][3]			= null;
-					$sec['SectorIndustry'][3]		= null;
-					$sec['SectorHealth'][3]			= null; #array('EffectMedicalCenters' => $ef2['EffectMedicalCenters'][0]);
-					$sec['SectorOther'][3]			= null;
-					$dic = array();
-					$dic = array_merge($dic, $us->q->queryLabelsFromGroup('MapOpt', $lg));
-					$dic = array_merge($dic, $us->q->queryLabelsFromGroup('Graph', $lg));
-					$dic = array_merge($dic, $us->q->queryLabelsFromGroup('DB', $lg));
-					$dic = array_merge($dic, $ef1);
-					$dic = array_merge($dic, $ef2);
-					$dic = array_merge($dic, $ef3);
-					$dic = array_merge($dic, $sec);
-					$t->assign('dic', $dic);
-					$t->assign('ef1', $ef1);
-					$t->assign('ef2', $ef2);
-					$t->assign('ef3', $ef3);
-					$t->assign('ef4', $ef4);
-					$t->assign('sec', $sec);
-					# DATA
-					$dc2 = array();
-					$dc2 = array_merge($dc2, $us->q->queryLabelsFromGroup('Disaster', $lg));
-					$dc2 = array_merge($dc2, $us->q->queryLabelsFromGroup('Record|2', $lg));
-					$dc2 = array_merge($dc2, $us->q->queryLabelsFromGroup('Geography', $lg));
-					$dc2 = array_merge($dc2, $us->q->queryLabelsFromGroup('Event', $lg));
-					$dc2 = array_merge($dc2, $us->q->queryLabelsFromGroup('Cause', $lg));
-					$dc2 = array_merge($dc2, $us->q->queryLabelsFromGroup('Effect', $lg));
-					$dc2 = array_merge($dc2, $us->q->queryLabelsFromGroup('Sector', $lg));
-					$t->assign('dc2', $dc2);
-					$fld = 'DisasterSerial,DisasterBeginTime,EventName,GeographyFQName,DisasterSiteNotes,'.
-						'DisasterSource,EffectNotes,EffectPeopleDead,EffectPeopleMissing,EffectPeopleInjured,'.
-						'EffectPeopleHarmed,EffectPeopleAffected,EffectPeopleEvacuated,EffectPeopleRelocated,'.
-						'EffectHousesDestroyed,EffectHousesAffected,EffectFarmingAndForest,EffectRoads,'.
-						'EffectEducationCenters,EffectMedicalCenters,EffectLiveStock,EffectLossesValueLocal,'.
-						'EffectLossesValueUSD,EffectOtherLosses,SectorTransport,SectorCommunications,SectorRelief,'.
-						'SectorAgricultural,SectorWaterSupply,SectorSewerage,SectorEducation,SectorPower,SectorIndustry,'.
-						'SectorHealth,SectorOther,EventDuration,EventMagnitude,CauseName,CauseNotes';
-					$sda = explode(',', $fld);
-					$t->assign('sda', $sda);
-					$sda1 = explode(',', 'GeographyCode,DisasterLatitude,DisasterLongitude,RecordAuthor,RecordCreation,RecordUpdate,EventNotes');
-					$t->assign('sda1', $sda1);	# array_diff_key($dc2, array_flip($sda))
-					# MAPS
-					$range[] = array(     10, '1 - 10'          , 'ffff99');
-					$range[] = array(    100, '11 - 100'        , 'ffff00');
-					$range[] = array(   1000, '101 - 1000'      , 'ffcc00');
-					$range[] = array(  10000, '1001 - 10000'    , 'ff6600');
-					$range[] = array( 100000, '10001 - 100000'  , 'cc0000');
-					$range[] = array(1000000, '100001 - 1000000', '660000');
-					$range[] = array(''     , '1000001 ->'      , '000000');
-					$t->assign('range', $range);
-					# STATISTIC
-					$st = array();
-					foreach ($us->q->loadGeoLevels('', -1, false) as $k=>$i)
-						$st['StatisticGeographyId_'. $k] = array($i[0], $i[1]);
-					$std = array();
-					$std = array_merge($std, $us->q->queryLabelsFromGroup('Statistic', $lg));
-					$std = array_merge($std, $st);
-					$t->assign('std', $std);
-
-					# DATACARDS
-					$t->assign('usr', $us->UserId);
-					# Validate if user has permission to access database
-					$dic = $us->q->queryLabelsFromGroup('DB', $lg);
-					switch ($desinventarUserRole)
-					{
-						case 'ADMINREGION':
-							$t->assign('showconfig', true);
-							$dicrole = $dic['DBRoleAdmin'][0];
-						break;
-						case 'OBSERVER':
-							$t->assign('showconfig', true);
-							$dicrole = $dic['DBRoleObserver'][0];
-						break;
-						case 'SUPERVISOR':
-							$dicrole = $dic['DBRoleSupervisor'][0];
-						break;
-						case 'USER':
-							$dicrole = $dic['DBRoleUser'][0];
-						break;
-						default:
-							$dicrole = null;
-						break;
-					}
-					$t->assign('dicrole', $dicrole);
-					$t->assign('ctl_effects', true);
-					$dis = $us->q->queryLabelsFromGroup('Disaster', $lg);
-					$dis = array_merge($dis, $us->q->queryLabelsFromGroup('Geography', $lg));
-					$t->assign('dis', $dis);
-					$t->assign('rc1', $us->q->queryLabelsFromGroup('Record|1', $lg));
-					$t->assign('rc2', $us->q->queryLabelsFromGroup('Record|2', $lg));
-					$t->assign('eve', $us->q->queryLabelsFromGroup('Event', $lg));
-					$t->assign('cau', $us->q->queryLabelsFromGroup('Cause', $lg));
-					$sc3 = $us->q->querySecLabelFromGroup('Effect|Affected', $lg);
-					$t->assign('sc3', $sc3);
-					$t->assign('dmg', $us->q->queryLabelsFromGroup('MetGuide', $lg));
-					
-					# Geography Levels
-					$GeoLevelList = $us->getGeoLevels();
-					$t->assign('GeoLevelList', $GeoLevelList);
-					
-					$lev = 0;
-					$t->assign('lev', $lev);
-					$t->assign('levmax', $us->q->getMaxGeoLev());
-					$t->assign('levname', $us->q->loadGeoLevById($lev));
-					$t->assign('geol', $us->q->loadGeography($lev));
-					$t->assign('EventList', $us->q->loadEvents('ALL', 'active', $lg, $us->RegionLangIsoCode));
-					$t->assign('CauseList', $us->q->loadCauses('ALL', 'active', $lg, $us->RegionLangIsoCode));
-					$EEFieldList = $us->q->getEEFieldList('True');
-					$t->assign('EEFieldList', $EEFieldList);
-					$t->assign('RegionId', $RegionId);
-					# DATACARDS END
-					
-					# BEGIN THEMATIC MAP
-					# 2010-01-18 (jhcaiced) Windows machines doesn't use remote servers
-					if (isset($_SERVER['WINDIR']))
-					{
-						$desinventarHasInternet = 0;
-					}
-					else
-					{
-						# Linux machines are assumed to be connected to internet
-						$desinventarHasInternet = 1;
-						#if (!fsockopen('www.google.com',80))
-						#{
-						#	$desinventarHasInternet = 0;
-						#}
-					}
-					$t->assign('desinventarHasInternet', $desinventarHasInternet);
-					$t->assign('configfile', $lg . '.conf');
-					$template = 'index-' . $lg . '.tpl';
-					$t->display($template);
-				break;
-			} # switch
+			case 'ADMINREGION':
+				$t->assign('showconfig', true);
+				$dicrole = $dic['DBRoleAdmin'][0];
+			break;
+			case 'OBSERVER':
+				$t->assign('showconfig', true);
+				$dicrole = $dic['DBRoleObserver'][0];
+			break;
+			case 'SUPERVISOR':
+				$dicrole = $dic['DBRoleSupervisor'][0];
+			break;
+			case 'USER':
+				$dicrole = $dic['DBRoleUser'][0];
+			break;
+			default:
+				$dicrole = null;
+			break;
 		}
+		$t->assign('dicrole', $dicrole);
+		$t->assign('ctl_effects', true);
+		$dis = $us->q->queryLabelsFromGroup('Disaster', $lg);
+		$dis = array_merge($dis, $us->q->queryLabelsFromGroup('Geography', $lg));
+		$t->assign('dis', $dis);
+		$t->assign('rc1', $us->q->queryLabelsFromGroup('Record|1', $lg));
+		$t->assign('rc2', $us->q->queryLabelsFromGroup('Record|2', $lg));
+		$t->assign('eve', $us->q->queryLabelsFromGroup('Event', $lg));
+		$t->assign('cau', $us->q->queryLabelsFromGroup('Cause', $lg));
+		$sc3 = $us->q->querySecLabelFromGroup('Effect|Affected', $lg);
+		$t->assign('sc3', $sc3);
+		$t->assign('dmg', $us->q->queryLabelsFromGroup('MetGuide', $lg));
+
+		# BEGIN THEMATIC MAP
+		# 2010-01-18 (jhcaiced) Windows machines doesn't use remote servers
+		if (isset($_SERVER['WINDIR']))
+		{
+			$desinventarHasInternet = 0;
+		}
+		else
+		{
+			# Linux machines are assumed to be connected to internet
+			$desinventarHasInternet = 1;
+			#if (!fsockopen('www.google.com',80))
+			#{
+			#	$desinventarHasInternet = 0;
+			#}
+		}
+		$t->assign('desinventarHasInternet', $desinventarHasInternet);
+		$t->assign('configfile', $lg . '.conf');
+		$template = 'index-' . $lg . '.tpl';
+		$t->display($template);
 	break;
 } #switch($cmd)
 
