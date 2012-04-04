959fb16acd62502beb572fdf53509b46c3363bad
Jhon H. Caicedo <jhcaiced@desinventar.org>
2012-04-04 18:41:34 -0500

    Updated version to 2012.04.095.01 for stable release

--- web/include/loader.php
+++ web/include/loader.php
@@ -12,7 +12,7 @@ if (! isset($_SERVER['DESINVENTAR_WEB']))
 
 // This is the version of the software
 define('MAJORVERSION', '2012');
-define('MINORVERSION', '03.093.002');
+define('MINORVERSION', '04.095.001');
 define('VERSION'     , MAJORVERSION . '.' . MINORVERSION);
 define('JSVERSION'   , VERSION);
 
