272b0811b470ad0c0f66827838178b849295f923
Jhon H. Caicedo <jhcaiced@desinventar.org>
2012-04-21 04:52:04 -0500

    Adding more graph type to new objects

--- web/include/digraphxml.class.php
+++ web/include/digraphxml.class.php
@@ -9,7 +9,22 @@ class DIGraphXML extends DIGraph
 	public function __construct($prmSession, $prmXML)
 	{
 		$options = query_convert_v2_to_v1($prmXML);
+		$graph_options = $this->convert_v2_to_v1($prmXML);
+		if (count($graph_options) > 0)
+		{
+			$options['Graph'] = $graph_options;
+		}
 		parent::__construct($prmSession, $options);
 	} #__construct()
+	
+	public function convert_v2_to_v1($xml_string)
+	{
+		$answer = array();
+		$xml_doc = new SimpleXMLElement($xml_string);
+		$xml_query = reset($xml_doc->xpath('graph'));
+		$answer['Type']    = query_trim(reset($xml_query->xpath('type')));
+		$answer['SubType'] = query_trim(reset($xml_query->xpath('subtype')));
+		return $answer;		
+	}
 } #class
 </script>
