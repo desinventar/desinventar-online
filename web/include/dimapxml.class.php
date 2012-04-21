88b86d9d3f57dc5a26b478000f282f278e37eb29
Jhon H. Caicedo <jhcaiced@desinventar.org>
2012-04-21 06:35:35 -0500

    Added classes for Map handling

---
+++ web/include/dimapxml.class.php
@@ -0,0 +1,14 @@
+<script language="php">
+#
+# DesInventar - http://www.desinventar.org
+# (c) 1998-2012 Corporación OSSO
+#
+
+class DIMapXML extends DIResult
+{
+	public function __construct($prmSession, $prmOptions)
+	{
+		parent::__construct($prmSession, $prmOptions);
+	}
+} #class
+</script>
