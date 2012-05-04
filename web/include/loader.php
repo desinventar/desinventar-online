e0811e2ba839d834eabe921f831cb3be4f4fd616
Jhon H. Caicedo <jhcaiced@gmail.com>
2012-05-04 12:32:23 -0500

    Bug #114: Fixed labels on graph parameters

--- web/include/loader.php
+++ web/include/loader.php
@@ -12,8 +12,8 @@ if (! isset($_SERVER['DESINVENTAR_WEB']))
 
 // This is the version of the software
 define('MAJORVERSION', '2012');
-define('MINORVERSION', '04.095.04');
-define('RELEASEDATE' , '2012-05-03');
+define('MINORVERSION', '04.095.05');
+define('RELEASEDATE' , '2012-05-04');
 define('VERSION'     , MAJORVERSION . '.' . MINORVERSION);
 define('JSVERSION'   , VERSION);
 
