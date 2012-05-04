d861282c5872f88c8ff9ca02d96c2919a439fea1
Jhon H. Caicedo <jhcaiced@desinventar.org>
2012-05-04 18:04:00 -0500

    Bug #119: Fixed error when editing datacard composed fields

--- web/include/loader.php
+++ web/include/loader.php
@@ -12,7 +12,7 @@ if (! isset($_SERVER['DESINVENTAR_WEB']))
 
 // This is the version of the software
 define('MAJORVERSION', '2012');
-define('MINORVERSION', '04.095.05');
+define('MINORVERSION', '04.095.06');
 define('RELEASEDATE' , '2012-05-04');
 define('VERSION'     , MAJORVERSION . '.' . MINORVERSION);
 define('JSVERSION'   , VERSION);
