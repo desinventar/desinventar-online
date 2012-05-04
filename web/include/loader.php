29ba9e4df1c483a80005366623c8698dd0d4d153
Jhon H. Caicedo <jhcaiced@desinventar.org>
2012-05-03 20:07:52 -0500

    Bug #111: Fixed datacard edition, error was caused by a typo in datacards.js

--- web/include/loader.php
+++ web/include/loader.php
@@ -12,8 +12,8 @@ if (! isset($_SERVER['DESINVENTAR_WEB']))
 
 // This is the version of the software
 define('MAJORVERSION', '2012');
-define('MINORVERSION', '04.095.03');
-define('RELEASEDATE' , '2012-04-27');
+define('MINORVERSION', '04.095.04');
+define('RELEASEDATE' , '2012-05-03');
 define('VERSION'     , MAJORVERSION . '.' . MINORVERSION);
 define('JSVERSION'   , VERSION);
 
