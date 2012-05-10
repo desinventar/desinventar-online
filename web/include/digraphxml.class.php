ffa010a4bc5f418cd59c61a1b38cf6d4f05e8b50
Jhon H. Caicedo <jhcaiced@desinventar.org>
2012-05-10 05:40:41 -0500

    Updated method for display of profile reports

--- web/include/digraphxml.class.php
+++ web/include/digraphxml.class.php
@@ -8,8 +8,10 @@ class DIGraphXML extends DIGraph
 {
 	public function __construct($prmSession, $xml_graph, $xml_query)
 	{
-		$options = query_convert_v2_to_v1($xml_query);
-		$graph_options = $this->convert_v2_to_v1($xml_graph);
+		$this->xml_graph = $xml_graph;
+		$this->xml_query = $xml_query;
+		$options = query_convert_v2_to_v1($this->xml_query);
+		$graph_options = $this->convert_v2_to_v1($this->xml_graph);
 		if (count($graph_options) > 0)
 		{
 			$options['Graph'] = $graph_options;
@@ -17,11 +19,11 @@ class DIGraphXML extends DIGraph
 		parent::__construct($prmSession, $options);
 	} #__construct()
 	
-	public function convert_v2_to_v1($xml)
+	public function convert_v2_to_v1($xml_graph)
 	{
 		$answer = array();
-		$answer['Type']    = query_trim(reset($xml->xpath('type')));
-		$answer['SubType'] = query_trim(reset($xml->xpath('subtype')));
+		$answer['Type']    = query_trim(reset($xml_graph->xpath('type')));
+		$answer['SubType'] = query_trim(reset($xml_graph->xpath('subtype')));
 		return $answer;		
 	}
 } #class
