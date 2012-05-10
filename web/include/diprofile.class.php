23d8c7ec0c2138c1317e180d84561293fce0cf8c
Jhon H. Caicedo <jhcaiced@desinventar.org>
2012-05-10 06:28:19 -0500

    Updated method for display of titles in profile reports

--- web/include/diprofile.class.php
+++ web/include/diprofile.class.php
@@ -15,16 +15,19 @@ class DIProfile
 	
 	public function execute($xml = '')
 	{
+		$LangIsoCode = $this->session->LangIsoCode;
 		if ($xml == '') { $xml = $this->xml; }
 		$html = '';
-		foreach($xml->xpath('title') as $node)
+		foreach($xml->xpath('title/text[@langisocode="' . $LangIsoCode . '"]') as $node)
 		{
 			$title = query_trim($node);
-			$html .= $title . '<br />';
+			$html .= '<h3>' . $title . '</h3>';
 		}
 		$xml_query = reset($xml->xpath('query'));
 		foreach($xml->xpath('graph') as $xml_graph)
 		{
+			$graph_title = query_trim(reset($xml_graph->xpath('title/text[@langisocode="' . $LangIsoCode . '"]')));
+			$html .= '<h4>' . $graph_title . '</h4>';
 			$graph = new DIGraphXML($this->session, $xml_graph, $xml_query);
 			$graph->execute();
 			$html .= '<img src="' . $graph->output['ImageURL'] . '" /><br />';
