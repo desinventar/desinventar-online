454ead6ad60b84a6b01938e9af240c7d0f8b220a
Jhon H. Caicedo <jhcaiced@inticol.com>
2012-05-19 07:27:29 -0500

    Bug #127: Fixed comparative of graphics by geography using only elements of the level selected

--- web/graphic.php
+++ web/graphic.php
@@ -45,13 +45,7 @@ $prmGraph = $post['prmGraph'];
 $GraphCommand = $prmGraph['Command'];
 if ($GraphCommand != '')
 {
-	// Process QueryDesign Fields and count results
-	$qd  = $us->q->genSQLWhereDesconsultar($post);
-	$sqc = $us->q->genSQLSelectCount($qd);	
-	$c   = $us->q->getresult($sqc);
-	$NumRecords = $c['counter'];
-	$t->assign ('NumRecords', $NumRecords);
-
+	$query_graph = '';
 	$prmGraph['SubType'] = (int)$prmGraph['SubType'];
 	if ($prmGraph['SubType'] == GRAPH_HISTOGRAM_TEMPORAL)
 	{
@@ -82,11 +76,26 @@ if ($GraphCommand != '')
 	{
 		$k = $prmGraph['SubType'] - 200;
 		$prmGraph['VarList'] = 'D.GeographyId_' . $k;
+		# Bug 127 - Limit data in graph results
+		$query_graph = 'G.GeographyLevel>=' . $k;
 	}
 	else
 	{
 		$prmGraph['VarList'] = $prmGraph['SubType'];
 	}
+
+	// Process QueryDesign Fields and count results
+	$qd  = $us->q->genSQLWhereDesconsultar($post);
+	# Add specific query parameters
+	if ($query_graph != '')
+	{
+		$qd .= ' AND (' . $query_graph . ')';
+	}
+	$sqc = $us->q->genSQLSelectCount($qd);	
+	$c   = $us->q->getresult($sqc);
+	$NumRecords = $c['counter'];
+	$t->assign ('NumRecords', $NumRecords);
+
 	$post['prmGraph'] = $prmGraph;
 
 	$sImageURL  = WWWDATA . '/graphs/graph_'. session_id() . '_' . time() . '.png';
