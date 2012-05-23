ac02dec33b8d746f7d8448b91fcca22ea4a7358a
Jhon H. Caicedo <jhcaiced@inticol.com>
2012-05-23 08:43:02 -0500

    Bug #113: Fixed alignment of XAxis labels in graphs

--- web/include/graphic.class.php
+++ web/include/graphic.class.php
@@ -278,14 +278,16 @@ class Graphic
 				$this->g->xaxis->title->SetFont(FF_ARIAL, FS_NORMAL);
 				$this->g->xaxis->SetTickLabels($XAxisLabels);
 				$this->g->xaxis->SetFont(FF_ARIAL,FS_NORMAL, 8);
-				$this->g->xaxis->SetLabelAngle(90);
-				$XAxisLabelMargin = $this->getTextArrayMaxWidth($XAxisLabels, 8);
-				if ($XAxisLabelMargin < 25)
-				{
-					$XAxisLabelMargin += 4;
-				}
+				#$this->g->xaxis->SetLabelAngle(90);
+				#$XAxisLabelMargin = $this->getTextArrayMaxWidth($XAxisLabels, 8);
+				#if ($XAxisLabelMargin < 25)
+				#{
+				#	$XAxisLabelMargin += 4;
+				#}
+				$this->g->xaxis->SetLabelAngle(-90);
+				$XAxisLabelMargin = 5;
 				$this->g->xaxis->SetLabelMargin($XAxisLabelMargin);
-				$this->g->xaxis->SetLabelAlign('center','bottom');
+				$this->g->xaxis->SetLabelAlign('center','top');
 				// 2009-02-03 (jhcaiced) Try to avoid overlapping labels in XAxis
 				// by calculating the interval of the labels
 				$iNumPoints = count($val);		
