c2d0b58ee77fe92530f46197c525079f0960f4b1
Jhon H. Caicedo <jhcaiced@inticol.com>
2012-05-25 04:21:07 -0500

    Added custom fields in metguide template

--- web/metguide.php
+++ web/metguide.php
@@ -15,6 +15,22 @@
 		$metguide[$pagekey]  = $us->q->queryLabel($mod, $pagekey , $lg);
 	}
 	$t->assign('metguide', $metguide);
+
+	# EventList
+	$EventListDefault = $us->q->loadEvents('BASE', null, $lg, $us->RegionLangIsoCode, false);
+	$t->assign('EventListDefault', $EventListDefault);
+
+	$CauseListDefault = $us->q->loadCauses('BASE', null, $lg, $us->RegionLangIsoCode, false);
+	$t->assign('CauseListDefault', $CauseListDefault);
+
+	$ef1 = $us->q->queryLabelsFromGroup('Effect|People', $lg);
+	$ef2 = $us->q->queryLabelsFromGroup('Effect|Affected', $lg);
+	$ef3 = $us->q->queryLabelsFromGroup('Effect|Economic', $lg);
+	$ef4 = $us->q->queryLabelsFromGroup('Effect|More', $lg);
+	$sec = $us->q->queryLabelsFromGroup('Sector', $lg);
+	$EffectList = array_merge($ef1,$ef2,$ef3,$ef4,$sec);
+	$t->assign('EffectList', $EffectList);
+	
 	$t->force_compile   = true; # Force this template to always compile
 	$t->display('metguide.tpl');
 </script>
