43fa3383be2c972afeba93dfe8cea2a335b3f170
Jhon H. Caicedo <jhcaiced@inticol.com>
2016-02-25 17:27:53 -0500

    DI-19: Moved login handler from js/user_login.js to js2/user_login.js
    Created a new module js2/user_login.js which is namespaced and
    uses modern JS standars
    
    Moved the event handler for the user login form into the new module

---
+++ web/js2/user_login.js
@@ -0,0 +1,53 @@
+/*global desinventar,jQuery
+ */
+desinventar.user_login = (function () {
+    'use strict';
+    var me = {};
+
+    function setupBindings() {
+        jQuery('#frmUserLogin').on('submit', function () {
+            var UserId     = jQuery('#fldUserId').val(),
+                UserPasswd = jQuery("#fldUserPasswd").val(),
+                url = jQuery('#desinventarURL').val() + '/';
+
+            doUserLoginUpdateMsg('');
+
+            if (UserId === '' || UserPasswd === '') {
+                doUserLoginUpdateMsg('msgEmptyFields');
+                return false;
+            }
+            jQuery.post(url,
+                {
+                    'cmd'        : 'cmdUserLogin',
+                    'RegionId'   : jQuery('#desinventarRegionId').val(),
+                    'UserId'     : UserId,
+                    'UserPasswd' : hex_md5(UserPasswd)
+                },
+                function (data) {
+                    if (parseInt(data.Status, 10) < 0) {
+                        doUserLoginUpdateMsg('msgInvalidPasswd');
+                        return false;
+                    }
+                    doUserLoginUpdateMsg('msgUserLoggedIn');
+                    jQuery('#fldUserId').val('');
+                    jQuery("#fldUserPasswd").val('');
+
+                    // Update UserInfo Fields...
+                    doUserUpdateInfo(data.User);
+
+                    Ext.getCmp('wndUserLogin').hide();
+                    // Trigger Event and Update User Menu etc.
+                    jQuery('body').trigger('cmdMainWindowUpdate');
+                },
+                'json'
+            );
+            return false;
+        });
+    }
+
+    me.init = function () {
+        setupBindings();
+        //do other important setup things
+    };
+    return me;
+}());
---
+++ web/templates/js2.tpl
@@ -0,0 +1,6 @@
+<script type="text/javascript" src="{-$desinventarURL-}/js2/user_login.js?version={-$jsversion-}"></script>
+<script type="text/javascript">
+	jQuery(document).ready(function() {
+		desinventar.user_login.init();
+	});
+</script>
