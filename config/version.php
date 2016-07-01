c8be98f9347f73e1268a89d5d2549c4decd56bdf
Jhon H. Caicedo <jhcaiced@inticol.com>
2016-07-01 16:52:44 -0500

    DI-48: Maps - Fixed urls when saving map image locally

--- config/version.php
+++ config/version.php
@@ -1,6 +1,6 @@
 <?php
 return array(
     'major_version' => '10',
-    'version' => '10.01.004',
-    'release_date' => '2016-06-21'
+    'version' => '10.01.005',
+    'release_date' => '2016-07-01'
 );
