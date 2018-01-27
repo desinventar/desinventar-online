1fe64bcdb62280ebce4ad5e06395a22b9246b588
Jhon H. Caicedo <jhcaiced@inticol.com>
2018-01-27 06:26:30 -0500

    devel: refactor region.php to PSR2

---
+++ web/include/region_operations.php
@@ -0,0 +1,29 @@
+<?php
+/*
+ DesInventar - http://www.desinventar.org
+ (c) Corporacion OSSO
+*/
+
+function form2region($val)
+{
+    $dat = array();
+    $dat['RegionLabel']     = $val['RegionLabel'];
+    $dat['LangIsoCode']     = $val['LangIsoCode'];
+    $dat['CountryIso']      = $val['CountryIso'];
+    if (empty($val['RegionId'])) {
+        $dat['RegionId']    = DIRegion::buildRegionId($dat['CountryIso'], $dat['RegionLabel']);
+    } else {
+        $dat['RegionId']    = $val['RegionId'];
+    }
+    if (isset($val['RegionActive']) && $val['RegionActive'] == 'on') {
+        $dat['RegionStatus'] |= CONST_REGIONACTIVE;
+    } else {
+        $dat['RegionStatus'] &= ~CONST_REGIONACTIVE;
+    }
+    if (isset($val['RegionPublic']) && $val['RegionPublic'] == 'on') {
+        $dat['RegionStatus'] |= CONST_REGIONPUBLIC;
+    } else {
+        $dat['RegionStatus'] &= ~CONST_REGIONPUBLIC;
+    }
+    return $dat;
+}
--- web/region.php
+++ web/region.php
@@ -1,7 +1,7 @@
-<script language="php">
+<?php
 /*
- DesInventar - http://www.desinventar.org  
- (c) 1998-2012 Corporacion OSSO
+ DesInventar - http://www.desinventar.org
+ (c) Corporacion OSSO
 */
 
 require_once('include/loader.php');
@@ -12,111 +12,66 @@ require_once('include/digeolevel.class.php');
 require_once('include/digeocarto.class.php');
 require_once('include/digeography.class.php');
 
-function form2region ($val)
-{
-	$dat = array();
-	$dat['RegionLabel']		= $val['RegionLabel'];
-	$dat['LangIsoCode']		= $val['LangIsoCode'];
-	$dat['CountryIso'] 		= $val['CountryIso'];
-	if (empty($val['RegionId']))
-	{
-		$dat['RegionId']	= DIRegion::buildRegionId($dat['CountryIso'], $dat['RegionLabel']);
-	}
-	else
-	{
-		$dat['RegionId']	= $val['RegionId'];
-	}
-	if (isset($val['RegionActive']) && $val['RegionActive'] == 'on')
-	{
-		$dat['RegionStatus'] |= CONST_REGIONACTIVE;
-	}
-	else
-	{
-		$dat['RegionStatus'] &= ~CONST_REGIONACTIVE;
-	}
-	if (isset($val['RegionPublic']) && $val['RegionPublic'] == 'on')
-	{
-		$dat['RegionStatus'] |= CONST_REGIONPUBLIC;
-	}
-	else
-	{
-		$dat['RegionStatus'] &= ~CONST_REGIONPUBLIC;
-	}
-	return $dat;
+require_once 'include/region_operations.php';
+
+use \DesInventar\Legacy\DIRegion;
+
+if (isset($_POST['cmd']) && !empty($_POST['cmd'])) {
+    $cmd = $_POST['cmd'];
+} elseif (isset($_GET['cmd']) && !empty($_GET['cmd'])) {
+    $cmd = $_GET['cmd'];
 }
 
-if (isset($_POST['cmd']) && !empty($_POST['cmd']))
-{
-	$cmd = $_POST['cmd'];
-}
-elseif (isset($_GET['cmd']) && !empty($_GET['cmd']))
-{
-	$cmd = $_GET['cmd'];
-}
-
-switch ($cmd)
-{
-	case 'adminreg':
-		// ADMINREG: Form to Create and assign regions
-		$t->assign('CountryList' , $us->q->getCountryList());
-		$t->assign('usr'         , $us->getUserList(''));
-		$t->assign('LanguageList', $us->q->loadLanguages(1));
-		$t->assign('ctl_adminreg', true);
-		$t->assign('ctl_reglist' , true);
-	break;
-	case 'list':
-		// ADMINREG: reload list from local SQLITE
-		$t->assign('RegionList', $us->q->getRegionAdminList());
-		$t->assign('ctl_reglist', true);
-	break;
-	case 'createRegionsFromDBDir':
-		DIRegion::rebuildRegionListFromDirectory($us);
-		$t->assign('RegionList', $us->q->getRegionAdminList());
-		$t->assign('ctl_reglist', true);
-	break;
-	default:
-		// ADMINREG: insert or update region
-		if (($cmd == 'insert') || ($cmd == 'update'))
-		{
-			$data = form2region($_GET);
-			$r = new DIRegion($us, $data['RegionId']);
-			$r->setFromArray($data);
-			$stat = ERR_NO_DATABASE;
-			$t->assign('ctl_admregmess', true);
-			$stat = 0;
-			if ($cmd == 'insert')
-			{
-				$stat = $r->createRegionDB();
-				$t->assign('cfunct', 'insert');
-			}
-			elseif ($cmd == 'update')
-			{
-				$stat = $r->update();
-				$t->assign('cfunct', 'update');
-			}
-			$t->assign('regid', $data['RegionId']);
-			// Set Role ADMINREGION in RegionAuth: master for this region
-			if (!iserror($stat))
-			{
-				$rol = $us->setUserRole($_GET['RegionUserAdmin'], $data['RegionId'], 'ADMINREGION');
-			}
-			else
-			{
-				$t->assign('cfunct', '');
-				$rol = $stat;
-			}
-			if (!iserror($rol))
-			{
-				$t->assign('csetrole', true);
-			}
-			else
-			{
-				$t->assign('errsetrole', $rol);
-			}
-		}
-	break;
+switch ($cmd) {
+    case 'adminreg':
+        // ADMINREG: Form to Create and assign regions
+        $t->assign('CountryList', $us->q->getCountryList());
+        $t->assign('usr', $us->getUserList(''));
+        $t->assign('LanguageList', $us->q->loadLanguages(1));
+        $t->assign('ctl_adminreg', true);
+        $t->assign('ctl_reglist', true);
+        break;
+    case 'list':
+        // ADMINREG: reload list from local SQLITE
+        $t->assign('RegionList', $us->q->getRegionAdminList());
+        $t->assign('ctl_reglist', true);
+        break;
+    case 'createRegionsFromDBDir':
+        DIRegion::rebuildRegionListFromDirectory($us);
+        $t->assign('RegionList', $us->q->getRegionAdminList());
+        $t->assign('ctl_reglist', true);
+        break;
+    default:
+        // ADMINREG: insert or update region
+        if (($cmd == 'insert') || ($cmd == 'update')) {
+            $data = form2region($_GET);
+            $r = new DIRegion($us, $data['RegionId']);
+            $r->setFromArray($data);
+            $stat = ERR_NO_DATABASE;
+            $t->assign('ctl_admregmess', true);
+            $stat = 0;
+            if ($cmd == 'insert') {
+                $stat = $r->createRegionDB();
+                $t->assign('cfunct', 'insert');
+            } elseif ($cmd == 'update') {
+                $stat = $r->update();
+                $t->assign('cfunct', 'update');
+            }
+            $t->assign('regid', $data['RegionId']);
+            // Set Role ADMINREGION in RegionAuth: master for this region
+            if (!iserror($stat)) {
+                $rol = $us->setUserRole($_GET['RegionUserAdmin'], $data['RegionId'], 'ADMINREGION');
+            } else {
+                $t->assign('cfunct', '');
+                $rol = $stat;
+            }
+            if (!iserror($rol)) {
+                $t->assign('csetrole', true);
+            } else {
+                $t->assign('errsetrole', $rol);
+            }
+        }
+        break;
 } //switch
 $t->assign('dic', $us->q->queryLabelsFromGroup('DB', $lg));
 $t->display('region.tpl');
-
-</script>
