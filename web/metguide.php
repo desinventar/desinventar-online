a04ebec803494d1d7079e90e86cd79a6088c8830
Jhon H. Caicedo <jhcaiced@inticol.com>
2018-01-26 20:13:18 -0500

    devel: refactor metguide.php to PSR2

--- web/metguide.php
+++ web/metguide.php
@@ -1,38 +1,36 @@
 <?php
 /*
  DesInventar - http://www.desinventar.org
- (c) 1998-2015 Corporacion OSSO
+ (c) Corporacion OSSO
 */
-	require_once('include/loader.php');
-	$mod = 'metguide';
-	$pages = array(
-		'intro','whatis','aboutdesinv','regioninfo','geography',
-		'events','causes','extraeffects','datacards','references'
-	);
-	$metguide = array();
-	foreach($pages as $pagekey)
-	{
-		$metguide[$pagekey]  = $us->q->queryLabel($mod, $pagekey , $lg);
-	}
-	$t->assign('metguide', $metguide);
+require_once('include/loader.php');
+$mod = 'metguide';
+$pages = array(
+    'intro','whatis','aboutdesinv','regioninfo','geography',
+    'events','causes','extraeffects','datacards','references'
+);
+$metguide = array();
+foreach ($pages as $pagekey) {
+    $metguide[$pagekey]  = $us->q->queryLabel($mod, $pagekey, $lg);
+}
+$t->assign('metguide', $metguide);
 
-	# EventList
-	$EventListDefault = $us->q->loadEvents('BASE', null, $lg, $us->RegionLangIsoCode, false);
-	$t->assign('EventListDefault', $EventListDefault);
+# EventList
+$EventListDefault = $us->q->loadEvents('BASE', null, $lg, $us->RegionLangIsoCode, false);
+$t->assign('EventListDefault', $EventListDefault);
 
-	$CauseListDefault = $us->q->loadCauses('BASE', null, $lg, $us->RegionLangIsoCode, false);
-	$t->assign('CauseListDefault', $CauseListDefault);
+$CauseListDefault = $us->q->loadCauses('BASE', null, $lg, $us->RegionLangIsoCode, false);
+$t->assign('CauseListDefault', $CauseListDefault);
 
-	$ef1 = $us->q->queryLabelsFromGroup('Effect|People', $lg);
-	$ef2 = $us->q->queryLabelsFromGroup('Effect|Affected', $lg);
-	$ef3 = $us->q->queryLabelsFromGroup('Effect|Economic', $lg);
-	$ef4 = $us->q->queryLabelsFromGroup('Effect|More', $lg);
-	$sec = $us->q->queryLabelsFromGroup('Sector', $lg);
-	$EffectList = array_merge($ef1,$ef2,$ef3,$ef4,$sec);
-	$t->assign('EffectList', $EffectList);
+$ef1 = $us->q->queryLabelsFromGroup('Effect|People', $lg);
+$ef2 = $us->q->queryLabelsFromGroup('Effect|Affected', $lg);
+$ef3 = $us->q->queryLabelsFromGroup('Effect|Economic', $lg);
+$ef4 = $us->q->queryLabelsFromGroup('Effect|More', $lg);
+$sec = $us->q->queryLabelsFromGroup('Sector', $lg);
+$EffectList = array_merge($ef1, $ef2, $ef3, $ef4, $sec);
+$t->assign('EffectList', $EffectList);
 
-	if ($config->flags['mode'] == 'devel')
-	{
-		$t->force_compile   = true; # Force this template to always compile
-	}
-	$t->display('metguide-' . $lg . '.tpl');
+if ($config->flags['mode'] == 'devel') {
+    $t->force_compile   = true; # Force this template to always compile
+}
+$t->display('metguide-' . $lg . '.tpl');
