cc371693cea4dc4f59403e848de2f10fade6e386
Jhon H. Caicedo <jhcaiced@inticol.com>
2018-01-26 20:47:13 -0500

    devel: refactor database_operations.php to PSR2

--- web/include/database_operations.php
+++ web/include/database_operations.php
@@ -22,45 +22,40 @@ class DatabaseOperations
         return $answer;
     }
 
-    public static function create($us, $prmRegionId, $prmRegionInfo)
+    public static function create($session, $prmRegionId, $prmRegionInfo)
     {
         $iReturn = ERR_NO_ERROR;
         if ($iReturn > 0) {
             $RegionId = $prmRegionId;
-            $r = new DIRegionRecord($us, $RegionId);
-            $iReturn = $r->setFromArray($prmRegionInfo);
-            if ($r->get('RegionId') == '') {
+            $region = new DIRegionRecord($session, $RegionId);
+            $iReturn = $region->setFromArray($prmRegionInfo);
+            if ($region->get('RegionId') == '') {
                 $iReturn = ERR_UNKNOWN_ERROR;
             }
         }
         if ($iReturn > 0) {
-            $RegionId = $r->get('RegionId');
-            if (DIRegion::existRegion($us, $RegionId) > 0) {
-                # Database already exists
-                $iReturn = ERR_UNKNOWN_ERROR;
-            } else {
-                $iReturn = $r->insert();
-            }
+            $RegionId = $region->get('RegionId');
+            $iReturn = DIRegion::existRegion($session, $RegionId) > 0 ? ERR_UNKNOWN_ERROR : $region->insert();
         }
         if ($iReturn > 0) {
             # Set Role ADMINREGION in RegionAuth: master for this region
-            $r->removeRegionUserAdmin();
-            $RegionUserAdmin = $us->UserId;
-            $iReturn = $us->setUserRole($RegionUserAdmin, $r->get('RegionId'), 'ADMINREGION');
+            $region->removeRegionUserAdmin();
+            $RegionUserAdmin = $session->UserId;
+            $iReturn = $session->setUserRole($RegionUserAdmin, $region->get('RegionId'), 'ADMINREGION');
         }
         if ($iReturn > 0) {
-            $r2 = new DIRegionDB($us, $RegionId);
-            $iReturn = $r2->createRegionDB();
+            $region2 = new DIRegionDB($session, $RegionId);
+            $iReturn = $region2->createRegionDB();
         }
         return $iReturn;
     }
 
-    public static function replace($us, $prmRegionId, $prmRegionLabel, $prmFilename)
+    public static function replace($session, $prmRegionId, $prmRegionLabel, $prmFilename)
     {
         $iReturn = ERR_NO_ERROR;
         $RegionId = $prmRegionId;
         $RegionLabel = $prmRegionLabel;
-        $OutDir = TMP_DIR . '/' . $us->sSessionId;
+        $OutDir = TMP_DIR . '/' . $session->sSessionId;
         $filename = $OutDir . '/' . $prmFilename;
         if (! file_exists($filename)) {
             $iReturn = ERR_DEFAULT_ERROR;
@@ -74,16 +69,16 @@ class DatabaseOperations
                 $iReturn = ERR_UNKNOWN_ERROR;
             }
             if ($iReturn > 0) {
-                $DBDir = $us->getDBDir();
+                $DBDir = $session->getDBDir();
                 $zip->extractTo($DBDir);
                 $zip->close();
 
-                $r = new DIRegion($us, $RegionId);
-                $r->set('RegionId', $RegionId);
+                $region = new DIRegion($session, $RegionId);
+                $region->set('RegionId', $RegionId);
                 if ($RegionLabel != '') {
-                    $r->set('RegionLabel', $RegionLabel);
+                    $region->set('RegionLabel', $RegionLabel);
                 }
-                $r->update();
+                $region->update();
             }
         }
         if (file_exists($filename)) {
