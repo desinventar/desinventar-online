d0be87721e8d0c58ab9f1856b99f3dc15cb3a99d
Jhon H. Caicedo <jhcaiced@inticol.com>
2018-01-27 06:39:03 -0500

    devel: refactor user.php to PSR2

--- web/include/diuser.class.php
+++ web/include/diuser.class.php
@@ -1,62 +1,62 @@
 <?php
 /*
  DesInventar - http://www.desinventar.org
- (c) 1998-2012 Corporacion OSSO
+ (c) Corporacion OSSO
 */
+namespace DesInventar\Legacy;
 
 use Aura\SqlQuery\QueryFactory;
+use \DIRecord;
 
 class DIUser extends DIRecord
 {
-	public function __construct($prmSession)
-	{
-		$this->sTableName   = "User";
-		$this->sPermPrefix  = "ADMIN";
-		$this->sFieldKeyDef = "UserId/STRING";
-		$this->sFieldDef    = "UserEMail/STRING," .
-		                      "UserPasswd/STRING," .
-		                      "UserFullName/STRING," .
-		                      "Organization/STRING," .
-		                      "CountryIso/STRING," .
-		                      "UserCity/STRING," .
-		                      "UserCreationDate/DATETIME," .
-		                      "UserNotes/STRING," .
-		                      "UserActive/BOOLEAN";
-		parent::__construct($prmSession);
-		$num_args = func_num_args();
-		$this->setConnection("core");
-		if ($num_args >= 2)
-		{
-			$prmUserId = func_get_arg(1);
-			if ($prmUserId != '')
-			{
-				$this->set('UserId', $prmUserId);
-				$this->load();
-			}
-		}
-	} // __construct
-	
-	public function getSelectQuery($prmTableName = '')
-	{
-		$query = parent::getSelectQuery($prmTableName);
-		$query .= " OR (UserNotes LIKE '%(UserName=" . $this->get('UserId') . ")%')";
-		return $query;
-	}
+    public function __construct($prmSession)
+    {
+        $this->sTableName   = "User";
+        $this->sPermPrefix  = "ADMIN";
+        $this->sFieldKeyDef = "UserId/STRING";
+        $this->sFieldDef    = "UserEMail/STRING," .
+                              "UserPasswd/STRING," .
+                              "UserFullName/STRING," .
+                              "Organization/STRING," .
+                              "CountryIso/STRING," .
+                              "UserCity/STRING," .
+                              "UserCreationDate/DATETIME," .
+                              "UserNotes/STRING," .
+                              "UserActive/BOOLEAN";
+        parent::__construct($prmSession);
+        $num_args = func_num_args();
+        $this->setConnection("core");
+        if ($num_args >= 2) {
+            $prmUserId = func_get_arg(1);
+            if ($prmUserId != '') {
+                $this->set('UserId', $prmUserId);
+                $this->load();
+            }
+        }
+    }
 
-	public function updatePasswd($id, $passwd) {
-		$queryFactory = new QueryFactory('sqlite');
-		$update = $queryFactory->newUpdate();
-		$update
-			->table($this->sTableName)
-			->cols(array('UserPasswd'))
-			->where('UserId = :UserId')
-			->bindValues(array(
-				'UserPasswd' => $passwd,
-				'UserId' => $id
-			));
-		$sth = $this->conn->prepare($update->getStatement());
-		$sth->execute($update->getBindValues());
-		return true;
-	}
+    public function getSelectQuery($prmTableName = '')
+    {
+        $query = parent::getSelectQuery($prmTableName);
+        $query .= " OR (UserNotes LIKE '%(UserName=" . $this->get('UserId') . ")%')";
+        return $query;
+    }
+
+    public function updatePasswd($id, $passwd)
+    {
+        $queryFactory = new QueryFactory('sqlite');
+        $update = $queryFactory->newUpdate();
+        $update
+            ->table($this->sTableName)
+            ->cols(array('UserPasswd'))
+            ->where('UserId = :UserId')
+            ->bindValues(array(
+                'UserPasswd' => $passwd,
+                'UserId' => $id
+            ));
+        $sth = $this->conn->prepare($update->getStatement());
+        $sth->execute($update->getBindValues());
+        return true;
+    }
 }
-
---
+++ web/include/user_operations.php
@@ -0,0 +1,39 @@
+<?php
+/*
+ DesInventar - http://www.desinventar.org
+ (c) Corporación OSSO
+*/
+
+function form2user($val)
+{
+    $dat = array();
+    if (!isset($val['UserId'])) {
+        $ifo = current($val);
+    }
+    $dat['UserId']          = isset($val['UserId'])? $val['UserId']: key($val);
+    $dat['UserFullName']    = isset($val['UserFullName'])? $val['UserFullName']: $ifo[2];
+    $dat['UserEMail']       = isset($val['UserEMail'])? $val['UserEMail']: $ifo[0];
+    $dat['UserCountry']     = isset($val['UserCountry'])? $val['UserCountry']: $ifo[4];
+    $dat['UserCity']        = isset($val['UserCity'])? $val['UserCity']: $ifo[5];
+    if (isset($val['UserActive'])) {
+        if (($val['UserActive'] == 'on') || $val['UserActive']) {
+            $dat['UserActive']  = true;
+        } else {
+            $dat['UserActive']  = false;
+        }
+    } else {
+        $dat['UserActive']  = 1;
+    }
+    $dat['NUserPasswd']     = isset($val['NUserPasswd'])? $val['NUserPasswd']: '';
+    $dat['NUserPasswd2']    = isset($val['NUserPasswd2'])? $val['NUserPasswd2']: '';
+    if (!empty($dat['NUserPasswd']) && ($dat['NUserPasswd'] == $dat['NUserPasswd2'])) {
+        $dat['UserPasswd']      = $dat['NUserPasswd'];
+    } elseif (isset($val['UserPasswd']) && !empty($val['UserPasswd'])) {
+        $dat['UserPasswd']      = $val['UserPasswd'];
+    } elseif (isset($ifo[1]) && !empty($ifo[1])) {
+        $dat['UserPasswd']      = $ifo[1];
+    } else {
+        $dat['UserPasswd']      = 'desinventar'; // default password
+    }
+    return $dat;
+}
