ddbd6d750c4ac515962b84591e07dfd29909579a
Jhon H. Caicedo <jhcaiced@inticol.com>
2018-02-04 12:33:36 +0000

    docker: Updated base docker image for desinventar/centos6
    * Added installation of firefox and Xvfb packages
    * Updated to use node v8.9.4
    * Update docker setup to use new image
    * Update circleci configuration to use new image

--- docker/centos6/docker-run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker run -d -p 8080:80 --name desinventar desinventar/centos6
-
-# Debug in interactive mode
-#docker run -it --rm -p 8080:80 desinventar/centos6
-
-
--- docker/centos6/docker-stop.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#docker ps -a | grep 'weeks ago' | awk '{print $1}' | xargs --no-run-if-empty docker rm
-
-docker stop desinventar
-docker rm desinventar
