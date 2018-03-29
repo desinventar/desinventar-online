9047c05a8829476a60afacd3bd2d595b75b97ea9
Jhon H. Caicedo <jhcaiced@inticol.com>
2018-03-29 18:40:45 -0500

    portal: Applying eslint/prettier fixes for JS code

--- portal/web/js/main.js
+++ portal/web/js/main.js
@@ -1,225 +1,221 @@
 /*
  DesInventar - http://www.desinventar.org
- (c) 1998-2013 Corporacion OSSO
+ (c) Corporacion OSSO
 */
 function onReadyPortal() {
-	var desinventarLang = jQuery('#desinventarLang').val();
+  var desinventarLang = jQuery('#desinventarLang').val()
 
-	jQuery('#btnMainWindow').attr('href', jQuery('#desinventarURL').val() + '/');
+  jQuery('#btnMainWindow').attr('href', jQuery('#desinventarURL').val() + '/')
 
-	// Main button to open a default desinventar
-	jQuery("#btnMainWindow").click(function() {
-		jQuery(this).attr('href', jQuery('#desinventarURL').val() + '/'  + '?lang=' + desinventarLang);
-		return true;
-	});
-	
-	// Update version number on screen
-	jQuery('#txtVersion').text(jQuery('#desinventarVersion').val());
-	
-	// Handle clicks on mainpage map
-	jQuery('area').click(function() {
-		var country = jQuery(this).attr('alt');
-		if (country != '') {
-			updateDatabaseList(country, 1);
-		}
-		// Prevent default action
-		return false;
-	});
-	
-	// Handle clicks on RegionGroup Items
-	jQuery('.RegionGroup').click(function() {
-		var group = jQuery(this).attr('alt');
-		if (group != '') {
-			var expanded = jQuery(this).attr('expanded');
-			if (expanded == 'yes') {
-				jQuery('#sect' + group).hide();
-				jQuery(this).attr('expanded','no');
-			} else {
-				jQuery('#sect' + group).show();
-				jQuery(this).attr('expanded','yes');
-			}
-			//displayList(group);
-		}
-		return false;
-	});
+  // Main button to open a default desinventar
+  jQuery('#btnMainWindow').click(function() {
+    jQuery(this).attr(
+      'href',
+      jQuery('#desinventarURL').val() + '/' + '?lang=' + desinventarLang
+    )
+    return true
+  })
 
-	// Expand some region groups at start
-	jQuery('.RegionGroup[expand=yes]').trigger('click');
+  // Update version number on screen
+  jQuery('#txtVersion').text(jQuery('#desinventarVersion').val())
 
-	// Handle clicks on countries that return a list of regions
-	jQuery('.RegionList').click(function() {
-		var country = jQuery(this).attr('alt');
-		if (country != '') {
-			updateDatabaseList(country, 1);
-		}
-		// Prevent default action
-		return false;
-	});
+  // Handle clicks on mainpage map
+  jQuery('area').click(function() {
+    var country = jQuery(this).attr('alt')
+    if (country != '') {
+      updateDatabaseList(country, 1)
+    }
+    // Prevent default action
+    return false
+  })
 
-	// Handle clicks on countries that directly shows a Region
-	jQuery('.RegionItem').click(function() {
-		var RegionId = jQuery(this).attr('alt');
-		if (RegionId != '') {
-			showRegionInfo(RegionId);
-		}
-		// Prevent default action
-		return false;
-	});
+  // Handle clicks on RegionGroup Items
+  jQuery('.RegionGroup').click(function() {
+    var group = jQuery(this).attr('alt')
+    if (group != '') {
+      var expanded = jQuery(this).attr('expanded')
+      if (expanded == 'yes') {
+        jQuery('#sect' + group).hide()
+        jQuery(this).attr('expanded', 'no')
+      } else {
+        jQuery('#sect' + group).show()
+        jQuery(this).attr('expanded', 'yes')
+      }
+      //displayList(group);
+    }
+    return false
+  })
 
-	jQuery('body').bind('UserUpdateInfo', function() {
-		UserName = jQuery('#desinventarUserFullName').val();
-		UserId   = jQuery('#desinventarUserId').val();
-		if (UserId != '')
-		{
-			jQuery('#txtUserInfo').text(UserId + ' ' + UserName);
-			jQuery('#txtUserFullName').text(UserName);
-			jQuery('#txtUserId').text('(' + UserId + ')');
-		} else {
-			jQuery('#txtUserInfo').text('');
-			jQuery('#txtUserFullName').text('');
-			jQuery('#txtUserId').text('');
-		}
-	});
-		
-	jQuery('body').bind('UserLoggedIn', function() {
-		jQuery('.lstUserMenu').show();
-		jQuery('#lstUserLogin').hide();
-		jQuery('#divUserIsLoggedIn').show();
-		jQuery('#divUserIsLoggedOut').hide();
-		updateDatabaseListByUser();
-		jQuery('body').trigger('UserUpdateInfo');
-	});
+  // Expand some region groups at start
+  jQuery('.RegionGroup[expand=yes]').trigger('click')
 
-	jQuery('body').bind('UserLoggedOut', function() {
-		// Update User Menu
-		jQuery('.lstUserMenu').show();
-		jQuery('#lstUserLogout').hide();
-		jQuery('#divUserIsLoggedIn').hide();
-		jQuery('#divUserIsLoggedOut').show();
-		jQuery('#frmUserLogin').hide();
-		jQuery('body').trigger('UserUpdateInfo');
-		showMap();
-	});
-	
-	// Handle clicks on right menu (Home/LangSelect)
-	jQuery('.MenuItem').click(function() {
-		var MenuItem = jQuery(this).attr('id');
-		if (MenuItem == 'mnuShowMap') {
-			displayPortal('portal');
-			showMap();
-		}
-		// Prevent default action
-		return false;
-	});
-	
-	jQuery('#linkShowUserLogin').click(function() {
-		jQuery('#frmUserLogin').toggle();
-		return false;
-	});
+  // Handle clicks on countries that return a list of regions
+  jQuery('.RegionList').click(function() {
+    var country = jQuery(this).attr('alt')
+    if (country != '') {
+      updateDatabaseList(country, 1)
+    }
+    // Prevent default action
+    return false
+  })
 
-	jQuery('#linkUserRegionList').click(function() {
-		updateDatabaseListByUser();
-		return false;
-	});
-	
-	jQuery('#linkUserLogout').click(function() {
-		doUserLogout();
-		return false;
-	});
+  // Handle clicks on countries that directly shows a Region
+  jQuery('.RegionItem').click(function() {
+    var RegionId = jQuery(this).attr('alt')
+    if (RegionId != '') {
+      showRegionInfo(RegionId)
+    }
+    // Prevent default action
+    return false
+  })
 
-	// Top Menu/Language Select
-	jQuery('#MainMenu').clickMenu();
-	// Remove the black border from the menu
-	jQuery('div.cmDiv').css('border','0px solid black');
-	
-	displayPortal(jQuery('#desinventarPortalType').val());
+  jQuery('body').bind('UserUpdateInfo', function() {
+    UserName = jQuery('#desinventarUserFullName').val()
+    UserId = jQuery('#desinventarUserId').val()
+    if (UserId != '') {
+      jQuery('#txtUserInfo').text(UserId + ' ' + UserName)
+      jQuery('#txtUserFullName').text(UserName)
+      jQuery('#txtUserId').text('(' + UserId + ')')
+    } else {
+      jQuery('#txtUserInfo').text('')
+      jQuery('#txtUserFullName').text('')
+      jQuery('#txtUserId').text('')
+    }
+  })
 
-	jQuery('#linkPortalGAR2011').click(function() {
-		displayPortal('gar-isdr-2011');
-		return false;
-	});
-	
-	if (jQuery('#desinventarUserId').val() != '') {
-		jQuery('body').trigger('UserLoggedIn');
-	} else {
-		jQuery('body').trigger('UserLoggedOut');
-	}
-	
-	jQuery('.regionlink').on('click', function(event) {
-		var RegionId = jQuery('#desinventarRegionId').val();
-		var LangIsoCode = jQuery('#desinventarLang').val();
-		window.open(jQuery('#desinventarURL').val() + '/#' + RegionId);
-		return false;
-	});
+  jQuery('body').bind('UserLoggedIn', function() {
+    jQuery('.lstUserMenu').show()
+    jQuery('#lstUserLogin').hide()
+    jQuery('#divUserIsLoggedIn').show()
+    jQuery('#divUserIsLoggedOut').hide()
+    updateDatabaseListByUser()
+    jQuery('body').trigger('UserUpdateInfo')
+  })
 
-	jQuery.post(
-		jQuery('#desinventarURL').val() + '/',
-		{
-			cmd : 'cmdGetVersion'
-		},
-		function(data)
-		{
-			if (parseInt(data.Status) > 0)
-			{
-				jQuery('#txtVersion').text(data.Version);
-			}
-		},
-		'json'
-	);
-	//Initialization code
-	jQuery('body').trigger('cmdUserGetInfo');
+  jQuery('body').bind('UserLoggedOut', function() {
+    // Update User Menu
+    jQuery('.lstUserMenu').show()
+    jQuery('#lstUserLogout').hide()
+    jQuery('#divUserIsLoggedIn').hide()
+    jQuery('#divUserIsLoggedOut').show()
+    jQuery('#frmUserLogin').hide()
+    jQuery('body').trigger('UserUpdateInfo')
+    showMap()
+  })
+
+  // Handle clicks on right menu (Home/LangSelect)
+  jQuery('.MenuItem').click(function() {
+    var MenuItem = jQuery(this).attr('id')
+    if (MenuItem == 'mnuShowMap') {
+      displayPortal('portal')
+      showMap()
+    }
+    // Prevent default action
+    return false
+  })
+
+  jQuery('#linkShowUserLogin').click(function() {
+    jQuery('#frmUserLogin').toggle()
+    return false
+  })
+
+  jQuery('#linkUserRegionList').click(function() {
+    updateDatabaseListByUser()
+    return false
+  })
+
+  jQuery('#linkUserLogout').click(function() {
+    doUserLogout()
+    return false
+  })
+
+  // Top Menu/Language Select
+  jQuery('#MainMenu').clickMenu()
+  // Remove the black border from the menu
+  jQuery('div.cmDiv').css('border', '0px solid black')
+
+  displayPortal(jQuery('#desinventarPortalType').val())
+
+  jQuery('#linkPortalGAR2011').click(function() {
+    displayPortal('gar-isdr-2011')
+    return false
+  })
+
+  if (jQuery('#desinventarUserId').val() != '') {
+    jQuery('body').trigger('UserLoggedIn')
+  } else {
+    jQuery('body').trigger('UserLoggedOut')
+  }
+
+  jQuery('.regionlink').on('click', function(event) {
+    var RegionId = jQuery('#desinventarRegionId').val()
+    var LangIsoCode = jQuery('#desinventarLang').val()
+    window.open(jQuery('#desinventarURL').val() + '/#' + RegionId)
+    return false
+  })
+
+  jQuery.post(
+    jQuery('#desinventarURL').val() + '/',
+    {
+      cmd: 'cmdGetVersion'
+    },
+    function(data) {
+      if (parseInt(data.Status) > 0) {
+        jQuery('#txtVersion').text(data.Version)
+      }
+    },
+    'json'
+  )
+  //Initialization code
+  jQuery('body').trigger('cmdUserGetInfo')
 } //onReadyPortal()
 
-function displayPortal(myPortal) 
-{
-	// Select which portal to display : main, gar2009, gar2011
-	jQuery('.divBlock').hide();
-	jQuery('#desinventarPortalType').val(myPortal);
-	switch(myPortal)
-	{
-		case 'gar2009':
-		case 'gar-isdr-2009':
-			jQuery('.divBlockGAR2009').show();
-		break;
-		case 'gar-isdr-2011':
-		case 'gar2011':
-			jQuery('.divBlockGAR2011').show();
-		break;
-		default:
-			jQuery('.divBlockSouthAmerica').show();
-			// At start, display the map 
-			showMap();
-		break;
-	}
+function displayPortal(myPortal) {
+  // Select which portal to display : main, gar2009, gar2011
+  jQuery('.divBlock').hide()
+  jQuery('#desinventarPortalType').val(myPortal)
+  switch (myPortal) {
+    case 'gar2009':
+    case 'gar-isdr-2009':
+      jQuery('.divBlockGAR2009').show()
+      break
+    case 'gar-isdr-2011':
+    case 'gar2011':
+      jQuery('.divBlockGAR2011').show()
+      break
+    default:
+      jQuery('.divBlockSouthAmerica').show()
+      // At start, display the map
+      showMap()
+      break
+  }
 }
 
 function showRegionInfo(RegionId) {
-	jQuery(".contentBlock" ).hide();
-	jQuery(".contentRegionBlock").hide();
-	displayRegionInfo(RegionId);
+  jQuery('.contentBlock').hide()
+  jQuery('.contentRegionBlock').hide()
+  displayRegionInfo(RegionId)
 }
 
 function displayRegionInfo(RegionId) {
-	jQuery(".contentBlock").hide();
-	jQuery("#pageinfo").hide();
-	doGetRegionInfo(RegionId);
-	jQuery('#desinventarRegionId').val(RegionId);
-	jQuery('#regionBlock').show();
-	jQuery('#pageinfo').show();
+  jQuery('.contentBlock').hide()
+  jQuery('#pageinfo').hide()
+  doGetRegionInfo(RegionId)
+  jQuery('#desinventarRegionId').val(RegionId)
+  jQuery('#regionBlock').show()
+  jQuery('#pageinfo').show()
 }
 
 function showMap() {
-	jQuery('.contentBlock').hide();
-	jQuery('#pagemap').show();
-};
+  jQuery('.contentBlock').hide()
+  jQuery('#pagemap').show()
+}
 
 // personalization List menu..
 function displayList(elem) {
-	lst = 7;
-	for (i=1; i <= lst; i++) {
-		if (i == elem)
-			jQuery("#sect"+ i).show();
-		else
-			jQuery("#sect"+ i).hide();
-	}
-}; //function
+  lst = 7
+  for (i = 1; i <= lst; i++) {
+    if (i == elem) jQuery('#sect' + i).show()
+    else jQuery('#sect' + i).hide()
+  }
+} //function
--- portal/web/js/user_login.js
+++ portal/web/js/user_login.js
@@ -1,131 +1,119 @@
 /*
  DesInventar - http://www.desinventar.org
- (c) 1998-2013 Corporacion OSSO
+ (c) Corporacion OSSO
 */
 
-function onReadyUserLogin()
-{
-	// hide all status messages on start
-	updateUserLoginMsg('');
-	
-	// submit form validation and process..
-	jQuery("#frmUserLogin").submit(function() {
-		doUserLogin();
-		return(false);
-	});
+function onReadyUserLogin() {
+  // hide all status messages on start
+  updateUserLoginMsg('')
 
-	jQuery('body').on('cmdUserGetInfo', function() {
-		doUserGetInfo();
-	});
+  // submit form validation and process..
+  jQuery('#frmUserLogin').submit(function() {
+    doUserLogin()
+    return false
+  })
+
+  jQuery('body').on('cmdUserGetInfo', function() {
+    doUserGetInfo()
+  })
 }
 
-function doUserGetInfo()
-{
-	jQuery.post(
-		jQuery('#desinventarURL').val() + '/',
-		{
-			cmd : 'cmdUserGetInfo'
-		},
-		function(data)
-		{
-			if (parseInt(data.Status) > 0)
-			{
-				updateUserLoginMsg("#msgUserLoggedIn");
-				// After login, clear passwd field
-				jQuery('#fldUserId').val('');
-				jQuery("#fldUserPasswd").val('');
+function doUserGetInfo() {
+  jQuery.post(
+    jQuery('#desinventarURL').val() + '/',
+    {
+      cmd: 'cmdUserGetInfo'
+    },
+    function(data) {
+      if (parseInt(data.Status) > 0) {
+        updateUserLoginMsg('#msgUserLoggedIn')
+        // After login, clear passwd field
+        jQuery('#fldUserId').val('')
+        jQuery('#fldUserPasswd').val('')
 
-				// Update UserInfo Fields...
-				jQuery('#desinventarUserId').val(data.User.Id);
-				jQuery('#desinventarUserFullName').val(data.User.FullName);
+        // Update UserInfo Fields...
+        jQuery('#desinventarUserId').val(data.User.Id)
+        jQuery('#desinventarUserFullName').val(data.User.FullName)
 
-				// Trigger Event and Update User Menu etc.
-				jQuery('body').trigger('UserLoggedIn');
-			}
-		},
-		'json'
-	);
+        // Trigger Event and Update User Menu etc.
+        jQuery('body').trigger('UserLoggedIn')
+      }
+    },
+    'json'
+  )
 }
 
-function doUserLogin()
-{
-	var UserId     = jQuery('#fldUserId').val();
-	var UserPasswd = jQuery("#fldUserPasswd").val();
-	if (UserId == '' || UserPasswd == '')
-	{
-		updateUserLoginMsg('#msgEmptyFields');
-	}
-	else
-	{
-		jQuery.post(
-			jQuery('#desinventarURL').val() + '/',
-			{
-				'cmd'        : 'cmdUserLogin',
-				'UserId'     : UserId,
-				'UserPasswd' : hex_md5(UserPasswd)
-			},
-			function(data) {
-				if (parseInt(data.Status) > 0) {
-					updateUserLoginMsg("#msgUserLoggedIn");
-					// After login, clear passwd field
-					jQuery('#fldUserId').val('');
-					jQuery("#fldUserPasswd").val('');
+function doUserLogin() {
+  var UserId = jQuery('#fldUserId').val()
+  var UserPasswd = jQuery('#fldUserPasswd').val()
+  if (UserId == '' || UserPasswd == '') {
+    updateUserLoginMsg('#msgEmptyFields')
+  } else {
+    jQuery.post(
+      jQuery('#desinventarURL').val() + '/',
+      {
+        cmd: 'cmdUserLogin',
+        UserId: UserId,
+        UserPasswd: hex_md5(UserPasswd)
+      },
+      function(data) {
+        if (parseInt(data.Status) > 0) {
+          updateUserLoginMsg('#msgUserLoggedIn')
+          // After login, clear passwd field
+          jQuery('#fldUserId').val('')
+          jQuery('#fldUserPasswd').val('')
 
-					// Update UserInfo Fields...
-					jQuery('#desinventarUserId').val(data.User.Id);
-					jQuery('#desinventarUserFullName').val(data.User.FullName);
+          // Update UserInfo Fields...
+          jQuery('#desinventarUserId').val(data.User.Id)
+          jQuery('#desinventarUserFullName').val(data.User.FullName)
 
-					// Trigger Event and Update User Menu etc.
-					jQuery('body').trigger('UserLoggedIn');
-				} else {
-					updateUserLoginMsg("#msgInvalidPasswd");
-				}
-			},
-			'json'
-		);
-	}
+          // Trigger Event and Update User Menu etc.
+          jQuery('body').trigger('UserLoggedIn')
+        } else {
+          updateUserLoginMsg('#msgInvalidPasswd')
+        }
+      },
+      'json'
+    )
+  }
 }
 
-function doUserLogout()
-{
-	var Answer = 0;
-	jQuery.post(
-		jQuery('#desinventarURL').val() + '/',
-		{
-			'cmd'        : 'cmdUserLogout'
-		},
-		function(data)
-		{
-			if (parseInt(data.Status) > 0)
-			{
-				Answer = 1;
-				updateUserLoginMsg("#msgUserLoggedOut");
-				// After login, clear passwd field
-				jQuery('#fldUserId').val('');
-				jQuery('#fldUserPasswd').val('');
-				
-				// Update UserInfo Fields...
-				jQuery('#desinventarUserId').val('');
-				jQuery('#desinventarUserFullName').val('');
-				
-				// Trigger Event, used to update menu or reload page...
-				jQuery('body').trigger('UserLoggedOut');
-			} else {
-				updateUserLoginMsg("#msgInvalidLogout");
-				Answer = 0;
-			}
-		},
-		'json'
-	);
-	return Answer;
+function doUserLogout() {
+  var Answer = 0
+  jQuery.post(
+    jQuery('#desinventarURL').val() + '/',
+    {
+      cmd: 'cmdUserLogout'
+    },
+    function(data) {
+      if (parseInt(data.Status) > 0) {
+        Answer = 1
+        updateUserLoginMsg('#msgUserLoggedOut')
+        // After login, clear passwd field
+        jQuery('#fldUserId').val('')
+        jQuery('#fldUserPasswd').val('')
+
+        // Update UserInfo Fields...
+        jQuery('#desinventarUserId').val('')
+        jQuery('#desinventarUserFullName').val('')
+
+        // Trigger Event, used to update menu or reload page...
+        jQuery('body').trigger('UserLoggedOut')
+      } else {
+        updateUserLoginMsg('#msgInvalidLogout')
+        Answer = 0
+      }
+    },
+    'json'
+  )
+  return Answer
 }
 function updateUserLoginMsg(msgId) {
-	// Hide all status Msgs (class="status")
-	jQuery(".status").hide();
-	if (msgId != '') {
-		// Show specified message(s)
-		jQuery(msgId).show();
-	}
-	return(true);
-};
-
+  // Hide all status Msgs (class="status")
+  jQuery('.status').hide()
+  if (msgId != '') {
+    // Show specified message(s)
+    jQuery(msgId).show()
+  }
+  return true
+}
