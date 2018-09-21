c688295cc2d74a71d588775969eb94978ae18611
Jhon H. Caicedo <jhcaiced@inticol.com>
2018-09-21 10:20:16 -0500

    [di-97] Updated xml2conf, handle parameters using GetOpt
    * Update makefile calls for xml2conf

--- scripts/xml2conf.php
+++ scripts/xml2conf.php
@@ -1,6 +1,24 @@
 <?php
-$FileName    = $argv[1];
-$LangIsoCode = $argv[2];
+
+use Fostam\GetOpts\Handler;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$getopts = new Handler();
+$getopts->addOption('filename')->long('file')->required()->argument('filename');
+$getopts->addOption('language')->long('language')->required()->argument('language');
+try {
+    $getopts->parse();
+} catch (Exception $e) {
+    echo "ERROR: " . $e->getMessage() . "\n";
+}
+
+$FileName = $getopts->get('filename');
+$LangIsoCode = $getopts->get('language');
+
+if (!file_exists($FileName)) {
+    throw new Exception('Cannot find XML file: ' . $FileName);
+}
 
 $dom = new DOMDocument('1.0', 'UTF-8');
 $dom->load($FileName);
@@ -16,8 +34,8 @@ foreach ($xp->query('Group') as $tree) {
         $Message = $tree1->getAttribute('id');
         $xp2 = new DOMXPath($dom);
         $xpathQuery = 'Group[@name="' . $GroupName . '"]' .
-          '/Message[@id="' . $Message . '"]' .
-          '/Text[@LangIsoCode="' . $LangIsoCode . '"]';
+            '/Message[@id="' . $Message . '"]' .
+            '/Text[@LangIsoCode="' . $LangIsoCode . '"]';
         foreach ($xp2->query($xpathQuery) as $node) {
             $value = '' . $node->nodeValue;
             $value = preg_replace('/"/', '\'', $value);
