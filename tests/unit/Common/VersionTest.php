72a6c5f78a36fb52972c3e57760d64d2048e1881
Jhon H. Caicedo <jhcaiced@inticol.com>
2018-12-23 19:57:02 -0500

    [di-213] Update method used to return the version number
    * Read version and release date values from package.json
    * Add config/test.json file to the repository, not used yet but
      this fixes a warning when running the API tests

--- tests/unit/Common/VersionTest.php
+++ tests/unit/Common/VersionTest.php
@@ -12,6 +12,7 @@ final class VersionTest extends TestCase
         $version = new Version('production');
         $this->assertTrue(is_numeric($version->getMajorVersion()));
         $this->assertTrue(is_array($version->getVersionArray()));
-        $this->assertTrue(preg_match('/\d{4}-\d{2}-\d{2}/', $version->getReleaseDate()) > 0);
+        $this->assertTrue(preg_match('/^\d{4}-\d{2}-\d{2}/', $version->getReleaseDate()) > 0);
+        $this->assertTrue(preg_match('/^\d{2}\.\d{2}\.\d{3}/', $version->getVersion()) > 0);
     }
 }
