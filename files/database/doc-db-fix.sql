405f4bac8fb24634f1642e1cf453a2d7ca057858
Jhon H. Caicedo <jhcaiced@inticol.com>
2019-03-23 09:30:17 -0500

    [devel] Remove unused sql and yaml files
    * These files were being used to initialize the databases, but now
      we use knex to migrate the databases

--- files/database/doc-db-fix.sql
+++
@@ -1,4 +0,0 @@
-UPDATE Dictionary SET LangIsoCode='spa' WHERE LangIsoCode='es';
-UPDATE Dictionary SET LangIsoCode='eng' WHERE LangIsoCode='en';
-UPDATE Dictionary SET LangIsoCode='fre' WHERE LangIsoCode='fr';
-UPDATE Dictionary SET LangIsoCode='por' WHERE LangIsoCode='pt';
