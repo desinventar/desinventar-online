175b3222b663b89b71323ad65140e1396bf00711
Jhon H. Caicedo <jhcaiced@inticol.com>
2019-09-27 18:46:08 -0500

    [DI-250] Endpoint to update the value of GeographyFQName

--- src/Api/Controllers/AdminGeographyController.php
+++ src/Api/Controllers/AdminGeographyController.php
@@ -10,12 +10,14 @@ use Api\Helpers\JsonApiResponse;
 
 use DesInventar\Actions\AdminGeographyGetByCodeAction;
 use DesInventar\Actions\AdminGeographyRenameByCodeAction;
+use DesInventar\Actions\AdminGeographyUpdateFQNameByCodeAction;
 
 class AdminGeographyController extends ApiController
 {
     public function routes($app)
     {
         $app->get('/geography/{code}', [$this, 'getByCode']);
+        $app->put('/geography/{code}/update-fqname', [$this, 'updateFQNameByCode']);
         $app->get('/geography/rename/{code}/{newCode}', [$this, 'renameByCode']);
     }
 
@@ -26,6 +28,20 @@ class AdminGeographyController extends ApiController
             'attr' => $request->getAttributes(),
             'response' => (
                 new AdminGeographyGetByCodeAction(
+                    $this->container->get('db')->getDbConnection($args['regionId']),
+                    $this->container->get('logger')
+                )
+            )->execute($args['code'])
+        ]);
+    }
+
+    public function updateFQNameByCode(Request $request, Response $response, $args)
+    {
+        return (new JsonApiResponse($response))->data([
+            'args' => $args,
+            'attr' => $request->getAttributes(),
+            'response' => (
+                new AdminGeographyUpdateFQNameByCodeAction(
                     $this->container->get('db')->getDbConnection($args['regionId']),
                     $this->container->get('logger')
                 )
---
+++ src/DesInventar/Actions/AdminGeographyUpdateFQNameByCodeAction.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace DesInventar\Actions;
+
+use Exception;
+use DesInventar\Models\Geography;
+
+class AdminGeographyUpdateFQNameByCodeAction
+{
+    protected $pdo = null;
+    protected $logger= null;
+
+    public function __construct($pdo, $logger)
+    {
+        $this->pdo = $pdo;
+        $this->logger = $logger;
+    }
+
+    public function execute($code)
+    {
+        $geography = (new Geography($this->pdo, $this->logger))->updateFQNameByCode($code);
+        if (!$geography) {
+            throw new Exception('Cannot find geography item with code: ' . $code);
+        }
+        return $geography;
+    }
+}
