34bafe4eff03534f09f90af736fe7da34eba5c35
Jhon H. Caicedo <jhcaiced@inticol.com>
2019-10-03 20:12:15 -0500

    [DI-203] Use Lokalise's json files to create language conf files
    * Download the translation strings from Lokalise in JSON format
    * Process the strings with a script to create the conf files
    * Remove old interface_strings.xml

---
+++ files/database/compileLanguage.js
@@ -0,0 +1,62 @@
+const fs = require('fs')
+const winston = require('winston')
+
+const languages = [
+  {
+    confFile: 'eng.conf',
+    jsonFile: 'locale/en.json'
+  },
+  {
+    confFile: 'spa.conf',
+    jsonFile: 'locale/es_419.json'
+  },
+  {
+    confFile: 'fre.conf',
+    jsonFile: 'locale/fr.json'
+  },
+  {
+    confFile: 'por.conf',
+    jsonFile: 'locale/pt.json'
+  }
+]
+
+const logger = winston.loggers.add('language', {
+  transports: [
+    new winston.transports.Console({
+      level: 'debug'
+    })
+  ]
+})
+
+function sortByGroup(keys) {
+  const stringsByGroup = {}
+  Object.keys(keys).forEach(key => {
+    const keyParts = key.split('.')
+    const groupKey = keyParts[0]
+    const stringKey = keyParts[1]
+    if (!stringsByGroup[groupKey]) {
+      stringsByGroup[groupKey] = {}
+    }
+    stringsByGroup[groupKey][stringKey] = keys[key]['translation']
+  })
+  return stringsByGroup
+}
+
+languages.forEach(language => {
+  const confFile = `${__dirname}/${language.confFile}`
+  logger.info(`${language.jsonFile} => ${language.confFile}`)
+  const fileName = `${__dirname}/${language.jsonFile}`
+  const keys = require(fileName)
+
+  const stringsByGroup = sortByGroup(keys)
+
+  const file = fs.createWriteStream(confFile)
+  Object.keys(stringsByGroup).forEach(groupKey => {
+    file.write(`[${groupKey}]\n`)
+    Object.keys(stringsByGroup[groupKey]).forEach(stringKey => {
+      file.write(`${stringKey}=${stringsByGroup[groupKey][stringKey]}\n`)
+    })
+    file.write(`\n`)
+  })
+  file.end()
+})
